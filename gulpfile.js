003b80a5b467409c65272ba8564dac1fb98808c1
Ryan Seddon <seddon.ryan@gmail.com>
2014-04-22 16:57:03 +1000

    Adding unit test task to build process
    * Setup travis integration
    * Flowdock and samson notifications
    * Jasmine + karma tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var jshint = require('gulp-jshint');
 var browserify = require('gulp-browserify');
 var through = require('through');
 var Compiler = require('es6-module-transpiler').Compiler;
+var karma = require('gulp-karma');
 
 function ES6ModuleCompile(opts) {
   var buf = '';
@@ -22,7 +23,12 @@ function ES6ModuleCompile(opts) {
   }
 }
 
-gulp.task('build', ['lint'], function() {
+var testFiles = [
+  'dist/main.js',
+  'test/**/*.js'
+];
+
+gulp.task('build', ['lint', 'test'], function() {
   return gulp.src('src/main.js')
     .pipe(browserify({
       transform: [ES6ModuleCompile()]
@@ -30,6 +36,17 @@ gulp.task('build', ['lint'], function() {
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('test', function() {
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      throw err;
+    });
+})
 
 gulp.task('lint', function() {
   gulp.src('src/*.js')
@@ -39,6 +56,7 @@ gulp.task('lint', function() {
 
 gulp.task('watch', ['build'], function() {
   gulp.watch(['src/**/*.js'], ['build']);
+  gulp.watch(['test/**/*.js'], ['test']);
 });
 
 gulp.task('default', ['build']);
