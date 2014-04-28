13880d34e6f0475b8de79d1ff82c1fd8d2b0f0a4
Isaac Su <me@isaacsu.com>
2014-04-28 15:18:56 +1000

    implemented launcher with iframe component "Frame".

---
+++ src/services/launcher/Frame.js
@@ -0,0 +1,58 @@
+/** @jsx React.DOM */
+import { _ } from 'lodash';
+module React from 'react';
+
+export var Frame = React.createClass({
+  documentNormalizer: function() {
+    return (
+      "body {              \
+         margin:0;         \
+         padding: 0px;     \
+         background: none; \
+      }                    \
+      .Launcher--default {             \
+         position: fixed;              \
+         width: 50px;                  \
+         height: 50px;                 \
+         background: rgba(0,0,0,.7);   \
+         text-align: center;           \
+         color: #fff;                  \
+         padding: 0;                   \
+         fontFamily: sans-serif;       \
+         fontSize: 12px;               \
+         border-radius: 25px 25px 3px; \
+         cursor: pointer;              \
+      }                                \
+      .Launcher--default > div {       \
+         font-family: sans-serif; \
+         font-weight: normal; \
+         font-size: 26px; \
+         margin-top: 11px; \
+      }");
+  },
+
+  render: function() {
+    var base = { border: 'none' },
+    iframeStyle = _.extend(base, this.props.style);
+    return <iframe style={iframeStyle} />;
+  },
+
+  componentDidMount: function() {
+    var head;
+    React.renderComponent(this.props.children, this.getDOMNode().contentDocument.body);
+    head = this.getDOMNode().contentDocument.getElementsByTagName("head")[0];
+
+    var styleTag = document.createElement('style');
+    head.appendChild(styleTag);
+    styleTag.innerHTML = this.documentNormalizer() + " " + this.props.documentStyle;
+  },
+
+  componentDidUpdate: function() {
+    React.renderComponent(this.props.children, this.getDOMNode().contentDocument.body);
+  },
+
+  componentWillUnmount: function() {
+    React.unmountComponentAtNode(this.getDOMNode().contentDocument.body);
+  }
+
+});
--- src/services/launcher/Launcher.js
+++ src/services/launcher/Launcher.js
@@ -1,89 +1,33 @@
 /** @jsx React.DOM */
 import { _ } from 'lodash';
 module React from 'react';
+import { Frame } from './Frame.js';
 
 var launchers = {};
 
 var Launcher = React.createClass({
-  wrapperStyle: function() {
-    var base = {
-      position:"fixed",
-      width:"auto",
-      height:"auto",
-      background:"#0a8",
-      textAlign:"center",
-      color:"#fff",
-      padding:"6px 20px",
-      fontFamily:"sans-serif",
-      fontSize:"12px",
-      borderRadius: "8px 8px 0 0",
-      cursor: "pointer",
-    };
-
-    switch(this.props.position) {
-
-      case 'bottom-left':
-        return _.extend(base, {
-          right: null,
-          left: "50px",
-          bottom: "0px"
-        });
-      break
-
-      case 'side-left':
-        return _.extend(base, {
-          right: null,
-          left: "0px",
-          bottom: "400px",
-          transform: "rotate(90deg)",
-          transformOrigin: "0% 100%"
-        });
-      break;
-
-      case 'side-right':
-        return _.extend(base, {
-          right: "0px",
-          left: null,
-          bottom: "400px",
-          transform: "rotate(-90deg)",
-          transformOrigin: "100% 100%"
-        });
-      break;
-
-      case 'bottom-right':
-      default:
-        return _.extend(base, {
-          right: "50px",
-          left: null,
-          bottom: "0px"
-        });
-      break;
-    }
-  },
-
-  textStyle: function() {
-    if (0 && _.any(['side-left', 'side-right'], function(el) { return this.props.position == el;}.bind(this))) {
-      return {
-        transform: "rotate(90deg)"
-      };
-    }
-    else {
-      return {};
-    }
-  },
-
   render: function() {
+      var iframeStyle = {
+          border: 'none',
+          height: '50px',
+          width: '50px',
+          position: 'fixed',
+          bottom: '10px',
+          right: '20px'
+      };
+
     return (
-      <div onClick={this.props.onClick} 
-           style={this.wrapperStyle()}>
-        <div style={this.textStyle()}>Hello, my name is {this.props.name}.</div>
-      </div>
+      <Frame style={iframeStyle}>
+        <div onClick={this.props.onClick} className="Launcher--default">
+          <div>?</div>
+        </div>
+      </Frame>
     );
   }
 });
 
-function create(name, position, callback) {
-  launchers[name] = <Launcher name={name} onClick={callback} position={position} />;
+function create(name, config) {
+  launchers[name] = <Launcher name={name} onClick={config.onClick} />;
 }
 
 function list() {
@@ -100,6 +44,7 @@ function get(name) {
 
 function render(name) {
   var el = document.body.appendChild(document.createElement('div'));
+  var iframe;
   React.renderComponent(launchers[name], el);
 }
 
