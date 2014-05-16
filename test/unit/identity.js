b7acb4de2305ac93840e85f415bb306929bc8ce5
Isaac Su <me@isaacsu.com>
2014-05-16 13:09:00 +1000

    Fixed module file reference in test/unit/identity.js

--- test/unit/identity.js
+++ test/unit/identity.js
@@ -6,11 +6,11 @@ describe('identity', function() {
     mockery.registerMock('service/persistence', {
       store: function() {}
     });
-    mockery.registerAllowable('../../unes6/service/identity');
+    mockery.registerAllowable('../../build/unes6/service/identity');
   });
 
   it('works', function() {
-    var identity = require('../../unes6/service/identity');
+    var identity = require('../../build/unes6/service/identity');
     expect(1).toBe(1);
   });
 });
