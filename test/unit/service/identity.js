cd80e0d2c14c7f240a3cedd0bb022d495c925104
Isaac Su <me@isaacsu.com>
2014-05-16 15:09:44 +1000

    Unit test for identity service.

--- test/unit/service/identity.js
+++ test/unit/service/identity.js
@@ -1,16 +1,50 @@
 var mockery = require('mockery');
 
 describe('identity', function() {
+  var identity,
+      mockPersistence = {
+        store: { 
+          get: function() {},
+          set: function() {}
+        }
+      };
+
   beforeEach(function() {
     mockery.enable();
-    mockery.registerMock('service/persistence', {
-      store: function() {}
-    });
+    mockery.registerMock('service/persistence', mockPersistence);
     mockery.registerAllowable(buildPath('service/identity'));
+    identity = require(buildPath('service/identity')).identity;
   });
 
-  it('works', function() {
-    var identity = require(buildPath('service/identity'));
-    expect(1).toBe(1);
+  afterEach(function() {
+    mockery.deregisterAll();
+    mockery.disable();
+  });
+
+  it('returns a previously set buid', function() {
+    var buid;
+    spyOn(mockPersistence.store, 'get').andReturn('abc123');
+
+    buid = identity.getBuid();
+
+    expect(mockPersistence.store.get).toHaveBeenCalledWith('buid');
+    expect(buid).toEqual('abc123');
+  });
+
+  it('sets a new buid if none is available', function() {
+    var recentCall;
+
+    spyOn(mockPersistence.store, 'get').andReturn(undefined);
+    spyOn(mockPersistence.store, 'set');
+
+    buid = identity.getBuid();
+
+    expect(mockPersistence.store.set).toHaveBeenCalled();
+
+    recentCall = mockPersistence.store.set.mostRecentCall;
+
+    expect(recentCall.args[0]).toEqual('buid');
+    expect(typeof recentCall.args[1]).toEqual('string');
+    expect(recentCall.args[1].length).toEqual(32);
   });
 });
