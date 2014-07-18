03c7b4231304aac666bb18e49ae8ab3d96e3f078
Isaac Su <me@isaacsu.com>
2014-07-18 14:45:06 +1000

    script.async, formatting.

--- src/cacheBuster/update.js
+++ src/cacheBuster/update.js
@@ -1,12 +1,12 @@
 var script = document.createElement('script');
 
+script.async = true;
 script.src='main.js';
 
 if (location.hash !== '#DONE') { 
   location.hash = 'DONE'; 
   location.reload(true);
-}
-else { 
+} else { 
   document.documentElement.appendChild(script);
   window.addEventListener('load', function() {
     parent.postMessage('cache_bust_done', '*');
