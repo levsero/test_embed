044e54d2e2668ac86e6a4ed9c9680943b18e3fd1
Briana Coppard <bcoppard@zendesk.com>
2014-07-28 12:21:57 +1000

    Add example page, some slight refactoring of chat and add config on show and hide so it works with launcher

--- src/embed/chat/chat.js
+++ src/embed/chat/chat.js
@@ -3,12 +3,12 @@ require('imports?_=lodash!lodash');
 
 var chats = {};
 
-var configDefaults = {
-  position: 'br',
-  offsetVertical: 70
-};
-
 function create(name, config) {
+  var configDefaults = {
+    position: 'br',
+    offsetVertical: 70
+  };
+
   chats[name] = {
     config: _.extend(configDefaults, config)
   };
@@ -24,23 +24,37 @@ function get(name) {
 
 function show(name) {
   var config = get(name).config;
+
   win.$zopim(function() {
     win.$zopim.livechat.window.setPosition(config.position);
     win.$zopim.livechat.window.setOffsetVertical(config.offsetVertical);
     win.$zopim.livechat.window.show();
   });
+
+  if(_.isFunction(config.onShow)) {
+    config.onShow();
+  }
 }
 
-function toggleVisibility(name) {
+function hide(name) {
   var config = get(name).config;
+
   win.$zopim(function() {
-    win.$zopim.livechat.window.setPosition(config.position);
-    win.$zopim.livechat.window.setOffsetVertical(config.offsetVertical);
-    if (win.$zopim.livechat.window.getDisplay()) {
-      win.$zopim.livechat.hideAll();
+    win.$zopim.livechat.hideAll();
+  });
+
+  if(_.isFunction(config.onHide)) {
+    config.onHide();
+  }
+}
+
+function toggleVisibility(name, isActive) {
+  win.$zopim(function() {
+    if (isActive && win.$zopim.livechat.window.getDisplay()) {
+      hide(name);
     }
     else {
-      win.$zopim.livechat.window.show();
+      show(name);
     }
   });
 }
@@ -49,20 +63,21 @@ function render(name) {
 
   var zopimId = get(name).config.zopimId,
       scriptTag,
+      snippet;
+
   /* jshint maxlen: false   */
   /* jshint quotmark: false */
-  
-      snippet = "window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s= d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set. _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8'); $.src='//v2.zopim.com/?<%= zopimId %>';z.t=+new Date;$. type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');$zopim(function(){";
-  snippet += "$zopim.livechat.hideAll();";
-  snippet += "$zopim.livechat.clearAll();";
-  snippet += "});";
+  snippet = "window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s= d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set. _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8'); $.src='//v2.zopim.com/?<%= zopimId %>';z.t=+new Date;$. type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');$zopim(function(){"
+          + "$zopim.livechat.hideAll();"
+          + "$zopim.livechat.clearAll();"
+          + "});";
 
   scriptTag = document.createElement('script');
   scriptTag.type='text/javascript';
   document.body.appendChild(scriptTag);
-  
-  scriptTag.innerHTML = _.template(snippet, {zopimId: zopimId});
 
+  scriptTag.innerHTML = _.template(snippet, {zopimId: zopimId});
+  hide(name);
 }
 
 export var chat  = {
