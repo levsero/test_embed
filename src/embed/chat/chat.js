dba9377c5a42ffdfabc0b0ea8c1d9ad0909f46f7
Briana Coppard <bcoppard@zendesk.com>
2014-07-29 13:36:18 +1000

    Formatting jshint lines and snippet

--- src/embed/chat/chat.js
+++ src/embed/chat/chat.js
@@ -64,23 +64,26 @@ function render(name) {
       scriptTag,
       snippet;
 
-  /* jshint maxlen: false   */
-  /* jshint quotmark: false */
-  /* jshint laxbreak: true */
-  snippet = "window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s= d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set. _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8'); $.src='//v2.zopim.com/?<%= zopimId %>';z.t=+new Date;$. type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');$zopim(function(){"
-          + "$zopim.livechat.hideAll();"
-          + "$zopim.livechat.clearAll();"
-          + "});";
+  /* jshint maxlen: false,
+            quotmark: false,
+            laxbreak: true
+  */
+  snippet = [
+    "window.$zopim||",
+    "(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s= d.createElement(s),e=d.getElementsByTagName(s)[0];",
+    "z.set=function(o){z.set. _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8'); ",
+    "$.src='//v2.zopim.com/?<%= zopimId %>';",
+    "z.t=+new Date;$. type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');$zopim(function(){",
+    "$zopim.livechat.hideAll();",
+    "$zopim.livechat.clearAll();",
+    "});"
+  ].join('');
 
   scriptTag = document.createElement('script');
   scriptTag.type='text/javascript';
   document.body.appendChild(scriptTag);
 
   scriptTag.innerHTML = _.template(snippet, {zopimId: zopimId});
-
-  win.$zopim(function() {
-    win.$zopim.livechat.hideAll();
-  });
 }
 
 export var chat  = {
