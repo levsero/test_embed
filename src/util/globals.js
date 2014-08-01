f2d63d4edee5321379272fa2279a63338076543c
Ryan Seddon <rseddon@zendesk.com>
2014-08-01 14:31:05 +1000

    Add hardcoded zopim api key and setup renderer to only show zopim on /zopim pathname

--- src/util/globals.js
+++ src/util/globals.js
@@ -1,6 +1,7 @@
 var win = window.top;
 var document = win.document; /* jshint ignore:line */
 var navigator = win.navigator; /* jshint ignore:line */
+var location = win.location; /* jshint ignore:line */
 
-export { win, document, navigator };
+export { win, document, navigator, location };
 
