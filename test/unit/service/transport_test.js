38fa9570299a071542b15f33409b1b90c8daead9
Briana Coppard <bcoppard@zendesk.com>
2014-09-03 13:09:41 +1000

    Fix tests

--- test/unit/service/transport_test.js
+++ test/unit/service/transport_test.js
@@ -112,7 +112,7 @@ describe('transport', function() {
       expect(mockSuperagent)
         .toHaveBeenCalledWith(
           'GET',
-          'http://zensnow.herokuapp.com/test/path');
+          'http://test.zendesk.host/test/path');
     });
 
     it('should respect host page protocol', function() {
@@ -126,7 +126,7 @@ describe('transport', function() {
       expect(mockSuperagent)
         .toHaveBeenCalledWith(
           'GET',
-          'https://zensnow.herokuapp.com/test/path');
+          'https://test.zendesk.host/test/path');
     });
 
     it('sets the json type', function() {
@@ -138,24 +138,6 @@ describe('transport', function() {
 
       expect(mockMethods.type)
         .toHaveBeenCalledWith('json');
-    });
-
-    it('appends the zendesk_host params to the params posted', function() {
-
-      var recentCall;
-      spyOn(mockMethods, 'send').andCallThrough();
-
-      transport.init(config);
-      transport.send(payload);
-
-      expect(mockMethods.send);
-
-      recentCall = mockMethods.send.mostRecentCall;
-
-      /* jshint sub:true */
-      expect(recentCall.args[0]['zendesk_host'])
-        .toBe(config.zendeskHost);
-
     });
 
     it('sets payload.params to {} if no params are passed through', function() {
