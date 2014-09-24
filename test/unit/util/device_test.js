5a848cd72519760ae93a751dc6faee2a5f4bcceb
Briana Coppard <bcoppard@zendesk.com>
2014-09-24 12:29:20 +1000

    Add tests, rename utils to utils_test to keep it constant with the rest of the tests

---
+++ test/unit/util/device_test.js
@@ -0,0 +1,53 @@
+describe('devices', function() {
+  var isBlacklisted,
+      mockGlobals = {
+        win: noop,
+        navigator: {
+          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.122 Safari/537.36' /* jshint ignore: line */
+        }
+      },
+      mockRegistry,
+      devicesPath = buildSrcPath('util/devices');
+
+  beforeEach(function() {
+    mockery.enable({ useCleanCache: true });
+
+    mockRegistry = initMockRegistry({
+      'utility/globals': mockGlobals
+    });
+
+    mockery.registerAllowable(devicesPath);
+    isBlacklisted = require(devicesPath).isBlacklisted;
+  });
+
+  afterEach(function() {
+    mockery.deregisterAll();
+    mockery.disable();
+  });
+
+  describe('isBlacklisted', function() {
+
+    it('returns false if user agent has nothing within it blacklisted', function() {
+      expect(isBlacklisted())
+        .toBe(false);
+    });
+
+    it('returns true if chrome browser on os 8 is within the user agent string', function() {
+      /* jshint maxlen: false */
+      mockGlobals.navigator.userAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 8_0 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3';
+
+      expect(isBlacklisted())
+        .toBe(true);
+    });
+
+    it('returns false if chrome browser before os 8 is within the user agent string', function() {
+      /* jshint maxlen: false */
+      mockGlobals.navigator.userAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 7_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3';
+
+      expect(isBlacklisted())
+        .toBe(false);
+    });
+
+  });
+
+});
