846db937fb6647f8290c62125ce6db6c8295e8d7
Briana Coppard <bcoppard@zendesk.com>
2014-09-25 10:11:55 +1000

    os -> ios

--- test/unit/util/device_test.js
+++ test/unit/util/device_test.js
@@ -32,7 +32,7 @@ describe('devices', function() {
         .toBe(false);
     });
 
-    it('returns true if chrome browser on os 8 is within the user agent string', function() {
+    it('returns true if chrome browser on iOS 8 is within the user agent string', function() {
       /* jshint maxlen: false */
       mockGlobals.navigator.userAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 8_0 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3';
 
@@ -40,7 +40,7 @@ describe('devices', function() {
         .toBe(true);
     });
 
-    it('returns false if chrome browser before os 8 is within the user agent string', function() {
+    it('returns false if chrome browser not on iOS 8 is within the user agent string', function() {
       /* jshint maxlen: false */
       mockGlobals.navigator.userAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 7_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3';
 
