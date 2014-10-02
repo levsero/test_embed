05f18bc30918195d7d214ae377a564293fc181ed
Isaac Su <me@isaacsu.com>
2014-10-02 14:36:26 +1000

    Move mediator rulesets from service/renderer to service/mediator

--- src/service/mediator.js
+++ src/service/mediator.js
@@ -2,6 +2,106 @@ module airwaves from 'airwaves';
 
 var channel = new airwaves.Channel();
 
+function initTicketSubmissionMediator() {
+  var ticketForm = 'ticketSubmissionForm',
+      launcher = 'ticketSubmissionLauncher',
+      c = channel,
+      state = {};
+
+  state[`${ticketForm}.isVisible`] = false;
+
+  channel.intercept(
+    [`${launcher}.onClick`,
+     `${ticketForm}.onClose`].join(','),
+    function() {
+      if (state[`${ticketForm}.isVisible`]) {
+        c.broadcast(`${ticketForm}.hide`);
+        c.broadcast(`${launcher}.deactivate`);
+        state[`${ticketForm}.isVisible`] = false;
+      }
+      else {
+        c.broadcast(`${ticketForm}.show`);
+        c.broadcast(`${launcher}.activate`);
+        state[`${ticketForm}.isVisible`] = true;
+      }
+    });
+}
+
+function initChatTicketSubmissionMediator() {
+  var ticketForm = 'ticketSubmissionForm',
+      launcher = 'chatLauncher',
+      chat = 'zopimChat',
+      state = {};
+
+  state[`${ticketForm}.isVisible`] = false;
+  state[`${chat}.isVisible`]       = false;
+  state[`${chat}.isOnline`]        = false;
+  state[`${chat}.unreadMsgs`]      = 0;
+
+  channel.intercept(`${chat}.onOnline`, function() {
+    state[`${chat}.isOnline`] = true;
+    channel.broadcast(`${launcher}.setLabelChat`);
+  });
+
+  channel.intercept(`${chat}.onOffline`, function() {
+    state[`${chat}.isOnline`] = false;
+    channel.broadcast(`${launcher}.setLabelHelp`);
+  });
+
+  channel.intercept(`${chat}.onShow`, function() {
+    state[`${chat}.isVisible`] = true;
+    channel.broadcast(`${launcher}.activate`);
+  });
+
+  channel.intercept(`${chat}.onUnreadMsgs`, function(b, count) {
+    console.log('unreadMsg', count);
+    state[`${chat}.unreadMsgs`] = count;
+    if (state[`${chat}.isOnline`]) {
+      channel.broadcast(`${launcher}.setLabelUnreadMsgs`, count);
+    }
+  });
+
+  channel.intercept(
+    [`${launcher}.onClick`,
+     `${ticketForm}.onClose`].join(','),
+    function() {
+      if (state[`${chat}.isVisible`] || state[`${ticketForm}.isVisible`]) {
+        if (state[`${chat}.isVisible`]) {
+          channel.broadcast(`${chat}.hide`);
+          state[`${chat}.isVisible`] = false;
+        }
+        if (state[`${ticketForm}.isVisible`]) {
+          channel.broadcast(`${ticketForm}.hide`);
+          state[`${ticketForm}.isVisible`] = false;
+        }
+        channel.broadcast(`${launcher}.deactivate`);
+      }
+      else {
+        if (state[`${chat}.isOnline`]) {
+          channel.broadcast(`${chat}.show`);
+          state[`${chat}.isVisible`] = true;
+        }
+        else {
+          channel.broadcast(`${ticketForm}.show`);
+          state[`${ticketForm}.isVisible`] = true;
+        }
+        channel.broadcast(`${launcher}.activate`);
+      }
+    });
+
+  channel.subscribe(`${launcher}.deactivate`, function() {
+    if (state[`${chat}.isOnline`]) {
+      channel.broadcast(`${launcher}.setLabelChat`);
+    }
+    else {
+      channel.broadcast(`${launcher}.setLabelHelp`);
+    }
+  });
+}
+
+
 export var mediator = {
-  channel: channel
+  channel: channel,
+  initTicketSubmissionMediator: initTicketSubmissionMediator,
+  initChatTicketSubmissionMediator: initChatTicketSubmissionMediator
 };
--- src/service/renderer.js
+++ src/service/renderer.js
@@ -53,105 +53,11 @@ function init(config) {
     });
 
     renderedEmbeds = config;
-    //initChatTicketSubmissionMediator();
-    initTicketSubmissionMediator();
+    mediator.initChatTicketSubmissionMediator();
+    //mediator.initTicketSubmissionMediator();
 
     initialised = true;
   }
-}
-
-function initChatTicketSubmissionMediator() {
-  var state = {
-    'ticketSubmissionForm.isVisible': false,
-    'zopimChat.isVisible': false,
-    'zopimChat.isOnline': false,
-    'zopimChat.unreadMsgs': 0
-  };
-
-  mediator.channel.intercept('zopimChat.onOnline', function() {
-    state['zopimChat.isOnline'] = true;
-    mediator.channel.broadcast('chatLauncher.setLabelChat');
-  });
-
-  mediator.channel.intercept('zopimChat.onOffline', function() {
-    state['zopimChat.isOnline'] = false;
-    mediator.channel.broadcast('chatLauncher.setLabelHelp');
-  });
-
-  mediator.channel.intercept('zopimChat.onShow', function() {
-    state['zopimChat.isVisible'] = true;
-    mediator.channel.broadcast('chatLauncher.activate');
-  });
-
-  mediator.channel.intercept('zopimChat.onUnreadMsgs', function(b, count) {
-    console.log('unreadMsg', count);
-    state['zopimChat.unreadMsgs'] = count;
-    if (state['zopimChat.isOnline']) {
-      mediator.channel.broadcast('chatLauncher.setLabelUnreadMsgs', count);
-    }
-  });
-
-  mediator.channel.intercept(
-    ['chatLauncher.onClick',
-     'ticketSubmissionForm.onClose'].join(','),
-    function() {
-      if (state['zopimChat.isVisible'] || state['ticketSubmissionForm.isVisible']) {
-        if (state['zopimChat.isVisible']) {
-          mediator.channel.broadcast('zopimChat.hide');
-          state['zopimChat.isVisible'] = false;
-        }
-        if (state['ticketSubmissionForm.isVisible']) {
-          mediator.channel.broadcast('ticketSubmissionForm.hide');
-          state['ticketSubmissionForm.isVisible'] = false;
-        }
-        mediator.channel.broadcast('chatLauncher.deactivate');
-      }
-      else {
-        if (state['zopimChat.isOnline']) {
-          mediator.channel.broadcast('zopimChat.show');
-          state['zopimChat.isVisible'] = true;
-        }
-        else {
-          mediator.channel.broadcast('ticketSubmissionForm.show');
-          state['ticketSubmissionForm.isVisible'] = true;
-        }
-        mediator.channel.broadcast('chatLauncher.activate');
-      }
-    });
-
-  mediator.channel.subscribe('chatLauncher.deactivate', function() {
-    if (state['zopimChat.isOnline']) {
-      mediator.channel.broadcast('chatLauncher.setLabelChat');
-    }
-    else {
-      mediator.channel.broadcast('chatLauncher.setLabelHelp');
-    }
-  });
-}
-
-function initTicketSubmissionMediator() {
-  var ticketForm = 'ticketSubmissionForm',
-      launcher = 'ticketSubmissionLauncher',
-      c = mediator.channel,
-      state = {};
-
-  state[`${ticketForm}.isVisible`] = false;
-
-  mediator.channel.intercept(
-    [`${launcher}.onClick`,
-     `${ticketForm}.onClose`].join(','),
-    function() {
-      if (state[`${ticketForm}.isVisible`]) {
-        c.broadcast(`${ticketForm}.hide`);
-        c.broadcast(`${launcher}.deactivate`);
-        state[`${ticketForm}.isVisible`] = false;
-      }
-      else {
-        c.broadcast(`${ticketForm}.show`);
-        c.broadcast(`${launcher}.activate`);
-        state[`${ticketForm}.isVisible`] = true;
-      }
-    });
 }
 
 function propagateFontRatio(ratio) {
