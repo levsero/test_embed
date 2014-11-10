236d2acaaa069d664802488e653f7c5b2ee58e4c
Briana Coppard <bcoppard@zendesk.com>
2014-11-10 17:14:45 +1100

    replace \W with manual punctuation search

--- src/component/mixin/searchFilter.js
+++ src/component/mixin/searchFilter.js
@@ -1,7 +1,7 @@
 import { stopWords } from 'mixin/stopWords';
 
 export var stopWordsFilter = function(str) {
-  var words = str.replace(/\W/g, ' ').split(' ');
+  var words = str.replace(/[!"#$%&()*+,\-./:;<=>?@[\\\]^_`{|}~]/g, ' ').split(' ');
 
   words = _.filter(words, function(word) {
     if (stopWords.indexOf(word.toLowerCase()) === -1) {
