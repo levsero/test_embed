0425bebe56ce3e6301ed66dfcb318ae9ccffcb11
Sadia Karim <sadiakarim31@gmail.com>
2015-01-19 14:39:48 +1100

    Break up lines/edit syntax

--- src/util/devices.js
+++ src/util/devices.js
@@ -43,8 +43,13 @@ function shouldGoFullscreen() {
 }
 
 function isBlacklisted() {
-  // Iphone chrome on ios 8.0.x displays a blank space instead of content
-  return ((navigator.userAgent.indexOf('CriOS') !== -1 && navigator.userAgent.indexOf('OS 8_0') !== -1) || navigator.userAgent.indexOf('MSIE 9.0') !== -1);
+  return (
+    // Iphone chrome on ios 8.0.x displays a blank space instead of content
+    (navigator.userAgent.indexOf('CriOS') !== -1 && navigator.userAgent.indexOf('OS 8_0') !== -1) ||
+
+    // MSIE 9.0
+    navigator.userAgent.indexOf('MSIE 9.0') !== -1
+  );
 }
 
 export {
