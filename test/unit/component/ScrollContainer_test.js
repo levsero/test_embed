eb5790a01895c9680a62e2424edbc9e990f82245
Ryan Seddon <rseddon@zendesk.com>
2015-07-24 16:43:37 +1000

    Add tests for scrollToBottom method

--- test/unit/component/ScrollContainer_test.js
+++ test/unit/component/ScrollContainer_test.js
@@ -54,6 +54,37 @@ describe('ScrollContainer component', function() {
       .toHaveBeenCalled();
   });
 
+  it('should call `this.getContentContainer` when `this.scrollToBottom` is called', function() {
+
+    var stub = spyOn(ScrollContainer.type.prototype.__reactAutoBindMap, 'getContentContainer')
+                 .and.callThrough(),
+        scrollContainer = React.render(
+      <ScrollContainer fullscreen={true} />,
+      global.document.body
+    );
+
+    scrollContainer.scrollToBottom();
+
+    expect(stub)
+      .toHaveBeenCalled();
+  });
+
+  it('should set scrollTop to scrollHeight value when calling `this.scrollToBottom`', function() {
+
+    spyOn(ScrollContainer.type.prototype.__reactAutoBindMap, 'getContentContainer')
+      .and.returnValue({scrollHeight: 100, scrollTop: 0});
+
+    var scrollContainer = React.render(
+      <ScrollContainer fullscreen={true} />,
+      global.document.body
+    );
+
+    scrollContainer.scrollToBottom();
+
+    expect(scrollContainer.getContentContainer().scrollTop)
+      .toEqual(100);
+  });
+
   it('should call `this.checkScrollOffset` on componentDidUpdate', function() {
 
     var stub = spyOn(ScrollContainer.type.prototype.__reactAutoBindMap, 'checkScrollOffset'),
