97ff9650e926454e18f4755b61456edb2948c8be
Isaac Su <me@isaacsu.com>
2015-08-07 11:16:09 +1000

    Button and ButtonSecondary should use className and style props.

--- test/unit/component/Button_test.js
+++ test/unit/component/Button_test.js
@@ -1,5 +1,6 @@
-describe('Button component', function() {
+describe('component/Button', function() {
   let Button,
+      ButtonSecondary,
       mockRegistry;
   const buttonPath = buildSrcPath('component/Button');
 
@@ -22,6 +23,7 @@ describe('Button component', function() {
     mockery.registerAllowable(buttonPath);
 
     Button = require(buttonPath).Button;
+    ButtonSecondary = require(buttonPath).ButtonSecondary;
   });
 
   afterEach(function() {
@@ -29,31 +31,95 @@ describe('Button component', function() {
     mockery.disable();
   });
 
-  it('should not have fullscreen classes when fullscreen prop is false', function() {
-    const button = React.render(
-      <Button />,
-      global.document.body
-    );
-    const buttonElem = ReactTestUtils
-      .findRenderedDOMComponentWithClass(button, 'c-btn');
-    const buttonClasses = buttonElem.props.className;
+  describe('Button', function() {
+    it('should not have fullscreen classes when fullscreen prop is false', function() {
+      const button = React.render(
+        <Button />,
+        global.document.body
+      );
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonClasses = buttonElem.props.className;
 
-    expect(buttonClasses)
-      .not.toMatch('u-sizeFull');
+      expect(buttonClasses)
+        .not.toMatch('u-sizeFull');
+    });
+
+    it('should have fullscreen classes when fullscreen prop is true', function() {
+      const button = React.render(
+        <Button fullscreen={true} />,
+        global.document.body
+      );
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonClasses = buttonElem.props.className;
+
+      expect(buttonClasses)
+        .toMatch('u-sizeFull');
+    });
+
+    it('should apply className prop to the underlying element', function() {
+      const button = React.render(
+        <Button className='testClass classTest' />,
+        global.document.body
+      );
+
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonClasses = buttonElem.props.className;
+
+      expect(buttonClasses)
+        .toMatch('testClass');
+
+      expect(buttonClasses)
+        .toMatch('classTest');
+    });
+
+    it('should apply style prop to the underlying element', function() {
+      const button = React.render(
+        <Button style={{ testStyle: 'success' }} />,
+        global.document.body
+      );
+
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonStyle = buttonElem.props.style;
+
+      expect(buttonStyle.testStyle)
+        .toEqual('success');
+    });
   });
 
-  it('should have fullscreen classes when fullscreen prop is true', function() {
-    const button = React.render(
-      <Button fullscreen={true} />,
-      global.document.body
-    );
-    const buttonElem = ReactTestUtils
-      .findRenderedDOMComponentWithClass(button, 'c-btn');
-    const buttonClasses = buttonElem.props.className;
+  describe('ButtonSecondary', function() {
+    it('should apply className prop to the underlying element', function() {
+      const button = React.render(
+        <ButtonSecondary className='testClass classTest' />,
+        global.document.body
+      );
 
-    expect(buttonClasses)
-      .toMatch('u-sizeFull');
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonClasses = buttonElem.props.className;
+
+      expect(buttonClasses)
+        .toMatch('testClass');
+
+      expect(buttonClasses)
+        .toMatch('classTest');
+    });
+
+    it('should apply style prop to the underlying element', function() {
+      const button = React.render(
+        <ButtonSecondary style={{ testStyle: 'success' }} />,
+        global.document.body
+      );
+
+      const buttonElem = ReactTestUtils
+        .findRenderedDOMComponentWithClass(button, 'c-btn');
+      const buttonStyle = buttonElem.props.style;
+
+      expect(buttonStyle.testStyle)
+        .toEqual('success');
+    });
   });
-
 });
-
