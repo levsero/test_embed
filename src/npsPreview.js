c94f1c8aaed3b4476522346dfbc780189030b3a4
Ryan Seddon <rseddon@zendesk.com>
2015-08-24 16:15:25 +1000

    move Embed, force setLocale
    - Move Embed component into renderNPS to avoid caching issue
    - Add force argument to setLocale to allow the setting of multiple locales

--- src/npsPreview.js
+++ src/npsPreview.js
@@ -12,18 +12,6 @@ const frameParams = {
   }
 };
 
-const Embed = React.createClass(frameFactory(
-  (params) => {
-    return (
-      <ParentNps
-        style={{width: '375px', margin: '15px' }}
-        updateFrameSize={params.updateFrameSize}
-        ref='rootComponent' />
-    );
-  },
-  frameParams
-));
-
 const ParentNps = React.createClass({
   getInitialState() {
     return {
@@ -31,9 +19,11 @@ const ParentNps = React.createClass({
       survey: {}
     }
   },
+
   componentDidUpdate(props, state) {
     this.refs.nps.setState(this.state);
   },
+
   render() {
     if (this.props.updateFrameSize) {
       setTimeout(() => this.props.updateFrameSize(), 0);
@@ -41,12 +31,24 @@ const ParentNps = React.createClass({
 
     return (this.state.isMobile)
         ? <div ref='nps' style={{background: "red", height: 100, width: 100}} />
-        : <Nps ref='nps' style={this.props.style} />
+        : <Nps ref='nps' style={{width: '375px', margin: '15px' }} />
   }
 });
 
 const renderNPS = (locale = 'en-GB', id) => {
-  i18n.setLocale(locale);
+  i18n.setLocale(locale, true);
+
+  const Embed = React.createClass(frameFactory(
+    (params) => {
+      return (
+        <ParentNps
+          updateFrameSize={params.updateFrameSize}
+          ref='rootComponent' />
+      );
+    },
+    frameParams
+  ));
+
   return React.render(<Embed />, document.querySelector(`.npsPreview_${id}`));
 }
 
