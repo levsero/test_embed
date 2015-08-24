aea2732262048f30c48ad7f6aa31d4c735d1fe3a
Ryan Seddon <rseddon@zendesk.com>
2015-08-24 14:47:52 +1000

    render multiple nps surveys.

--- src/npsPreview.js
+++ src/npsPreview.js
@@ -12,24 +12,38 @@ const frameParams = {
   }
 };
 
-let isMobileBrowser = false;
-
 const Embed = React.createClass(frameFactory(
   (params) => {
     return (
-      (isMobileBrowser)
-        ? <div
-            ref='rootComponent'
-            updateFrameSize={params.updateFrameSize}
-            style={{background: "red", height: 100, width: 100}} />
-        : <Nps
-            ref='rootComponent'
-            updateFrameSize={params.updateFrameSize}
-            style={{width: '375px', margin: '15px' }} />
+      <ParentNps
+        style={{width: '375px', margin: '15px' }}
+        updateFrameSize={params.updateFrameSize}
+        ref='rootComponent' />
     );
   },
   frameParams
 ));
+
+const ParentNps = React.createClass({
+  getInitialState() {
+    return {
+      isMobile: false,
+      survey: {}
+    }
+  },
+  componentDidUpdate(props, state) {
+    this.refs.nps.setState(this.state);
+  },
+  render() {
+    if (this.props.updateFrameSize) {
+      setTimeout(() => this.props.updateFrameSize(), 0);
+    }
+
+    return (this.state.isMobile)
+        ? <div ref='nps' style={{background: "red", height: 100, width: 100}} />
+        : <Nps ref='nps' style={this.props.style} />
+  }
+});
 
 const renderNPS = (locale = 'en-GB', id) => {
   i18n.setLocale(locale);
@@ -37,6 +51,5 @@ const renderNPS = (locale = 'en-GB', id) => {
 }
 
 window.zE = {
-  renderNPS: renderNPS,
-  isMobileBrowser: isMobileBrowser
+  renderNPS: renderNPS
 };
