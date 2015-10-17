39e46a51fe9026c1d2400456a057df4b2fe057f3
Adam Beck <abeck@zendesk.com>
2015-10-17 13:32:17 +1100

    fix failing test

--- test/unit/component/NpsRatingsList_test.js
+++ test/unit/component/NpsRatingsList_test.js
@@ -11,8 +11,8 @@ describe('NpsRatingsList component', () => {
     ratingsRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     npsRatingsListProps = {
-      likelyLabel:'',
-      notLikelyLabel:'',
+      likelyLabel:'10 = Extremely likely',
+      notLikelyLabel:'0 = Not likely',
       ratingsRange:ratingsRange,
       selectedRating: null,
       isSubmittingRating: false,
