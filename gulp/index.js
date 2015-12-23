622010a025c4f7696aa4d14f8070d45eddb01948
Anthony Del Ciotto <adelciotto@zendesk.com>
2015-12-23 14:23:32 +1100

    removed left over gulp tasks; removed gulp and gulp modules

--- gulp/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const onlyScripts = require('./util/scriptFilter');
-const tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
-
-tasks.forEach(function(task) {
-  require('./tasks/' + task);
-});
