ee0d66b9e28fdbe34c4425233fe34735e0a94afa
Anthony Del Ciotto <adelciotto@zendesk.com>
2016-03-15 13:47:37 +1100

    added renew and expiry functionality

--- src/service/authentication.js
+++ src/service/authentication.js
@@ -7,14 +7,44 @@ function init() {
 }
 
 function authenticate(webToken) {
+  authenticateRequest('/embeddable/authenticate', webtoken);
+}
+
+function renew() {
+  const token = store.get('zE_oauth').token;
+
+  authenticateRequest('/embeddable/authenticate/renew', token);
+}
+
+function getToken() {
+  if (isExpired()) {
+    renew();
+  } else {
+    console.log('NOT EXPIRED');
+  }
+
+  return store.get('zE_oauth').token;
+}
+
+function renew() {
+  const token = store.get('zE_oauth').token;
+}
+
+function authenticateRequest(path, params) {
   const payload = {
     method: 'POST',
-    path: '/embeddable/authenticate',
-    params: webToken,
+    path: path,
+    params: params,
     callbacks: {
       done: function(res) {
         if (res.status === 200) {
-          store.set('zE_oauth', { 'token': res.body.oauth_token, 'expiry': res.body.oauth_expiry });
+          store.set(
+            'zE_oauth',
+            {
+              'token': res.body.oauth_token,
+              'expiry': res.body.oauth_expiry
+            }
+          );
         }
       }
     }
@@ -23,8 +53,15 @@ function authenticate(webToken) {
   transport.send(payload);
 }
 
-function getToken() {
-  return store.get('zE_oauth').token;
+function isExpired() {
+  const zeoauth = store.get('zE_oauth');
+  const timeInterval = 1000 * 60 * 10; // 10 mins in ms
+
+  if (zeoauth && zeoauth.expiry) {
+    return Date.now() > zeoauth.expiry - timeInterval;
+  } else {
+    return false;
+  }
 }
 
 export const authentication = {
