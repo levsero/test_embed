8efc5cee0a1f02a9da77617bd3268308681e776d
Briana Coppard <bcoppard@zendesk.com>
2016-08-03 10:46:26 +1000

    remove old describe block

--- test/unit/component/button/ButtonPill_test.js
+++ test/unit/component/button/ButtonPill_test.js
@@ -35,19 +35,17 @@ describe('ButtonPill component', () => {
     mockery.disable();
   });
 
-  describe('ButtonPill', () => {
-    it('should not have is-mobile class when fullscreen is false', () => {
-      const button = shallowRender(<ButtonPill />);
+  it('should not have is-mobile class when fullscreen is false', () => {
+    const button = shallowRender(<ButtonPill />);
 
-      expect(button.props.className)
-        .not.toMatch('is-mobile');
-    });
+    expect(button.props.className)
+      .not.toMatch('is-mobile');
+  });
 
-    it('should have is-mobile class when fullscreen is true', () => {
-      const button = shallowRender(<ButtonPill fullscreen={true} />);
+  it('should have is-mobile class when fullscreen is true', () => {
+    const button = shallowRender(<ButtonPill fullscreen={true} />);
 
-      expect(button.props.className)
-        .toMatch('is-mobile');
-    });
+    expect(button.props.className)
+      .toMatch('is-mobile');
   });
 });
