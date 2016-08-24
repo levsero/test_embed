9df95ff9dc1c1c018ec7bb43f29414c821e385f9
Damen Turnbull <dturnbull@zendesk.com>
2016-08-24 10:25:53 +1000

    Send validation request for ticket using ticket_id and token parameters.

--- src/embed/automaticAnswers/automaticAnswers.js
+++ src/embed/automaticAnswers/automaticAnswers.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
 import { AutomaticAnswers } from 'component/automaticAnswers/AutomaticAnswers';
 import { frameFactory } from 'embed/frameFactory';
 import { getDocumentHost } from 'utility/globals';
+import { transport } from 'service/transport';
+import { getURLParameterByName } from 'utility/pages';
 
 let embed;
+const STATUS_SOLVED = 3;
 
 function create(name, config) {
   const frameParams = {
@@ -38,8 +41,44 @@ function render() {
   embed.instance = ReactDOM.render(embed.component, element);
 }
 
+function postRender() {
+  const ticketId = getURLParameterByName('ticket_id'),
+    token = getURLParameterByName('token');
+
+  if (ticketId && token) {
+    fetchTicketFn(ticketId, token);
+  }
+}
+
+function fetchTicketFn(ticketId, token) {
+  const payload = {
+    path: `/requests/automatic-answers/ticket/${ticketId}/fetch/token/${token}`,
+    method: 'get',
+    callbacks: {
+      done: fetchTicketDone,
+      fail: fetchTicketFail
+    }
+  };
+
+  transport.automaticAnswersApiRequest(payload);
+}
+
+function fetchTicketDone(res) {
+  const ticket = res.body.ticket;
+
+  if (ticket.status_id < STATUS_SOLVED) {
+    // TODO - Pass ticket data to react component;
+    embed.instance.show({});
+  }
+}
+
+function fetchTicketFail() {
+  embed.instance.hide({});
+}
+
 export const automaticAnswers = {
   create: create,
   get: get,
-  render: render
+  render: render,
+  postRender: postRender
 };
