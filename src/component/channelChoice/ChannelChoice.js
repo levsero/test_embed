108a100104ab18e84fbd9e78c67663fa1988388a
Adrian Evans <aevans@zendesk.com>
2016-09-15 18:14:31 +1000

    Initial embed and component for channel choice

---
+++ src/component/channelChoice/ChannelChoice.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+export class ChannelChoice extends Component {
+  render() {
+    return (
+      <div className='ChannelChoice'>
+        <p>Select a channel</p>
+      </div>
+    );
+  }
+}
+
+ChannelChoice.propTypes = {
+};
+
+ChannelChoice.defaultProps = {
+};
+
---
+++ src/embed/channelChoice/channelChoice.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+import { frameFactory } from 'embed/frameFactory';
+import { mediator } from 'service/mediator';
+import { settings } from 'service/settings';
+import { generateUserCSS } from 'utility/color';
+import { isIE,
+         isMobileBrowser } from 'utility/devices';
+
+let channelChoices = {};
+
+function create(name, config) {
+  const frameParams = {
+    name: name
+  };
+
+  const Embed = React.createClass(frameFactory(
+    (params) => {
+      return (
+        <ChannelChoice />
+      );
+    },
+    frameParams
+  ));
+
+  channelChoices[name] = {
+    component: <Embed visible={false} />,
+    config: config
+  };
+
+  return this;
+}
+
+function list() {
+  return channelChoices;
+}
+
+function get(name) {
+  return channelChoices[name];
+}
+
+function getRootComponent(name) {
+  return get(name).instance.getRootComponent();
+}
+
+function waitForRootComponent(name, callback) {
+  if (getRootComponent(name)) {
+    callback();
+  } else {
+    setTimeout(() => {
+      waitForRootComponent(name, callback);
+    }, 0);
+  }
+}
+
+function render(name) {
+  if (channelChoices[name] && channelChoices[name].instance) {
+    throw new Error(`HelpCenter ${name} has already been rendered.`);
+  }
+
+  const element = getDocumentHost().appendChild(document.createElement('div'));
+
+  channelChoices[name].instance = ReactDOM.render(channelChoices[name].component, element);
+}
+
+function postRender(name) {
+  // const config = get(name).config;
+}
+
+export const channelChoice = {
+  create: create,
+  list: list,
+  get: get,
+  render: render,
+  postRender: postRender
+};
