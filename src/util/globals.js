f2b6e96f68f9e8c94ae05b9b0aec3bc2cb0afeea
Daniel Angel-Bradford <locusdelicti@gmail.com>
2016-09-19 14:02:12 +1000

    retire requestAnimationFrame shim

--- src/util/globals.js
+++ src/util/globals.js
@@ -9,25 +9,4 @@ function getDocumentHost() {
   return document.body || document.documentElement;
 }
 
-// Shim rAF for older browsers that either don't have it or need the prefixed version.
-(function() {
-  let lastTime = 0;
-  const vendors = ['webkit', 'moz'];
-
-  for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function(callback) {
-      const currTime = new Date().getTime();
-      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      const id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-}());
-
 export { win, document, navigator, location, getDocumentHost };
