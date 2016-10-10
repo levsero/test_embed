3be6163e5fad37c638afff08b87e6debae11012d
Daniel Angel-Bradford <locusdelicti@gmail.com>
2016-10-10 16:24:02 +1100

    Re-add the manual settings.init call, as not doing so breaks animations

--- src/service/transitionFactory.js
+++ src/service/transitionFactory.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import { isMobileBrowser } from 'utility/devices';
 import { settings } from 'service/settings';
+// TODO: Re-visit the boot process to avoid this
+settings.init();
 
 const applyHiddenState = (frameHeight) => {
   let topPosition = {};
