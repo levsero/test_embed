fc1af51161ad1dad858b58e4911f6550af2c9411
Briana Coppard <bcoppard@zendesk.com>
2016-10-26 09:56:49 +1100

    Finish adding tests

--- test/unit/component/Dropzone_test.js
+++ test/unit/component/Dropzone_test.js
@@ -1,6 +1,5 @@
 describe('Dropzone component', () => {
-  let Dropzone,
-    files = [];
+  let Dropzone;
   const dropzonePath = buildSrcPath('component/Dropzone');
 
   beforeEach(() => {
@@ -18,11 +17,6 @@ describe('Dropzone component', () => {
     mockery.registerAllowable(dropzonePath);
 
     Dropzone = requireUncached(dropzonePath).Dropzone;
-
-    files = [{
-      name: 'file1.pdf',
-      size: 1111
-    }];
   });
 
   afterEach(() => {
@@ -73,6 +67,108 @@ describe('Dropzone component', () => {
         expect(document.querySelectorAll('.bar').length)
           .not.toEqual(0);
       });
+    });
+  });
+
+  describe('onDragEnter', () => {
+    let dropzone;
+
+    beforeEach(() => {
+      dropzone = domRender(<Dropzone onDragEnter={jasmine.createSpy()} />);
+      dropzone.onDragEnter({ preventDefault: noop });
+    });
+
+    it('should increase the enterCounter', () => {
+      expect(dropzone.enterCounter)
+        .toEqual(1);
+    });
+
+    it('should call the onDragEnter prop', () => {
+      expect(dropzone.props.onDragEnter)
+        .toHaveBeenCalled();
+    });
+
+    it('should set dragActive to true', () => {
+      expect(dropzone.state.isDragActive)
+        .toEqual(true);
+    });
+  });
+
+  describe('onDragLeave', () => {
+    let dropzone;
+
+    beforeEach(() => {
+      dropzone = domRender(<Dropzone onDragLeave={jasmine.createSpy()} />);
+      dropzone.onDragEnter({ preventDefault: noop });
+      dropzone.onDragLeave({ preventDefault: noop });
+    });
+
+    it('should decrease the enterCounter', () => {
+      expect(dropzone.enterCounter)
+        .toEqual(0);
+    });
+
+    it('should call the onDragLeave prop', () => {
+      expect(dropzone.props.onDragLeave)
+        .toHaveBeenCalled();
+    });
+
+    it('should set dragActive to false', () => {
+      expect(dropzone.state.isDragActive)
+        .toEqual(false);
+    });
+
+    describe('when there are multiple children entered', () => {
+      beforeEach(() => {
+        dropzone = domRender(<Dropzone onDragLeave={jasmine.createSpy()} />);
+        dropzone.onDragEnter({ preventDefault: noop });
+        dropzone.onDragEnter({ preventDefault: noop });
+        dropzone.onDragLeave({ preventDefault: noop });
+      });
+
+      it('should not call the onDragLeave prop', () => {
+        expect(dropzone.props.onDragLeave)
+          .not.toHaveBeenCalled();
+      });
+
+      it('should not set dragActive to false', () => {
+        expect(dropzone.state.isDragActive)
+          .toEqual(true);
+      });
+    });
+  });
+
+  describe('onDrop', () => {
+    let dropzone, files;
+
+    beforeEach(() => {
+      files = [{
+        name: 'file1.pdf',
+        size: 1111
+      }];
+
+      dropzone = domRender(<Dropzone onDrop={jasmine.createSpy()} />);
+      dropzone.onDrop({
+        preventDefault: noop,
+        dataTransfer: { files: files }
+      });
+    });
+
+    it('should set the enterCounter to 0', () => {
+      expect(dropzone.enterCounter)
+        .toEqual(0);
+    });
+
+    it('should set dragActive to false', () => {
+      expect(dropzone.state.isDragActive)
+        .toEqual(false);
+    });
+
+    it('should call the onDrop prop with the dropped files', () => {
+      expect(dropzone.props.onDrop)
+        .toHaveBeenCalled();
+      expect(dropzone.props.onDrop.calls.mostRecent().args[0])
+        .toEqual(files);
     });
   });
 });
