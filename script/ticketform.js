9dea921b2c7df9ea6910c64931cc81cc63aca925
Briana Coppard <bcoppard@zendesk.com>
2016-11-10 11:42:40 +1100

    es6ify function calls, add missing semicolons, only use backticks when doing string interpolation

--- script/ticketform.js
+++ script/ticketform.js
@@ -2,11 +2,11 @@ const http = require('http');
 const _ = require('lodash');
 
 const fields = [
-  `{ "ticket_field": { "type": "text", "title": "textField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "true" } }`,
-  `{ "ticket_field": { "type": "textarea", "title": "textareaField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }`,
-  `{ "ticket_field": { "type": "checkbox", "title": "checkboxField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }`,
-  `{ "ticket_field": { "type": "integer", "title": "integerField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }`,
-  `{ "ticket_field": { "type": "decimal", "title": "decimalField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }`,
+  '{ "ticket_field": { "type": "text", "title": "textField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "true" } }',
+  '{ "ticket_field": { "type": "textarea", "title": "textareaField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }',
+  '{ "ticket_field": { "type": "checkbox", "title": "checkboxField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }',
+  '{ "ticket_field": { "type": "integer", "title": "integerField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }',
+  '{ "ticket_field": { "type": "decimal", "title": "decimalField", "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }',
   `{ "ticket_field": { "type": "tagger", "title": "taggerField", "custom_field_options": [{ "name": "Option1", "value": "${Math.random()}" }, {"name": "Option2", "value": "${Math.random()}"}], "visible_in_portal": "true", "editable_in_portal": "true", "required_in_portal": "false" } }`,
 ];
 
@@ -24,16 +24,16 @@ function createTicketForm(fieldIds) {
 
   options.path = '/api/v2/ticket_forms.json';
 
-  const ticketFormRequest = http.request(options, function(res) {
+  const ticketFormRequest = http.request(options, (res) => {
     console.log('STATUS: ' + res.statusCode);
     res.setEncoding('utf8');
-    res.on('data', function (data) {
+    res.on('data', (data) => {
       console.log('DONE! ticket form id: ' + JSON.parse(data).ticket_form.id);
     });
   });
 
-  ticketFormRequest.on('error', function(e) {
-    console.log('problem with ticket form request: ' + e.message)
+  ticketFormRequest.on('error', (e) => {
+    console.log('problem with ticket form request: ' + e.message);
   });
 
   ticketFormRequest.write(`{
@@ -57,10 +57,10 @@ function createTicketFields() {
   const fieldIds = [];
 
   _.forEach(fields, (field) => {
-    const ticketFieldsRequest = http.request(options, function(res) {
+    const ticketFieldsRequest = http.request(options, (res) => {
 
       res.setEncoding('utf8');
-      res.on('data', function (data) {
+      res.on('data', (data) => {
         const id = JSON.parse(data).ticket_field.id;
 
         console.log('STATUS: ' + res.statusCode + '  created field: ' + id);
@@ -72,8 +72,8 @@ function createTicketFields() {
       });
     });
 
-    ticketFieldsRequest.on('error', function(e) {
-      console.log('problem with ticket field request: ' + e.message)
+    ticketFieldsRequest.on('error', (e) => {
+      console.log('problem with ticket field request: ' + e.message);
     });
 
     ticketFieldsRequest.write(field);
