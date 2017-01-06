e0e5e0675c6f3d4741cc78383c46c23b2a3b8f49
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-01-06 14:02:16 +1100

    switched to webpack-dev-server; added webpack dashboard

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,16 @@
 var config = require('./webpack.config');
 var path = require('path');
 var webpack = require('webpack');
+var DashboardPlugin = require('webpack-dashboard/plugin');
 
 config.debug = true;
 config.devtool = 'cheap-module-eval-source-map';
+
+config.devServer = {
+  port: 1337,
+  contentBase: path.join(__dirname, 'example'),
+  publicPath: '/dist/'
+};
 
 config.plugins = [
   new webpack.DefinePlugin({
@@ -14,7 +21,8 @@ config.plugins = [
     path.resolve(__dirname, './node_modules/'),
     path.resolve(__dirname, './test/')
   ]),
-  new webpack.NoErrorsPlugin()
+  new webpack.NoErrorsPlugin(),
+  new DashboardPlugin()
 ];
 
 module.exports = config;
