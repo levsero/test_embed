ebf7eb6f5251e190c39c06c2e129009442e2786a
Briana Coppard <bcoppard@zendesk.com>
2017-01-12 14:15:04 +1100

    check if embed is open before activating

--- src/embed/webWidget/webWidget.js
+++ src/embed/webWidget/webWidget.js
@@ -321,6 +321,8 @@ function setUpMediator() {
   });
 
   mediator.channel.subscribe('webWidget.activate', () => {
+    if (embed.instance.state.visible) return;
+
     waitForRootComponent(() => {
       getWebWidgetComponent().activate();
       embed.instance.show();
