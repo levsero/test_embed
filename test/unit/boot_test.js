08412604aceb5eaa6c583f886327f338d68137f3
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-03-10 11:20:52 +1100

    finishing getConfig test

--- test/unit/boot_test.js
+++ test/unit/boot_test.js
@@ -17,7 +17,7 @@ describe('boot', () => {
       'service/beacon': registerImportSpy('beacon', 'sendPageView', 'trackSettings', 'sendConfigLoadTime'),
       'service/i18n': registerImportSpy('i18n', 'init', 'setLocale'),
       'service/identity': registerImportSpy('identity', 'init'),
-      'service/logging': registerImportSpy('logging', 'init'),
+      'service/logging': registerImportSpy('logging', 'init', 'error'),
       'service/settings': {
         settings: {
           get: noop
@@ -49,12 +49,15 @@ describe('boot', () => {
   describe('getConfig', () => {
     let win,
       postRenderQueue,
-      transportSpy;
+      transportSpy,
+      mockGetCalls;
 
     beforeEach(() => {
       win = {};
       postRenderQueue = [];
+
       transportSpy = mockRegistry['service/transport'].transport;
+      mockGetCalls = transportSpy.get.calls;
     });
 
     it('makes a GET request to /embeddable/config', () => {
@@ -74,11 +77,75 @@ describe('boot', () => {
     });
 
     describe('when the request succeeds', () => {
+      it('calls beacon.setConfig with the config', () => {
 
+      });
+
+      it('calls beacon.sendPageView', () => {
+
+      });
+
+      it('calls renderer.init with the config', () => {
+
+      });
+
+      it('calls handlePostRenderQueue', () => {
+
+      });
+
+      describe('when one in ten times', () => {
+        it('should call beacon.sendConfigLoadTime with the load time', () => {
+
+        });
+      });
+
+      describe('when win.zESettings is defined', () => {
+        it('should call beacon.trackSettings', () => {
+
+        });
+      });
     });
 
     describe('when the request fails', () => {
+      let failHandler,
+        loggingSpy;
 
+      beforeEach(() => {
+        getConfig(win, postRenderQueue);
+
+        failHandler = mockGetCalls.mostRecent().args[0].callbacks.fail;
+        loggingSpy = mockRegistry['service/logging'].logging;
+      });
+
+      describe('when the error status code is 404', () => {
+        beforeEach(() => {
+          failHandler({ status: 404 });
+        });
+
+        it('should not call logging.error', () => {
+          expect(loggingSpy.error)
+            .not.toHaveBeenCalled();
+        });
+      });
+
+      describe('when the error status code is not 404', () => {
+        const error = { status: 500 };
+
+        beforeEach(() => {
+          document.zendeskHost = 'pizza.zendesk.com';
+          failHandler(error);
+        });
+
+        it('should call logging.error', () => {
+          expect(loggingSpy.error)
+            .toHaveBeenCalledWith({
+              error,
+              context: {
+                account: 'pizza.zendesk.com'
+              }
+            });
+        });
+      });
     });
   });
 });
