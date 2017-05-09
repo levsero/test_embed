31ad77902ebc74d23e852419fa19c23ceddb0c1a
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-05-09 13:56:50 +1000

    added csp example file and instructions

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,9 @@ var root = config.root;
 
 root.devtool = 'cheap-module-eval-source-map';
 
+// Enable this for testing CSP.
+// root.devtool = 'source-map';
+
 root.devServer = {
   port: 1337,
   contentBase: 'example',
