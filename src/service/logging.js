c778aa0d5519ac418d0472cb604bd74a8c53730d
Terence Liew <tliew@zendesk.com>
2017-06-01 12:26:24 +1000

    Blacklist unknown script error

--- src/service/logging.js
+++ src/service/logging.js
@@ -9,7 +9,8 @@ let rollbar;
 let useRollbar;
 const errorMessageBlacklist = [
   'Access-Control-Allow-Origin',
-  'timeout of [0-9]+ms exceeded'
+  'timeout of [0-9]+ms exceeded',
+  '^(\(unknown\): )?(Script error).?$'
 ];
 const rollbarConfig =  {
   accessToken: '94eb0137fdc14471b21b34c5a04f9359',
