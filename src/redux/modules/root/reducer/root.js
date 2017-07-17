bf9b80e3ca5bb7af4ed0d63f51d4b86a9e7578be
Briana Coppard <bcoppard@zendesk.com>
2017-07-17 14:43:20 +1000

    remove root actions for now, will add when we need

--- src/redux/modules/root/reducer/root.js
+++ src/redux/modules/root/reducer/root.js
@@ -1,12 +1,5 @@
-import { BACK_CLICK } from '../root-action-types';
-
 const rootReducer = (state = {}, action) => {
   switch (action.type) {
-    case BACK_CLICK: {
-      console.log(state);
-
-      return state;
-    }
     default: {
       return state;
     }
--- src/redux/modules/root/root-actions.js
+++
@@ -1,9 +0,0 @@
-import {
-  BACK_CLICK,
-  NEXT_CLICK,
-  CANCEL_CLICK
-} from './root-action-types';
-
-export const onBackClick = () => {
-  return { type: BACK_CLICK };
-};
