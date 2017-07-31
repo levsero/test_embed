b5a624336f369f80d2d584d943ec08654dcb03e8
Briana Coppard <bcoppard@zendesk.com>
2017-07-31 16:41:27 +1000

    Add all fields

--- src/component/chat/ChatPrechatForm.js
+++ src/component/chat/ChatPrechatForm.js
@@ -25,14 +25,33 @@ export class ChatPrechatForm extends Component {
 
   render = () => {
     return (
-      <div className={styles.input}>
+      <form
+        noValidate={true}
+        className={`${styles.form}`}>
         <Field
-          name='chatBox' />
+          placeholder={i18n.t('embeddable_framework.common.textLabel.name', { fallback: 'Your name' })}
+          required={true}
+          name='name' />
+        <Field
+          placeholder={i18n.t('embeddable_framework.common.textLabel.email', { fallback: 'Email' })}
+          required={true}
+          pattern="[a-zA-Z0-9!#$%&'*+/=?^_`{|}~\-`']+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~\-`']+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?" // eslint-disable-line
+          name='email' />
+        <Field
+          placeholder={i18n.t('embeddable_framework.common.textLabel.phoneNumber', { fallback: 'Phone Number' })}
+          required={true}
+          type='number'
+          name='phone' />
+        <Field
+          placeholder={i18n.t('embeddable_framework.common.textLabel.message', { fallback: 'Message' })}
+          required={true}
+          input={<textarea rows='3' />}
+          name='message' />
         <Button
           label={i18n.t('embeddable_framework.chat.preChat.online.button.startChat')}
-          disabled={false}
-          onClick={this.handleButtonClick} />
-      </div>
+          disabled={!this.state.valid}
+          type='submit' />
+      </form>
     );
   }
 }
