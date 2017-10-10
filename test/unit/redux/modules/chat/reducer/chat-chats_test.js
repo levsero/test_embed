4f86a371ae9f9b5a4fa6b3969fac6f0a6ed76b3f
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-10-10 15:09:37 +1100

    added test for incoming message sound playing

--- test/unit/redux/modules/chat/reducer/chat-chats_test.js
+++ test/unit/redux/modules/chat/reducer/chat-chats_test.js
@@ -3,13 +3,22 @@ import * as chatActionTypes from '../../../../../../src/redux/modules/chat/chat-
 describe('chat reducer chats', () => {
   let reducer,
     actionTypes,
-    initialState;
+    initialState,
+    playSoundSpy;
 
   beforeAll(() => {
     mockery.enable();
 
     const reducerPath = buildSrcPath('redux/modules/chat/reducer/chat-chats');
     const actionTypesPath = buildSrcPath('redux/modules/chat/chat-action-types');
+
+    playSoundSpy = jasmine.createSpy('playSound');
+
+    initMockRegistry({
+      'service/audio': {
+        audio: { playSound: playSoundSpy }
+      }
+    });
 
     reducer = requireUncached(reducerPath).default;
     actionTypes = requireUncached(actionTypesPath);
@@ -61,22 +70,48 @@ describe('chat reducer chats', () => {
     });
 
     describe('chat SDK actions', () => {
+      let state,
+        detail;
       const sdkActionTypes = [
         chatActionTypes.SDK_CHAT_FILE,
         chatActionTypes.SDK_CHAT_WAIT_QUEUE,
         chatActionTypes.SDK_CHAT_REQUEST_RATING,
         chatActionTypes.SDK_CHAT_RATING,
         chatActionTypes.SDK_CHAT_COMMENT,
-        chatActionTypes.SDK_CHAT_MSG,
         chatActionTypes.SDK_CHAT_MEMBER_JOIN,
         chatActionTypes.SDK_CHAT_MEMBER_LEAVE
       ];
 
+      describe('when a SDK_CHAT_MSG action is dispatched', () => {
+        beforeEach(() => {
+          detail = {
+            timestamp: Date.now(),
+            nick: 'visitor:x',
+            display_name: 'Mr X'
+          };
+
+          state = reducer(initialState, {
+            type: chatActionTypes.SDK_CHAT_MSG,
+            payload: { detail: detail }
+          });
+        });
+
+        it('plays the incoming_message sound', () => {
+          expect(playSoundSpy)
+            .toHaveBeenCalledWith('incoming_message');
+        });
+
+        it('adds the message to the chats collection', () => {
+          expect(state.size)
+            .toEqual(1);
+
+          expect(state.get(detail.timestamp))
+            .toEqual(jasmine.objectContaining(detail));
+        });
+      });
+
       sdkActionTypes.forEach((actionType) => {
         describe(`when a ${actionType} action is dispatched`, () => {
-          let state,
-            detail;
-
           beforeEach(() => {
             detail = {
               timestamp: Date.now(),
