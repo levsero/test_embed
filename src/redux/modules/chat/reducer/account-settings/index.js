073774150bb04c0de8567cd99a0ede5aac794b65
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-10-12 09:33:02 +1100

    added prechat-form reducer

--- src/redux/modules/chat/reducer/account-settings/index.js
+++ src/redux/modules/chat/reducer/account-settings/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import concierge from './concierge';
+import preChatForm from './prechat-form';
 
 export default combineReducers({
-  concierge
+  concierge,
+  preChatForm
 });
 
---
+++ src/redux/modules/chat/reducer/account-settings/prechat-form.js
@@ -0,0 +1,19 @@
+import { UPDATE_ACCOUNT_SETTINGS } from '../../chat-action-types';
+
+const initialState = {
+  form: {},
+  message: '',
+  profile_required: false,
+  required: true
+};
+
+const prechatForm = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_ACCOUNT_SETTINGS:
+      return action.payload.forms.pre_chat_form;
+    default:
+      return state;
+  }
+};
+
+export default prechatForm;
