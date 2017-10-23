5259a79ec1bf097fca8c2a766fb4ea8ee0a71af8
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-10-23 15:09:30 +1100

    stop using MessageBubble component; use translation for title

--- src/component/chat/ChatReconnectionBubble.js
+++ src/component/chat/ChatReconnectionBubble.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-import { MessageBubble } from './MessageBubble';
+import { LoadingSpinner } from 'component/loading/LoadingSpinner';
+import { i18n } from 'service/i18n';
+
 import { locals as styles } from './ChatReconnectionBubble.sass';
 
 export class ChatReconnectionBubble extends Component {
   render = () => {
+    const message = i18n.t(
+      'embeddable_framework.chat.reconnecting.label',
+      { fallback: 'Reconnecting' }
+    );
+
     return (
       <div className={styles.container}>
-        <MessageBubble className={styles.bubble} message='Reconnecting' />
+        <div className={styles.bubble}>
+          <div className={styles.contentContainer}>
+            <div className={styles.title}>{message}</div>
+            <LoadingSpinner className={styles.loadingSpinner} circleClasses={styles.circle} />
+          </div>
+        </div>
       </div>
     );
   }
