b733cb50c00214dc4e640176d93002bbbb604b71
Wayne See <wsee@zendesk.com>
2017-10-31 13:33:02 +1100

    move LoadingEllipses style to web widget style

--- src/embed/sharedStyles.js
+++ src/embed/sharedStyles.js
@@ -1,6 +1,5 @@
 import CheckboxStyles from 'component/field/Checkbox.sass';
 import LoadingSpinnerStyles from 'component/loading/LoadingSpinner.sass';
-import LoadingEllipsesStyles from 'component/loading/LoadingEllipses.sass';
 import ScrollContainerStyles from 'component/container/ScrollContainer.sass';
 import AvatarStyles from 'component/Avatar.sass';
 import ButtonStyles from 'component/button/Button.sass';
@@ -8,7 +7,6 @@ import ButtonStyles from 'component/button/Button.sass';
 export const sharedStyles = `
   ${CheckboxStyles}
   ${LoadingSpinnerStyles}
-  ${LoadingEllipsesStyles}
   ${ScrollContainerStyles}
   ${AvatarStyles}
   ${ButtonStyles}
