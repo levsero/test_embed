839954885fdcf59e551f04bd595280c64c6f8d29
Wayne See <wsee@zendesk.com>
2017-11-27 09:49:56 +1100

    Remove Flag.sass from webWidgetStyles
    Since it's not used yet let's remove it from the shared styles
    first. We can revert this commit when it's used.

--- src/embed/webWidget/webWidgetStyles.js
+++ src/embed/webWidget/webWidgetStyles.js
@@ -26,7 +26,6 @@ import ContainerStyles from 'component/container/Container.sass';
 import DropdownStyles from 'component/field/Dropdown.sass';
 import DropdownOptionStyles from 'component/field/DropdownOption.sass';
 import FieldStyles from 'component/field/Field.sass';
-import FlagStyles from 'component/Flag.sass';
 import HelpCenterArticleStyles from 'component/helpCenter/HelpCenterArticle.sass';
 import HelpCenterDesktopStyles from 'component/helpCenter/HelpCenterDesktop.sass';
 import HelpCenterMobileStyles from 'component/helpCenter/HelpCenterMobile.sass';
@@ -73,7 +72,6 @@ export const webWidgetStyles = `
   ${DropdownStyles}
   ${DropdownOptionStyles}
   ${FieldStyles}
-  ${FlagStyles}
   ${HelpCenterArticleStyles}
   ${HelpCenterDesktopStyles}
   ${HelpCenterMobileStyles}
