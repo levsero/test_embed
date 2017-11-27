5598b052e4652d91f72107006a2d39523183408b
Anthony Del Ciotto <adelciotto@zendesk.com>
2017-11-27 14:47:05 +1100

    Use env var for aws region

--- config/deploy/ac_deploy.rb
+++ config/deploy/ac_deploy.rb
@@ -2,23 +2,23 @@ require 'json'
 require 'jwt'
 require_relative './s3_deployer'
 
-set :version, fetch(:branch) || fetch(:local_head_revision)
-set :deploy_files, [
-  'web_widget.js',
-  'manifest.json',
-  'ze_translations.js',
-  'ze_localeIdMap.js'
-]
-
-set :aws_credentials, Aws::Credentials.new(ENV['WEB_WIDGET_AWS_ACCESS_KEY'], ENV['WEB_WIDGET_AWS_SECRET_KEY'])
-set :aws_region, ENV['STATIC_ASSETS_AWS_REGION']
-set :s3_release_directory, "web_widget/#{fetch(:version)}"
-set :s3_bucket_name, ENV['STATIC_ASSETS_AWS_BUCKET_NAME']
-set :static_assets_domain, ENV['STATIC_ASSETS_DOMAIN']
-set :ekr_base_url, ENV['EKR_BASE_URL']
-set :ekr_jwt_secret, ENV['EKR_RW_JWT_SECRET']
-
 namespace :ac_embeddable_framework do
+  set :version, fetch(:branch) || fetch(:local_head_revision)
+  set :deploy_files, [
+    'web_widget.js',
+    'manifest.json',
+    'ze_translations.js',
+    'ze_localeIdMap.js'
+  ]
+
+  set :aws_credentials, Aws::Credentials.new(ENV['WEB_WIDGET_AWS_ACCESS_KEY'], ENV['WEB_WIDGET_AWS_SECRET_KEY'])
+  set :aws_region, ENV['STATIC_ASSETS_AWS_REGION']
+  set :s3_release_directory, "web_widget/#{fetch(:version)}"
+  set :s3_bucket_name, ENV['STATIC_ASSETS_AWS_BUCKET_NAME']
+  set :static_assets_domain, ENV['STATIC_ASSETS_DOMAIN']
+  set :ekr_base_url, ENV['EKR_BASE_URL']
+  set :ekr_jwt_secret, ENV['EKR_RW_JWT_SECRET']
+
   desc 'Build framework ac assets'
   task :build_assets do
     logger.info 'Building ac assets'
