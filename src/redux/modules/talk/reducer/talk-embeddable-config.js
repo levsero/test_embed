d18dd5cf7bb80b3c638c525cbf59affe597bbea0
adelciotto <adelciotto@zendesk.com>
2017-12-06 19:58:41 +1100

    Map capability to talk screen

--- src/redux/modules/talk/reducer/talk-embeddable-config.js
+++ src/redux/modules/talk/reducer/talk-embeddable-config.js
@@ -1,4 +1,8 @@
 import { TALK_EMBEDDABLE_CONFIG } from '../talk-action-types';
+import {
+  CALLBACK_ONLY_SCREEN,
+  PHONE_ONLY_SCREEN,
+  CALLBACK_AND_PHONE_SCREEN } from 'src/redux/modules/talk/talk-screen-types';
 
 const initialState = {
   averageWaitTimeSetting: null,
@@ -9,11 +13,18 @@ const initialState = {
   phoneNumber: '',
   supportedCountries: []
 };
+const capabilityScreenMap = {
+  '0': CALLBACK_ONLY_SCREEN,
+  '1': PHONE_ONLY_SCREEN,
+  '2': CALLBACK_AND_PHONE_SCREEN
+};
 
 const embeddableConfig = (state = initialState, action) => {
   switch (action.type) {
     case TALK_EMBEDDABLE_CONFIG:
-      return action.payload;
+      const { payload } = action;
+
+      return { ...payload, capability: capabilityScreenMap[payload.capability] };
     default:
       return state;
   }
