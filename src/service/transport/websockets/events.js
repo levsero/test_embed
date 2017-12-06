e13ecf12d995595d2a59a85c12dcc247d8ad4d9a
adelciotto <adelciotto@zendesk.com>
2017-12-06 20:01:11 +1100

    Reset talk screen on embeddableConfig socket event

--- src/service/transport/websockets/events.js
+++ src/service/transport/websockets/events.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
 
-const socketTalkEmbeddableConfigEvent = 'socket.embeddableConfig';
-const socketTalkAgentAvailability = 'socket.availability';
-const socketTalkAverageWaitTime = 'socket.waitTimeChange';
+import {
+  TALK_EMBEDDABLE_CONFIG,
+  TALK_AGENT_AVAILABILITY,
+  TALK_AVERAGE_WAIT_TIME } from 'src/redux/modules/talk/talk-action-types';
+import { resetTalkScreen } from 'src/redux/modules/talk/talk-actions';
 
 export function talkEmbeddableConfigEventToAction(socket, reduxStore) {
-  socket.on(socketTalkEmbeddableConfigEvent, (config) => {
-    const configActionType = `talk/${socketTalkEmbeddableConfigEvent}`;
-    const availabilityActionType = `talk/${socketTalkAgentAvailability}`;
-    const averageWaitTimeActionType = `talk/${socketTalkAverageWaitTime}`;
-
-    reduxStore.dispatch({ type: configActionType, payload: _.omit(config, ['agentAvailability', 'averageWaitTime']) });
-    reduxStore.dispatch({ type: availabilityActionType, payload: config.agentAvailability });
+  socket.on('socket.embeddableConfig', (config) => {
+    reduxStore.dispatch({
+      type: TALK_EMBEDDABLE_CONFIG,
+      payload: _.omit(config, ['agentAvailability', 'averageWaitTime'])
+    });
+    reduxStore.dispatch({ type: TALK_AGENT_AVAILABILITY, payload: config.agentAvailability });
 
     // Right now the talk service returns these two values as null if the average wait time is enabled
     // in the talk widget admin. TODO: Consult with the foxes team and clean this up.
     if (config.hasOwnProperty('averageWaitTime') && config.hasOwnProperty('averageWaitTimeSetting')) {
-      reduxStore.dispatch({ type: averageWaitTimeActionType, payload: config.averageWaitTime });
+      reduxStore.dispatch({ type: TALK_AVERAGE_WAIT_TIME, payload: config.averageWaitTime });
     }
+
+    reduxStore.dispatch(resetTalkScreen());
   });
 }
 
 export function talkAgentAvailabilityEventToAction(socket, reduxStore) {
-  socket.on(socketTalkAgentAvailability, (availability) => {
-    const type = `talk/${socketTalkAgentAvailability}`;
-
-    reduxStore.dispatch({ type, payload: availability.agentAvailability });
+  socket.on('socket.availability', (availability) => {
+    reduxStore.dispatch({ type: TALK_AGENT_AVAILABILITY, payload: availability.agentAvailability });
   });
 }
 
 export function talkAverageWaitTimeEventToAction(socket, reduxStore) {
-  socket.on(socketTalkAverageWaitTime, (waitTime) => {
-    const type = `talk/${socketTalkAverageWaitTime}`;
-
-    reduxStore.dispatch({ type, payload: waitTime });
+  socket.on('socket.waitTimeChange', (waitTime) => {
+    reduxStore.dispatch({ type: TALK_AVERAGE_WAIT_TIME, payload: waitTime });
   });
 }
