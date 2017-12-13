9e4e2e471f4e8b5dc6b0459ed7a7ab19c6ade7bd
adelciotto <adelciotto@zendesk.com>
2017-12-13 16:15:54 +1100

    Fix agentAvailability typo

--- src/redux/modules/talk/reducer/index.js
+++ src/redux/modules/talk/reducer/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 import embeddableConfig from './talk-embeddable-config';
-import agentAvailbility from './talk-agent-availability';
+import agentAvailability from './talk-agent-availability';
 import screen from './talk-screen';
 import formState from './talk-form-state';
 import callback from './talk-callback';
@@ -9,7 +9,7 @@ import averageWaitTime from './talk-average-wait-time';
 
 export default combineReducers({
   embeddableConfig,
-  agentAvailbility,
+  agentAvailability,
   screen,
   formState,
   callback,
