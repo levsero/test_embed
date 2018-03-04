fceb8230e193eb453d9d602cf355ab66a8a8bb67
Terence Liew <tliew@zendesk.com>
2018-03-04 19:31:26 +1100

    Add new theme sub-state to acccount_settings and adjust relevant references

--- src/redux/modules/chat/reducer/account-settings/index.js
+++ src/redux/modules/chat/reducer/account-settings/index.js
@@ -5,12 +5,14 @@ import concierge from './concierge';
 import prechatForm from './prechat-form';
 import postchatForm from './postchat-form';
 import rating from './rating';
+import theme from './theme';
 
 export default combineReducers({
   attachments,
   concierge,
   prechatForm,
   postchatForm,
-  rating
+  rating,
+  theme
 });
 
---
+++ test/unit/redux/modules/chat/reducer/account-settings/theme_test.js
@@ -0,0 +1,59 @@
+describe('chat reducer accountSettings theme', () => {
+  let reducer,
+    actionTypes,
+    initialState;
+
+  beforeAll(() => {
+    mockery.enable();
+
+    const reducerPath = buildSrcPath('redux/modules/chat/reducer/account-settings/theme');
+    const actionTypesPath = buildSrcPath('redux/modules/chat/chat-action-types');
+
+    reducer = requireUncached(reducerPath).default;
+    actionTypes = requireUncached(actionTypesPath);
+
+    initialState = reducer(undefined, { type: '' });
+  });
+
+  afterAll(() => {
+    mockery.disable();
+    mockery.deregisterAll();
+  });
+
+  describe('reducer', () => {
+    let state;
+
+    describe('initial state', () => {
+      it('sets the initial state to an empty with an empty message_type property', () => {
+        const expected = { message_type: '' };
+
+        expect(initialState)
+          .toEqual(expected);
+      });
+    });
+
+    describe('when a GET_ACCOUNT_SETTINGS_REQUEST_SUCCESS action is dispatched', () => {
+      beforeEach(() => {
+        const mockSettings = {
+          theme: {
+            message_type: 'bubble_avatar'
+          }
+        };
+
+        state = reducer(initialState, {
+          type: actionTypes.GET_ACCOUNT_SETTINGS_REQUEST_SUCCESS,
+          payload: mockSettings
+        });
+      });
+
+      it('sets the action payload as the state', () => {
+        const expected = {
+          message_type: 'bubble_avatar'
+        };
+
+        expect(state)
+          .toEqual(expected);
+      });
+    });
+  });
+});
