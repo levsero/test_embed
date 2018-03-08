fb50a4fa79cdb9fa55ee055c350ee88bfa24aa76
Apoorv Kansal <akansal@zendesk.com>
2018-03-08 11:36:09 +1100

    Refactor MessageBubble with options integration

---
+++ src/component/shared/MessageBubble/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { locals as styles } from './MessageBubble.scss';
+
+import { MessageOptions }  from 'component/shared/MessageOptions';
+
+export class MessageBubble extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    message: PropTypes.string,
+    options: PropTypes.array,
+    sendMsgFn: PropTypes.func
+  };
+
+  static defaultProps = {
+    options: [],
+    sendMsgFn: () => {}
+  }
+
+  renderOptions = () => {
+    const { options } = this.props;
+
+    if (!options.length) return;
+
+    const optionItems = options.map((option, index) => {
+      return (
+        <a key={index} onClick={() => this.props.sendMsgFn(option)}>
+          {option}
+        </a>
+      );
+    });
+
+    return (
+      <MessageOptions isMessageBubbleLinked={true} optionItems={optionItems} />
+    );
+  }
+
+  render() {
+    const messageBubbleStyle = this.props.options.length ? styles.messageBubbleWithOptions : styles.messageBubble;
+
+    return (
+      <div>
+        <div className={`${messageBubbleStyle} ${this.props.className}`}>
+          {this.props.message}
+        </div>
+        {this.renderOptions()}
+      </div>
+    );
+  }
+}
