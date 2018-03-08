bd1657ea9e1c4560ac45fb9d5bb11647251191da
Briana Coppard <bcoppard@zendesk.com>
2018-03-08 16:51:41 +1100

    Handle back button visibility for now notification

--- test/unit/redux/modules/base/reducer/base-back-button_test.js
+++ test/unit/redux/modules/base/reducer/base-back-button_test.js
@@ -2,16 +2,21 @@ describe('base reducer back button', () => {
   let reducer,
     actionTypes,
     getArticleSuccess,
+    ticketSubmissionSuccess,
     initialState;
 
   beforeAll(() => {
     mockery.enable();
 
     getArticleSuccess = 'getArticleSuccess';
+    ticketSubmissionSuccess = 'ticketSubmissionSuccess';
 
     initMockRegistry({
       'src/redux/modules/helpCenter/helpCenter-action-types': {
         GET_ARTICLE_REQUEST_SUCCESS: getArticleSuccess
+      },
+      'src/redux/modules/submitTicket/submitTicket-action-types': {
+        TICKET_SUBMISSION_REQUEST_SUCCESS: ticketSubmissionSuccess
       }
     });
 
@@ -58,6 +63,21 @@ describe('base reducer back button', () => {
     beforeEach(() => {
       state = reducer(initialState, {
         type: getArticleSuccess
+      });
+    });
+
+    it('sets state to false', () => {
+      expect(state)
+        .toEqual(false);
+    });
+  });
+
+  describe(`when an TICKET_SUBMISSION_REQUEST_SUCCESS action is dispatched`, () => {
+    let state;
+
+    beforeEach(() => {
+      state = reducer(initialState, {
+        type: ticketSubmissionSuccess
       });
     });
 
