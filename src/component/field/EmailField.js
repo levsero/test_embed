c70cf93ddcaf68b614b9f75ac6ebd88329dfc595
Terence Liew <tliew@zendesk.com>
2018-03-19 04:52:47 +1100

    Implement different screen states for ChatContactDetailsPopup

--- src/component/field/EmailField.js
+++ src/component/field/EmailField.js
@@ -11,7 +11,8 @@ export class EmailField extends Component {
     label: PropTypes.string,
     required: PropTypes.bool,
     fieldContainerClasses: PropTypes.string,
-    fieldClasses: PropTypes.string
+    fieldClasses: PropTypes.string,
+    placeholder: PropTypes.string
   };
 
   static defaultProps = {
@@ -19,7 +20,8 @@ export class EmailField extends Component {
     required: false,
     value: '',
     fieldContainerClasses: '',
-    fieldClasses: ''
+    fieldClasses: '',
+    placeholder: ''
   };
 
   render = () => {
@@ -28,6 +30,7 @@ export class EmailField extends Component {
         label={this.props.label}
         required={this.props.required}
         value={this.props.value}
+        placeholder={this.props.placeholder}
         name='email'
         pattern="[a-zA-Z0-9!#$%&'*+/=?^_`{|}~\-`']+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~\-`']+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?" // eslint-disable-line
         fieldContainerClasses={this.props.fieldContainerClasses}
