bd48f42ea64920adf7f5073fce143009972d2a65
Apoorv Kansal <akansal@zendesk.com>
2018-03-26 16:21:38 +1100

    Add agentJoined reducer

---
+++ src/redux/modules/chat/reducer/chat-agent-joined.js
@@ -0,0 +1,23 @@
+import {
+  SDK_CHAT_MEMBER_JOIN,
+  END_CHAT_REQUEST_SUCCESS } from '../chat-action-types';
+
+const initialState = false;
+
+const isAgent = (nick) => nick.indexOf('agent:') > -1;
+
+const agentJoined = (state = initialState, action) => {
+  switch (action.type) {
+    case SDK_CHAT_MEMBER_JOIN:
+      if (isAgent(action.payload.detail.nick)) {
+        return true;
+      }
+      return state;
+    case END_CHAT_REQUEST_SUCCESS:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default agentJoined;
