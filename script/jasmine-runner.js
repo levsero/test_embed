eb7ddd65e4a50eb884c125e73222bc5b22e5922b
Adrian Evans <aevans@zendesk.com>
2018-04-09 12:48:38 +1000

    Reduce amount of test output on travis

--- script/jasmine-runner.js
+++ script/jasmine-runner.js
@@ -2,8 +2,11 @@ const Jasmine = require('jasmine');
 const SpecReporter = require('jasmine-spec-reporter');
 
 const jRunner = new Jasmine();
+const env = process.env.NODE_ENV;
 
-jRunner.configureDefaultReporter({ print: function(){} });
-jasmine.getEnv().addReporter(new SpecReporter());
+if (env !== 'test') {
+  jRunner.configureDefaultReporter({ print: () => {} });
+  jRunner.addReporter(new SpecReporter());
+}
 jRunner.loadConfigFile('test/jasmine.json');
 jRunner.execute();
