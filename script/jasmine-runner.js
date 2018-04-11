5efc6270d0b44ac07bc3d281b61b806784f7409e
Adrian Evans <aevans@zendesk.com>
2018-04-11 11:22:08 +1000

    Add comment for NODE_ENV value

--- script/jasmine-runner.js
+++ script/jasmine-runner.js
@@ -4,6 +4,8 @@ const SpecReporter = require('jasmine-spec-reporter');
 const jRunner = new Jasmine();
 const env = process.env.NODE_ENV;
 
+// NODE_ENV value set by test:ci npm task for use on Travis - matches Travis defaults:
+// https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 if (env !== 'test') {
   jRunner.configureDefaultReporter({ print: () => {} });
   jRunner.addReporter(new SpecReporter());
