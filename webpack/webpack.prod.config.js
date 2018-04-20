fb98e2fb86f4a749f7326bca3496e644af07495a
Adrian Evans <aevans@zendesk.com>
2018-04-20 16:50:36 +1000

    Fix build:prod task

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,7 +1,20 @@
 var config = require('./webpack.config');
 var webpack = require('webpack');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var root = config.root;
+
+root.mode = 'production';
+
+root.optimization = {
+  minimizer: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        comments: false
+      }
+    })
+  ]
+}
 
 root.plugins = [
   new webpack.DefinePlugin({
@@ -9,7 +22,6 @@ root.plugins = [
     __DEV__: JSON.stringify(false),
     'process.env.NODE_ENV': JSON.stringify('production')
   }),
-  new webpack.optimize.UglifyJsPlugin({ comments: false }),
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false
