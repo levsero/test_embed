855067af15d10db6c05211b2a5054e2ab2c7a5c6
Terence Liew <tliew@zendesk.com>
2018-06-27 11:15:23 +1000

    Add index reducer test for helpCenter

---
+++ test/unit/redux/modules/helpCenter/reducer/index_test.js
@@ -0,0 +1,49 @@
+describe('helpCenter root reducer', () => {
+  let reducer;
+
+  beforeAll(() => {
+    mockery.enable();
+
+    const reducerPath = buildSrcPath('redux/modules/helpCenter/reducer/index');
+
+    reducer = requireUncached(reducerPath).default;
+  });
+
+  afterAll(() => {
+    mockery.disable();
+    mockery.deregisterAll();
+  });
+
+  describe('initial state', () => {
+    let state;
+
+    beforeEach(() => {
+      state = reducer({}, { type: '' });
+    });
+
+    const subStateList = [
+      'activeArticle',
+      'searchLoading',
+      'articleClicked',
+      'searchFailed',
+      'searchTerm',
+      'articles',
+      'resultsCount',
+      'totalUserSearches',
+      'restrictedImages',
+      'channelChoiceShown',
+      'searchFieldValue',
+      'searchFieldFocused',
+      'articleDisplayed',
+      'contextualSearch'
+    ];
+
+    it('has the expected sub states', () => {
+      _.keys(state).forEach((subState) => {
+        if (!subStateList.includes(subState)) {
+          fail(`${subState} sub state is missing`);
+        }
+      });
+    });
+  });
+});
