2c911d61d9c98cf59aa085f20f6e90a672b11954
Briana Coppard <bcoppard@zendesk.com>
2018-07-05 13:40:39 +1000

    Apply some style overrides to dropdown

--- src/component/frame/gardenOverrides.js
+++ src/component/frame/gardenOverrides.js
@@ -1,8 +1,11 @@
-import { zdColorAluminum } from '@zendeskgarden/css-variables';
+import { zdColorAluminum, zdColorPlatinum } from '@zendeskgarden/css-variables';
 import { FONT_SIZE } from 'constants/shared';
+import { css } from 'styled-components';
+
+/* eslint max-len: 0 */
 
 export default {
-  'textfields.textarea': `
+  'textfields.textarea': css`
     :hover, :focus {
       border-color: ${zdColorAluminum} !important;
     }
@@ -11,7 +14,7 @@ export default {
       box-shadow: 0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important;
     }
   `,
-  'textfields.input': `
+  'textfields.input': css`
     :hover, :focus {
       border-color: ${zdColorAluminum} !important;
     }
@@ -19,5 +22,24 @@ export default {
     :focus {
       box-shadow: 0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important;
     }
+  `,
+  'select.select_view': css`
+    :hover, :focus {
+      border-color: ${zdColorAluminum} !important;
+    }
+
+    :focus {
+      box-shadow: 0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important;
+    }
+    box-shadow: ${props => props.focused && `0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important`}
+    border-color: ${props => props.focused && `${zdColorAluminum} !important`}
+  `,
+  'select.item': css`
+    :hover, :focus {
+      background-color: ${zdColorPlatinum} !important;
+    }
+
+    box-shadow: ${props => props.focused && `inset 0 ${3/FONT_SIZE}rem 0 rgba(153,153,153, 0.4), inset 0 -${3/FONT_SIZE}rem 0 rgba(153,153,153, 0.4) !important`};
+    background-color: ${props => props.focused && `${zdColorPlatinum} !important`};
   `
 };
