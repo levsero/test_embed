ee287913c4d25c44dda2ecb8581b0b809ad576b9
Wayne See <wsee@zendesk.com>
2018-08-03 16:17:01 +1000

    Apply mobile styles for garden dropdown

--- src/component/frame/gardenOverrides.js
+++ src/component/frame/gardenOverrides.js
@@ -27,8 +27,23 @@ const checkboxHintMobileOverrides = isMobile ? css`
 
 const bottomMargin = css`margin-bottom: ${20/FONT_SIZE}rem !important;`;
 
+const borderOverrides = isMobile ? css`
+    border-radius: ${4/FONT_SIZE}rem !important;
+    border-width: ${1.1/FONT_SIZE}rem !important;
+  `
+  : '';
+
 const checkboxLabelMobileOverrides = isMobile ? css`
     ${mobileOverrides}
+
+    &:before {
+      width: ${14/FONT_SIZE}rem !important;
+      height: ${14/FONT_SIZE}rem !important;
+      top: ${7/FONT_SIZE}rem !important;
+      margin-top: -${4/FONT_SIZE}rem !important;
+      ${borderOverrides}
+    }
+
     [dir='ltr'] & {
       padding-left: ${22/FONT_SIZE}rem !important;
     }
@@ -37,23 +52,11 @@ const checkboxLabelMobileOverrides = isMobile ? css`
       padding-right: ${22/FONT_SIZE}rem !important;
     }
     :focus {
-      box-shadow: ${'0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important;'}
+      box-shadow: 0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important;
     }
 
     box-shadow: ${props => props.focused && `0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important`}
     border-color: ${props => props.focused && `${zdColorGrey600} !important`}
-
-    &:before {
-      width: ${14/FONT_SIZE}rem !important;
-      height: ${14/FONT_SIZE}rem !important;
-      margin-top: -0.05rem !important;
-    }
-  `
-  : '';
-
-const borderOverrides = isMobile ? css`
-    border-radius: ${4/FONT_SIZE}rem !important;
-    border-width: ${1.1/FONT_SIZE}rem !important;
   `
   : '';
 
@@ -64,7 +67,25 @@ const selectOverrides = css`
 
   box-shadow: ${props => props.focused && `inset 0 ${3/FONT_SIZE}rem 0 rgba(153,153,153, 0.4), inset 0 -${3/FONT_SIZE}rem 0 rgba(153,153,153, 0.4) !important`};
   background-color: ${props => props.focused && `${zdColorGrey400} !important`};
+  ${mobileOverrides}
+  ${isMobileBrowser() ? `padding: ${10/FONT_SIZE}rem ${14/FONT_SIZE}rem !important;` : ''}
 `;
+
+const arrowOverrides = isMobileBrowser() ? css`
+  &::before {
+    background-size: ${14/FONT_SIZE}rem !important;
+    width: ${50/FONT_SIZE}rem !important;
+    height: ${40/FONT_SIZE}rem !important;
+  }
+` : '';
+const selectArrowOverrides = css`
+  ${selectOverrides}
+  ${arrowOverrides}
+`;
+const dropdownOverrides = isMobileBrowser() ? css`
+  ${mobileOverrides}
+  max-height: ${300/FONT_SIZE}rem !important;
+` : '';
 
 const genericOverrides = css`
   :hover, :focus {
@@ -119,13 +140,16 @@ function getGardenOverrides() {
     'select.select_view': css`
       ${genericOverrides}
       ${bottomMargin}
+      ${borderOverrides}
       box-shadow: ${props => props.focused && `0 0 0 ${3/FONT_SIZE}rem rgba(153,153,153, 0.4) !important`}
       border-color: ${props => props.focused && `${zdColorGrey600} !important`}
       margin-bottom: ${20/FONT_SIZE}rem !important;
+      ${arrowOverrides}
     `,
+    'select.dropdown': dropdownOverrides,
     'select.item': selectOverrides,
-    'select.next_item': selectOverrides,
-    'select.previous_item': selectOverrides
+    'select.next_item': selectArrowOverrides,
+    'select.previous_item': selectArrowOverrides
   };
 }
 
