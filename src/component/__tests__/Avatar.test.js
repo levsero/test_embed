2a7de6b3f241359f5737a144f4b2f237ac8a870a
Wayne See <wsee@zendesk.com>
2018-09-22 16:41:41 +1000

    Add jest and default config

---
+++ src/component/__tests__/Avatar.test.js
@@ -0,0 +1,9 @@
+import { render } from 'react-testing-library';
+
+import { Avatar } from '../Avatar';
+
+test('adds 1 + 2 to equal 3', () => {
+  const blah = render(<Avatar />);
+
+  console.log(blah);
+});
