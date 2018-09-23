7a4e5c80346ecd7733bbe54918d62d36f7260102
Wayne See <wsee@zendesk.com>
2018-09-23 23:05:33 +1000

    Add prechat testing example

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,10 @@
 import 'jest-dom/extend-expect';
-
 import 'react-testing-library/cleanup-after-each';
+import { i18n } from 'service/i18n';
+
+i18n.setLocale('en-US');
+
+jest.mock('translation/ze_translations');
+jest.mock('translation/ze_localeIdMap');
+
+jest.mock('component/Refocus');
