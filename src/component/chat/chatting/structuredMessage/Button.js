8040695b0db3dfd84be784cfa618ec1d5d2fcedc
colin <colinnus@gmail.com>
2018-10-17 16:26:35 +0800

    Add chat-related schema on top of pure buttons

---
+++ src/component/chat/chatting/structuredMessage/Button.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import { Button as PureButton } from 'component/shared/StructuredMessage/pure/Button';
+import {CHAT_STRUCTURED_MESSAGE_ACTION_TYPE} from 'constants/chat';
+
+const {QUICK_REPLY_ACTION, LINK_ACTION} = CHAT_STRUCTURED_MESSAGE_ACTION_TYPE;
+
+const ActionPropType = PropTypes.shape({
+  type: PropTypes.oneOf([QUICK_REPLY_ACTION, LINK_ACTION]).isRequired,
+  value: PropTypes.string.isRequired
+});
+
+const ButtonSchemaShape = {
+  text: PropTypes.string.isRequired,
+  action: ActionPropType.isRequired
+};
+
+export const ButtonSchemaPropType = PropTypes.shape(ButtonSchemaShape);
+
+export class Button extends Component {
+  static propTypes = {
+    ...ButtonSchemaShape,
+    createAction: PropTypes.func
+  };
+
+  render() {
+    const { text, createAction, action } = this.props;
+
+    return (
+      <PureButton label={text} onClick={createAction(action)} />
+    );
+  }
+}
---
+++ test/unit/component/chat/chatting/StructuredMessage/Button_test.js
@@ -0,0 +1,72 @@
+describe('Button Component', () => {
+  let Button;
+
+  const buttonPath = buildSrcPath('component/chat/chatting/structuredMessage/Button');
+  const chatConstantsPath = buildSrcPath('constants/chat');
+
+  const PureButton = noopReactComponent();
+
+  const createActionSpy = jasmine.createSpy('createAction');
+
+  const chatConstants = requireUncached(chatConstantsPath);
+  let CHAT_STRUCTURED_MESSAGE_ACTION_TYPE = chatConstants.CHAT_STRUCTURED_MESSAGE_ACTION_TYPE;
+
+  beforeEach(() => {
+    mockery.enable();
+
+    initMockRegistry({
+      'component/shared/StructuredMessage/pure/Button': {
+        Button: PureButton
+      },
+      'constants/chat': {
+        CHAT_STRUCTURED_MESSAGE_ACTION_TYPE
+      },
+    });
+
+    mockery.registerAllowable(buttonPath);
+    Button = requireUncached(buttonPath).Button;
+  });
+
+  afterEach(() => {
+    mockery.deregisterAll();
+    mockery.disable();
+
+    createActionSpy.calls.reset();
+  });
+
+  describe('#render', () => {
+    const mockProps = {
+      text: 'Hey!',
+      action: {
+        type: CHAT_STRUCTURED_MESSAGE_ACTION_TYPE.QUICK_REPLY_ACTION,
+        value: 'replied'
+      }
+    };
+
+    let component,
+      result;
+
+    beforeEach(() => {
+      component = instanceRender(<Button {...mockProps} createAction={createActionSpy} />);
+      result = component.render();
+    });
+
+    it('should return a PureButton component', () => {
+      expect(TestUtils.isElementOfType(result, PureButton)).toEqual(true);
+    });
+
+    it('should pass the correct label props to the Pure button components', () => {
+      expect(result.props.label).toEqual(mockProps.text);
+    });
+
+    it('should pass the correct onClick props to the Pure button components', () => {
+      const mockActionSpy = jasmine.createSpy('mockActionSpy');
+
+      createActionSpy.and.callFake(() => mockActionSpy);
+
+      const result = component.render();
+
+      expect(result.props.onClick).toEqual(mockActionSpy);
+    });
+  });
+})
