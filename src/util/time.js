6d3b27a0b362d76413cf0ef53b382e8bcfa08913
Apoorv Kansal <33816630+apoorvkk@users.noreply.github.com>
2018-10-23 17:54:04 +1100

    Revert "[CE-4041] [RF-1363] Uses Intl to do i18n for time for operating hours(chat)"

--- src/util/time.js
+++ src/util/time.js
@@ -1,13 +1,26 @@
-function i18nTimeFromMinutes(minuteString, locale = 'en-US') {
-  const dateTimeFormat = new Intl.DateTimeFormat(locale, {
-    hour: 'numeric',
-    minute: 'numeric'
-  });
+function zeroPad(number) {
+  return (number < 10) ? `0${number}` : `${number}`;
+}
+
+function formatHours(number, is24Hour) {
+  const hours = (!is24Hour && number > 12) ? number - 12 : number;
+
+  return zeroPad(hours);
+}
+
+function timeFromMinutes(minuteString, am, pm) {
   const minutesFromMidnight = parseInt(minuteString, 10);
   const hours = Math.floor(minutesFromMidnight / 60);
   const minutes = minutesFromMidnight % 60;
+  const is24Hour = !am || !pm;
+  const amOrPm = hours >= 12 ? pm : am;
+  const period = !is24Hour ? amOrPm : '';
 
-  return dateTimeFormat.format(new Date(2018, 10, 15, hours, minutes, 0));
+  return {
+    time: `${formatHours(hours, is24Hour)}:${zeroPad(minutes)}`,
+    is24Hour: is24Hour,
+    period: period
+  };
 }
 
-export { i18nTimeFromMinutes };
+export { timeFromMinutes };
--- test/unit/util/time_test.js
+++ test/unit/util/time_test.js
@@ -1,100 +1,117 @@
 describe('time', () => {
-  let i18nTimeFromMinutes;
+  let timeFromMinutes;
   const timePath = buildSrcPath('util/time');
-  const dateTimeFormatterSpy = jasmine.createSpy('dateTimeFormatterSpy');
 
   beforeEach(() => {
-    ({ i18nTimeFromMinutes } = requireUncached(timePath));
+    timeFromMinutes = requireUncached(timePath).timeFromMinutes;
   });
 
-  describe('#i18nTimeFromMinutes', () => {
-    let timeInMinutes,
-      locale;
-
-    const defaultLocale = 'en-US';
-
-    beforeAll(() => {
-      spyOn(Intl, 'DateTimeFormat').and.returnValue({ format: dateTimeFormatterSpy });
-    });
+  describe('#timeFromMinutes', () => {
+    let result,
+      timeInMinutes,
+      amString,
+      pmString;
 
     beforeEach(() => {
-      Intl.DateTimeFormat.calls.reset();
-      dateTimeFormatterSpy.calls.reset();
-
-      if (locale) {
-        i18nTimeFromMinutes(timeInMinutes, locale);
-      } else {
-        i18nTimeFromMinutes(timeInMinutes);
-      }
+      result = timeFromMinutes(timeInMinutes, amString, pmString);
     });
 
-    describe('when a different locale is set (ja)', () => {
-      beforeAll(() => {
-        timeInMinutes = 0;
-        locale = 'ja';
-      });
-
-      afterAll(() => {
-        locale = null;
-      });
-
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(locale, jasmine.any(Object));
-      });
-    });
-
-    describe('when the time is before noon', () => {
-      beforeAll(() => {
-        timeInMinutes = 280;
-      });
-
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(defaultLocale, jasmine.any(Object));
-        expect(dateTimeFormatterSpy).toHaveBeenCalledWith(new Date(2018, 10, 15, 4, 40, 0));
-      });
-    });
-
-    describe('when the time is after noon', () => {
+    describe('when the time is 24-hour time (military style)', () => {
       beforeAll(() => {
         timeInMinutes = 1000;
+        amString = '';
+        pmString = 'somethingThatShouldBeIgnored';
       });
 
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(defaultLocale, jasmine.any(Object));
-        expect(dateTimeFormatterSpy).toHaveBeenCalledWith(new Date(2018, 10, 15, 16, 40, 0));
-      });
-    });
-
-    describe('when the time is exactly noon', () => {
-      beforeAll(() => {
-        timeInMinutes = 720;
+      it('formats the time in 24-hour time style', () => {
+        expect(result.time).toEqual('16:40');
       });
 
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(defaultLocale, jasmine.any(Object));
-        expect(dateTimeFormatterSpy).toHaveBeenCalledWith(new Date(2018, 10, 15, 12, 0, 0));
+      it('recognises that the time should be the time in 24-hour style', () => {
+        expect(result.is24Hour).toEqual(true);
+      });
+
+      it('passes no am/pm value', () => {
+        expect(result.period).toEqual('');
       });
     });
 
-    describe('when the time is exactly midnight (0 minutes)', () => {
+    describe('when the time is 12-hour time (civilian style)', () => {
       beforeAll(() => {
-        timeInMinutes = 0;
+        amString = 'am';
+        pmString = 'pm';
       });
 
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(defaultLocale, jasmine.any(Object));
-        expect(dateTimeFormatterSpy).toHaveBeenCalledWith(new Date(2018, 10, 15, 0, 0, 0));
-      });
-    });
+      describe('when the time is before noon', () => {
+        beforeAll(() => {
+          timeInMinutes = 280;
+        });
 
-    describe('when the time is exactly midnight (1440 minutes)', () => {
-      beforeAll(() => {
-        timeInMinutes = 1440;
+        it('formats the time in 12-hour time style', () => {
+          expect(result.time).toEqual('04:40');
+        });
+
+        it('recognises that the time should be the time in 12-hour style', () => {
+          expect(result.is24Hour).toEqual(false);
+        });
+
+        it('passes the right period', () => {
+          expect(result.period).toEqual('am');
+        });
       });
 
-      it('calls the formatter and factory correctly', () => {
-        expect(Intl.DateTimeFormat).toHaveBeenCalledWith(defaultLocale, jasmine.any(Object));
-        expect(dateTimeFormatterSpy).toHaveBeenCalledWith(new Date(2018, 10, 15, 24, 0, 0));
+      describe('when the time is after noon', () => {
+        beforeAll(() => {
+          timeInMinutes = 1000;
+        });
+
+        it('formats the time in 12-hour time style', () => {
+          expect(result.time).toEqual('04:40');
+        });
+
+        it('recognises that the time should be the time in 12-hour style', () => {
+          expect(result.is24Hour).toEqual(false);
+        });
+
+        it('passes the right period', () => {
+          expect(result.period).toEqual('pm');
+        });
+      });
+
+      describe('when the time is exactly noon', () => {
+        beforeAll(() => {
+          timeInMinutes = 720;
+        });
+
+        it('formats the time in 12-hour time style', () => {
+          expect(result.time).toEqual('12:00');
+        });
+
+        it('recognises that the time should be the time in 12-hour style', () => {
+          expect(result.is24Hour).toEqual(false);
+        });
+
+        it('passes the right period', () => {
+          expect(result.period).toEqual('pm');
+        });
+      });
+
+      describe('when the time is exactly midnight', () => {
+        beforeAll(() => {
+          timeInMinutes = 0;
+        });
+
+        it('formats the time in 12-hour time style', () => {
+          expect(result.time).toEqual('00:00');
+        });
+
+        it('recognises that the time should be the time in 12-hour style', () => {
+          expect(result.is24Hour).toEqual(false);
+        });
+
+        it('passes the right period', () => {
+          expect(result.period).toEqual('am');
+        });
       });
     });
   });
