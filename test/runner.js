d0edd7d258ae19cc9bdd04b030ed9eb30efb4618
David Allen <1337dallen@gmail.com>
2018-10-26 10:18:32 +1100

    Bump jasmine-spec-reporter and force it to display a stack trace

--- test/runner.js
+++ test/runner.js
@@ -3,7 +3,12 @@ require('babel-register')({
 });
 
 const Jasmine = require('jasmine');
-const SpecReporter = require('jasmine-spec-reporter');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const reporter = new SpecReporter({
+  spec: {
+    displayStacktrace: true,
+  }
+});
 
 const jRunner = new Jasmine();
 const env = process.env.NODE_ENV;
@@ -12,7 +17,7 @@ const env = process.env.NODE_ENV;
 // https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 if (env !== 'test') {
   jRunner.configureDefaultReporter({ print: () => {} });
-  jRunner.addReporter(new SpecReporter());
+  jRunner.addReporter(reporter);
 }
 
 jRunner.loadConfigFile('test/jasmine.json');
