5e6d181bf7e9d7dc7784f9a8e50f47dac99d5c48
Daniel Angel-Bradford <locusdelicti@gmail.com>
2018-11-01 11:36:28 +1100

    Remove redundant warnings

--- src/util/nullZChat.js
+++ src/util/nullZChat.js
@@ -2,7 +2,9 @@ let nullZChat = null;
 
 if (typeof Proxy !== 'undefined') {
   nullZChat = new Proxy(new Object(), {
-    get(_target, _prop) {
+    get(_target, prop) {
+      if (prop === 'toJSON') return {};
+
       return (_data) => {
         /* eslint-disable max-len, no-console */
         console.warn('You are trying to use a Zendesk Web Widget API method that is native to the Widget\'s Integrated Chat experience.');
