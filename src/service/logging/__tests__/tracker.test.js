e7a2f1dac58584d12083450362f8aa98ff941847
Wayne See <wsee@zendesk.com>
2018-11-09 20:01:24 +1100

    Add queueing function to tracker

--- src/service/logging/__tests__/tracker.test.js
+++ src/service/logging/__tests__/tracker.test.js
@@ -4,6 +4,10 @@ jest.mock('service/beacon');
 
 import { beacon } from 'service/beacon';
 
+beforeEach(() => {
+  tracker.queue = [];
+});
+
 it('does not track if disabled', () => {
   tracker.send = false;
   tracker.track('blah');
@@ -51,6 +55,15 @@ describe('enabled', () => {
       );
   });
 
+  it('handles no arguments', () => {
+    tracker.track('webWidget.blah');
+
+    expect(beacon.trackUserAction)
+      .toHaveBeenCalledWith('api', 'webWidget.blah', null,
+        { args: null }
+      );
+  });
+
   describe('addTo', () => {
     let subject;
 
@@ -73,6 +86,42 @@ describe('enabled', () => {
         .toHaveBeenCalledWith('api', 'test.add', null,
           { args: [5, 9] }
         );
+    });
+  });
+
+  describe('queueing', () => {
+    beforeEach(() => {
+      tracker.enqueue('method1', 1, 2, 3);
+      tracker.enqueue('method2');
+      tracker.enqueue('method3', () => {});
+    });
+
+    it('enqueues items', () => {
+      expect(tracker.queue.length)
+        .toEqual(3);
+    });
+
+    describe('flushing', () => {
+      beforeEach(() => {
+        tracker.flush();
+      });
+
+      it('removes everything in the queue', () => {
+        expect(tracker.queue.length)
+          .toEqual(0);
+      });
+
+      it('tracks everything in the queue', () => {
+        expect(beacon.trackUserAction)
+          .toHaveBeenCalledTimes(3);
+
+        expect(beacon.trackUserAction)
+          .toHaveBeenNthCalledWith(1, 'api', 'method1', null, { args: [1, 2, 3] });
+        expect(beacon.trackUserAction)
+          .toHaveBeenNthCalledWith(2, 'api', 'method2', null, { args: null });
+        expect(beacon.trackUserAction)
+          .toHaveBeenNthCalledWith(3, 'api', 'method3', null, { args: '<callback function>' });
+      });
     });
   });
 });
