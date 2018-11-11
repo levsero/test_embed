aa15b26847ea042accdb0ba8775233a7b359f497
David Allen <1337dallen@gmail.com>
2018-11-11 22:07:38 +1100

    Improve html template regex pattern

--- script/setup_html_templates.rb
+++ script/setup_html_templates.rb
@@ -2,12 +2,12 @@
 require 'fileutils'
 
 directory = 'dev/templates/web_widget'
-example_files = Dir.entries(directory).select { |f| f =~ /example/ }
+example_files = Dir.entries(directory).select { |f| f =~ /^.+\.example$/ }
 
 raise 'Error: Missing webwidget HTML template files' if example_files.empty?
 
 files = example_files.each_with_object({}) do |file, acc|
-  acc[file] = file.sub(/.example/, '')
+  acc[file] = file.sub(/\.example/, '')
 end
 
 files.each do |example, html|
