caac364a487724aad250e0df8b41b40b21be357c
David Allen <1337dallen@gmail.com>
2018-11-12 16:29:44 +1100

    Add commit hooks for linting and testing

--- test/runner.js
+++ test/runner.js
@@ -2,24 +2,21 @@ require('babel-register')({
   plugins: ['dynamic-import-node']
 });
 
-const Jasmine = require('jasmine');
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
-const reporter = new SpecReporter({
-  spec: {
-    displayStacktrace: true,
-  }
-});
-
-const jRunner = new Jasmine();
-const env = process.env.NODE_ENV;
-
 // NODE_ENV value set by test:ci npm task for use on Travis - matches Travis defaults:
 // https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
-if (env !== 'test') {
-  jRunner.configureDefaultReporter({ print: () => {} });
-  jRunner.addReporter(reporter);
-}
+const env = process.env.NODE_ENV;
+const Jasmine = require('jasmine');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const jRunner = new Jasmine();
+const reporterConfig = {
+  spec: {
+    displaySuccessful: (env === 'test' ? false : true),
+    displayStacktrace: true
+  }
+};
 
+jRunner.configureDefaultReporter({ print: () => {} });
+jRunner.addReporter(new SpecReporter(reporterConfig));
 jRunner.loadConfigFile('test/jasmine.json');
 // Support running of specific tests:
 // https://jasmine.github.io/2.3/node.html#section-24
