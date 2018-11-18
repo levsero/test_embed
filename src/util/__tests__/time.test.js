0edde4724b8669db043190932c6fdd4070cf2971
Wayne See <wsee@zendesk.com>
2018-11-19 10:31:59 +1100

    Use library so that we can mock timezones and get tests to pass consistently

--- src/util/__tests__/time.test.js
+++ src/util/__tests__/time.test.js
@@ -1,11 +1,14 @@
 import { i18nTimeFromMinutes } from '../time';
+import { default as tz } from 'timezone-mock';
+
+tz.register('UTC');
 
 test.each([
-  [0, 'ja', '23:00'],
-  [280, null, '3:40 AM'],
-  [1000, null, '3:40 PM'],
-  [720, null, '11:00 AM'],
-  [1440, null, '11:00 PM']
+  [0, 'ja', '10:00'],
+  [280, null, '2:40 PM'],
+  [1000, null, '2:40 AM'],
+  [720, null, '10:00 PM'],
+  [1440, null, '10:00 AM']
 ])('i18nTimeFromMinutes(%i, %s)',
   (timeInMinutes, locale, expected) => {
     const result = locale
