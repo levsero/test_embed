35ff3cabbe327f4961f9398a0e9adf258fafd3c5
Wayne See <wsee@zendesk.com>
2018-11-23 13:49:12 +1100

    Remove rAF polyfill

--- src/util/globals.js
+++ src/util/globals.js
@@ -34,30 +34,6 @@ const getZendeskHost = doc => {
 
 const isPopout = () => win.zEPopout === true
 
-// Shim rAF for older browsers that either don't have it or need the prefixed version.
-// Attaching to the iframe window as that's what snabbt looks at.
-;(function() {
-  let lastTime = 0
-  const vendors = ['webkit', 'moz']
-
-  for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function(callback) {
-      const currTime = new Date().getTime()
-      const timeToCall = Math.max(0, 16 - (currTime - lastTime))
-      const id = window.setTimeout(function() {
-        callback(currTime + timeToCall)
-      }, timeToCall)
-
-      lastTime = currTime + timeToCall
-      return id
-    }
-  }
-})()
-
 const getWebWidgetFrameContentDocument = () => {
   try {
     return document.getElementById('webWidget').contentDocument
