336a16e030c3eaee2e816792c5dd0a677b9fa59a
Wayne See <wsee@zendesk.com>
2018-11-30 15:31:06 +1100

    Migrate all preview tests to jest

--- src/__tests__/chatPreview.test.js
+++ src/__tests__/chatPreview.test.js
@@ -1,71 +1,176 @@
 import '../chatPreview';
 
-let preview;
+import { i18n } from 'service/i18n';
 
 beforeEach(() => {
   const div = document.createElement('div');
 
   div.setAttribute('id', 'preview');
   document.body.appendChild(div);
-
-  preview = window.zEPreview.renderPreview({
-    element: document.getElementById('preview')
-  });
 });
 
 afterEach(() => {
-  chatPreview().remove();
+  const rendered = chatPreview();
+
+  if (rendered) {
+    rendered.remove();
+  }
   document.getElementById('preview').remove();
 });
 
 const chatPreview = () => document.getElementById('chatPreview');
 const chatPreviewBody = () => chatPreview().contentWindow.document.body.innerHTML;
+const chatPreviewBodyEl = () => chatPreview().contentWindow.document.body;
 
-it('creates the iframe for preview', (done) => {
-  preview.waitForComponent(() => {
+describe('rendered with default options', () => {
+  let preview;
+
+  beforeEach(() => {
+    preview = window.zEPreview.renderPreview({
+      element: document.getElementById('preview')
+    });
+  });
+
+  it('creates the iframe for preview', (done) => {
+    preview.waitForComponent(() => {
+      expect(chatPreview())
+        .toBeInTheDocument();
+      done();
+    });
+  });
+
+  it('creates the iframe with the expected styles', () => {
     expect(chatPreview())
-      .toBeInTheDocument();
-    done();
+      .toHaveStyle(`
+        position: relative;
+        float: right;
+        margin-top: 16px;
+        margin-right: 16px;
+        right: 0px;
+        bottom: 0px;
+        width: 369px;
+        height: 565px;
+    `);
+  });
+
+  it('can update to chatting screen', (done) => {
+    preview.updateScreen('widget/chat/CHATTING_SCREEN');
+    preview.waitForComponent(() => {
+      expect(chatPreviewBodyEl())
+        .toHaveTextContent('Type a message here');
+      done();
+    });
+  });
+
+  it('can update to prechat screen', (done) => {
+    preview.updateScreen('widget/chat/PRECHAT_SCREEN');
+    preview.waitForComponent(() => {
+      expect(chatPreviewBodyEl())
+        .toHaveTextContent('Start chat');
+      done();
+    });
+  });
+
+  it('can update to offline message screen', (done) => {
+    preview.updateScreen('widget/chat/OFFLINE_MESSAGE_SCREEN');
+    preview.waitForComponent(() => {
+      expect(chatPreviewBodyEl())
+        .toHaveTextContent('Sorry, we are not online at the moment');
+      done();
+    });
+  });
+
+  it('allows setting of color', (done) => {
+    preview.setColor('#FF1234');
+
+    preview.waitForComponent(() => {
+      expect(chatPreviewBody())
+        .toMatch('background-color: #FF1234 !important;');
+      done();
+    });
+  });
+
+  it('sets it with default color', (done) => {
+    preview.setColor();
+
+    preview.waitForComponent(() => {
+      expect(chatPreviewBody())
+        .toMatch('background-color: #659700 !important;');
+      done();
+    });
+  });
+
+  it('allows updating of locale', (done) => {
+    preview.updateLocale('zh');
+
+    preview.waitForComponent(() => {
+      expect(i18n.getLocale())
+        .toEqual('zh-cn');
+      done();
+    });
+  });
+
+  it('allows setting of chat state', (done) => {
+    const action = { type: 'account_status', detail: 'online' };
+
+    preview.updateChatState(action);
+
+    preview.waitForComponent(() => {
+      expect(chatPreviewBodyEl())
+        .toHaveTextContent('Chat with us');
+      expect(chatPreviewBodyEl())
+        .toHaveTextContent('Live Support');
+      done();
+    });
+  });
+
+  it('allows updating of preview settings', (done) => {
+    preview.updateScreen('widget/chat/CHATTING_SCREEN');
+    preview.updateSettings({ concierge: { title: 'updated concierge title' } });
+
+    preview.waitForComponent(() => {
+      expect(chatPreviewBodyEl().querySelector('header'))
+        .toHaveTextContent('updated concierge title');
+      done();
+    });
   });
 });
 
-it('creates the iframe with the expected styles', () => {
+describe('when calling with no element property in options', () => {
+  it('throws an error', () => {
+    expect(() => window.zEPreview.renderPreview())
+      .toThrowError('A DOM element is required to render the Preview into');
+  });
+});
+
+test('locale can be set', () => {
+  window.zEPreview.renderPreview({
+    element: document.getElementById('preview'),
+    locale: 'fr'
+  });
+
+  expect(i18n.getLocale())
+    .toEqual('fr');
+});
+
+test('styles can be customized', () => {
+  const styles = {
+    float: 'left',
+    marginTop: '32px',
+    marginLeft: '32px',
+    width: '100px'
+  };
+
+  window.zEPreview.renderPreview({
+    element: document.getElementById('preview'),
+    styles
+  });
+
   expect(chatPreview())
     .toHaveStyle(`
-      position: relative;
-      float: right;
-      margin-top: 16px;
-      margin-right: 16px;
-      right: 0px;
-      bottom: 0px;
-      width: 369px;
-      height: 565px;
-  `);
-});
-
-it('can update to chatting screen', (done) => {
-  preview.updateScreen('widget/chat/CHATTING_SCREEN');
-  preview.waitForComponent(() => {
-    expect(chatPreviewBody())
-      .toMatch(/Type a message here/);
-    done();
-  });
-});
-
-it('can update to prechat screen', (done) => {
-  preview.updateScreen('widget/chat/PRECHAT_SCREEN');
-  preview.waitForComponent(() => {
-    expect(chatPreviewBody())
-      .toMatch(/Start chat/);
-    done();
-  });
-});
-
-it('can update to offline message screen', (done) => {
-  preview.updateScreen('widget/chat/OFFLINE_MESSAGE_SCREEN');
-  preview.waitForComponent(() => {
-    expect(chatPreviewBody())
-      .toMatch(/Sorry, we are not online at the moment/);
-    done();
-  });
+      float: left;
+      margin-top: 32px;
+      margin-left: 32px;
+      width: 112px;
+    `);
 });
--- src/__tests__/webWidgetPreview.test.js
+++ src/__tests__/webWidgetPreview.test.js
@@ -1,57 +1,89 @@
 import '../webWidgetPreview';
 
-let preview;
+import { i18n } from 'service/i18n';
 
 beforeEach(() => {
   const div = document.createElement('div');
 
   div.setAttribute('id', 'preview');
   document.body.appendChild(div);
-
-  preview = window.zE.renderWebWidgetPreview({
-    element: document.getElementById('preview')
-  });
 });
 
 afterEach(() => {
-  webWidgetPreview().remove();
+  const rendered = webWidgetPreview();
+
+  if (rendered) {
+    rendered.remove();
+  }
   document.getElementById('preview').remove();
 });
 
 const webWidgetPreview = () => document.getElementById('webWidgetPreview');
 const webWidgetPreviewBody = () => webWidgetPreview().contentWindow.document.body.innerHTML;
+const webWidgetPreviewBodyEl = () => webWidgetPreview().contentWindow.document.body;
 
-it('creates the iframe for preview', () => {
-  expect(webWidgetPreview())
-    .toBeInTheDocument();
-});
+describe('default parameters', () => {
+  let preview;
 
-it('creates the iframe with the expected styles', () => {
-  expect(webWidgetPreview())
-    .toHaveStyle(`
-      position: relative;
-      float: right;
-      margin-top: 16px;
-      margin-right: 16px;
-      right: 0px;
-      bottom: 0px;
-      width: 369px;
-      height: 565px;
-  `);
-});
+  beforeEach(() => {
+    preview = window.zE.renderWebWidgetPreview({
+      element: document.getElementById('preview')
+    });
+  });
 
-it('renders the message title', () => {
-  preview.setTitle('message');
+  it('creates the iframe for preview', () => {
+    expect(webWidgetPreview())
+      .toBeInTheDocument();
+  });
 
-  expect(webWidgetPreviewBody())
-    .toMatch(/Leave us a message/);
-});
+  it('creates the iframe with the expected styles', () => {
+    expect(webWidgetPreview())
+      .toHaveStyle(`
+        position: relative;
+        float: right;
+        margin-top: 16px;
+        margin-right: 16px;
+        right: 0px;
+        bottom: 0px;
+        width: 369px;
+        height: 565px;
+    `);
+  });
 
-it('renders the contact title', () => {
-  preview.setTitle('contact');
+  it('renders the message title', () => {
+    preview.setTitle('message');
 
-  expect(webWidgetPreviewBody())
-    .toMatch(/Contact us/);
+    expect(webWidgetPreviewBodyEl())
+      .toHaveTextContent('Leave us a message');
+  });
+
+  it('renders the contact title', () => {
+    preview.setTitle('contact');
+
+    expect(webWidgetPreviewBodyEl())
+      .toHaveTextContent('Contact us');
+  });
+
+  it('sets it with default title if no title is passed', () => {
+    preview.setTitle();
+
+    expect(webWidgetPreviewBodyEl())
+      .toHaveTextContent('Leave us a message');
+  });
+
+  it('allows setting of color', () => {
+    preview.setColor('#FF1234');
+
+    expect(webWidgetPreviewBody())
+      .toMatch('background-color: #FF1234 !important;');
+  });
+
+  it('sets it with default color', () => {
+    preview.setColor();
+
+    expect(webWidgetPreviewBody())
+      .toMatch('background-color: #659700 !important;');
+  });
 });
 
 describe('when calling with no element property in options', () => {
@@ -59,4 +91,36 @@ describe('when calling with no element property in options', () => {
     expect(() => window.zE.renderWebWidgetPreview())
       .toThrowError('A DOM element is required to render the Web Widget Preview into');
   });
+});
+
+test('locale can be set', () => {
+  window.zE.renderWebWidgetPreview({
+    element: document.getElementById('preview'),
+    locale: 'fr'
+  });
+
+  expect(i18n.getLocale())
+    .toEqual('fr');
+});
+
+test('styles can be customized', () => {
+  const styles = {
+    float: 'left',
+    marginTop: '32px',
+    marginLeft: '32px',
+    width: '100px'
+  };
+
+  window.zE.renderWebWidgetPreview({
+    element: document.getElementById('preview'),
+    styles
+  });
+
+  expect(webWidgetPreview())
+    .toHaveStyle(`
+      float: left;
+      margin-top: 32px;
+      margin-left: 32px;
+      width: 112px;
+    `);
 });
