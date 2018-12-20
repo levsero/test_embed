d233019fdf86a9ae65eb21cd4eeca563746b6a7b
Wayne See <wsee@zendesk.com>
2018-12-20 13:19:39 +1100

    Use anchor instead of query string

--- src/util/__tests__/chat.test.js
+++ src/util/__tests__/chat.test.js
@@ -212,7 +212,7 @@ describe('createChatPopoutWindow', () => {
   it('creates the query string correctly', () => {
     createChatPopoutWindow('settings', 'machineId', 'defactoLanguage');
 
-    const url = 'https://static.zdassets.com/web_widget/latest/liveChat.html?key=a.zendesk.com&settings="settings"&mid=machineId&locale=defactoLanguage';
+    const url = 'https://static.zdassets.com/web_widget/latest/liveChat.html?v=1#key=a.zendesk.com&settings="settings"&mid=machineId&locale=defactoLanguage';
 
     expect(globals.win.open)
       .toHaveBeenCalledWith(url, 'Web Widget LiveChat', 'height=600,width=400');
