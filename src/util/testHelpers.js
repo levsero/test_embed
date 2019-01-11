779aff43de6dfa66374c621e2016d96c39dba9b0
David Allen <1337dallen@gmail.com>
2019-01-11 13:36:35 +1100

    Add test helpers

--- src/util/testHelpers.js
+++ src/util/testHelpers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { GET_ACCOUNT_SETTINGS_REQUEST_SUCCESS } from 'src/redux/modules/chat/chat-action-types';
+import { i18n } from 'src/service/i18n';
 
 export const dispatchChatAccountSettings = (store, settings) => {
   store.dispatch({
@@ -27,4 +28,50 @@ export const noopReactComponent = () => class extends Component {
       </div>
     );
   }
+};
+
+/*
+  For testing reducers (duh).
+  1. Pass it your reducer and an array of different action objects.
+  2. Verify that the generated snapshots match your expectations.
+  3. Enjoy not having to write boilerplate reducer tests.
+*/
+export const testReducer = (reducer, actions) => {
+  actions.forEach((action) => {
+    test(`${reducer.name}, action: ${action.type}`, () => {
+      expect(reducer(undefined, action)).toMatchSnapshot();
+    });
+  });
+};
+
+/*
+  For testing basic translation selectors made using reselect's `createSelector`.
+  And by basic, I mean of the i18n.getSettingTranslations(setting) || i18n.t('default.translation') kind.
+  The resultFunc needs to have an arity of 2 for the unused locale param.
+*/
+export const testTranslationStringSelector = (selector) => {
+  const subject = selector.resultFunc;
+  const defaultTranslation = 'default translation';
+  const settingsTranslation = 'settings translation';
+
+  if (!subject || subject.length !== 2) {
+    throw new Error('For testing create selector resultFuncs with arity of 2');
+  }
+
+  describe(`${selector.name}`, () => {
+    test.each([
+      [undefined, null, defaultTranslation],
+      [settingsTranslation, null, settingsTranslation]
+    ])('resultFunc(%p, %p) returns %p',
+      (settingString, locale, expected) => {
+        jest.spyOn(i18n, 't').mockReturnValue(defaultTranslation);
+        jest.spyOn(i18n, 'getSettingTranslation').mockReturnValue(settingString);
+
+        expect(subject(settingString, locale)).toEqual(expected);
+
+        i18n.t.mockRestore();
+        i18n.getSettingTranslation.mockRestore();
+      }
+    );
+  });
 };
