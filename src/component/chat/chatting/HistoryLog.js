b45e1afd481304f6f68d1c77587be89e6a705984
Timothy Patullock <oddalicious@users.noreply.github.com>
2019-02-04 17:15:12 +1100

    Revert "Revert "[CE-5074] remove luxon, update Formatters.js to use the i18n tool""

--- src/component/chat/chatting/HistoryLog.js
+++ src/component/chat/chatting/HistoryLog.js
@@ -22,7 +22,6 @@ export class HistoryLog extends Component {
     agents: PropTypes.object,
     showAvatar: PropTypes.bool.isRequired,
     firstMessageTimestamp: PropTypes.number,
-    luxon: PropTypes.object.isRequired,
     isMobile: PropTypes.bool.isRequired
   };
 
@@ -42,7 +41,7 @@ export class HistoryLog extends Component {
   }
 
   renderDivider = (timestamp) => {
-    const format = dateTime(this.props.luxon, timestamp, { showToday: true });
+    const format = dateTime(timestamp, { showToday: true });
 
     return <div className={styles.divider}>{format}</div>;
   }
--- src/util/__tests__/formatters.test.js
+++ src/util/__tests__/formatters.test.js
@@ -1,32 +1,55 @@
 import { dateTime } from '../formatters';
 import { i18n } from 'service/i18n';
-import * as luxon from 'luxon';
-
-jest.mock('service/i18n');
 
 i18n.getLocale = jest.fn(() => 'en-US');
+i18n.t = jest.fn();
 
 describe('dateTime', () => {
-  it('formats timestamp into a proper format', () => {
-    expect(dateTime(luxon, new Date(1525654192982)))
-      .toContain('May 7, 2018');
+  describe('when passed a timestamp', () => {
+    it('attempts to pass timestamp to client-i18n-tools', () => {
+      expect(dateTime(1525654192982))
+        .toEqual('1525654192982');
+    });
   });
 
-  it('uses `today` translation string when `showToday` option is passed', () => {
-    dateTime(luxon, Date.now(), { showToday: true });
-    expect(i18n.t)
-      .toHaveBeenCalledWith(
-        'embeddable_framework.common.today',
-        expect.objectContaining({
-          time: expect.any(String)
-        })
-      );
+  describe('when `showToday` option is passed', () => {
+    const originalDate = Date,
+      originalNow = Date.now;
+
+    beforeEach(() => {
+      global.Date = jest.fn(() => {
+        return {
+          getMonth: () => 1,
+          getDay: () => 2,
+          getFullYear: () => 3,
+          getHours: () => 4,
+          getMinutes: () => 5
+        };
+      });
+      global.Date.now = jest.fn();
+    });
+
+    afterEach(() => {
+      global.Date = originalDate;
+      global.Date.now = originalNow;
+    });
+
+    it('attempts to translate Today string', () => {
+      dateTime(123, { showToday: true });
+      expect(i18n.t)
+        .toHaveBeenCalledWith(
+          'embeddable_framework.common.today',
+          expect.objectContaining({
+            time: expect.any(String)
+          })
+        );
+    });
   });
 
   it('does not throw error when locale is not supported', () => {
     i18n.getLocale = jest.fn(() => 'gibberish');
 
-    expect(dateTime(luxon, new Date(1525654192982)))
-      .toContain('May 7, 2018');
+    expect(dateTime(1525654192982))
+      .toEqual('1525654192982');
   });
 });
