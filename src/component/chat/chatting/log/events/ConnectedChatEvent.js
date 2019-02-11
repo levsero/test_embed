59d85fadf55f64d27198b494e239cc9fdeb4d4b3
Timothy Patullock <tpatullock@zendesk.com>
2019-02-11 14:47:16 +1100

    connect locale to ConnectedChatEvent

--- src/component/chat/chatting/log/events/ConnectedChatEvent.js
+++ src/component/chat/chatting/log/events/ConnectedChatEvent.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import EventMessage from './EventMessage';
 import { getEventMessage } from 'src/redux/modules/chat/chat-selectors';
+import { i18n } from 'service/i18n';
 
 const mapStateToProps = (state, props) => ({
-  event: getEventMessage(state, props.eventKey)
+  event: getEventMessage(state, props.eventKey),
+  locale: i18n.getLocale()
 });
 
 export default connect(mapStateToProps, {}, null, { withRef: true })(EventMessage);
--- src/component/chat/chatting/log/messages/ConnectedHistoryGroup.js
+++ src/component/chat/chatting/log/messages/ConnectedHistoryGroup.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import ChatGroup from './ChatGroup';
 import { getGroupMessages } from 'src/redux/modules/chat/chat-history-selectors';
+import { i18n } from 'service/i18n';
 
 const mapStateToProps = (state, props) => ({
-  messages: getGroupMessages(state, props.messageKeys)
+  messages: getGroupMessages(state, props.messageKeys),
+  locale: i18n.getLocale()
 });
 
 export default connect(mapStateToProps, {}, null, { withRef: true })(ChatGroup);
