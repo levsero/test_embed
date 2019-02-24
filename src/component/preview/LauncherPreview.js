76bfaf9bac8264c216d9977f2cc33ade10d56688
Apoorv Kansal <akansal@zendesk.com>
2019-02-25 09:54:13 +1100

    Fix warning errors in tests

--- src/component/preview/LauncherPreview.js
+++ src/component/preview/LauncherPreview.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ChatBadge from 'src/component/launcher/ChatBadge';
 
 const noop = () => {};
 
-export const LauncherPreview = () => (
-  <ChatBadge
-    onSend={noop}
-    handleChatBadgeMessageChange={noop}
-    resetCurrentMessage={noop}
-    sendMsg={noop}
-    handleChatBadgeMinimize={noop}
-    updateChatScreen={noop}
-    chatBadgeClicked={noop}
-    prechatFormRequired={false}
-    hideBranding={false}
-  />
-);
+export class LauncherPreview extends Component {
+  render() {
+    return (
+      <ChatBadge
+        onSend={noop}
+        handleChatBadgeMessageChange={noop}
+        resetCurrentMessage={noop}
+        sendMsg={noop}
+        handleChatBadgeMinimize={noop}
+        updateChatScreen={noop}
+        chatBadgeClicked={noop}
+        prechatFormRequired={false}
+        hideBranding={false}
+      />
+    );
+  }
+}
