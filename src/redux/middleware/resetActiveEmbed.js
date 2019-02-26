155100bc4714ebb0899cf5ce1d936c627137d1bd
Adrian Evans <aevans@zendesk.com>
2019-02-26 11:57:11 +1100

    Ensure UPDATE_SETTINGS and SDK_ACCOUNT_STATUS trigger resetActiveEmbed when appropriate

--- src/redux/middleware/resetActiveEmbed.js
+++ src/redux/middleware/resetActiveEmbed.js
@@ -6,6 +6,7 @@ import {
   AUTHENTICATION_SUCCESS,
   API_RESET_WIDGET
 } from 'src/redux/modules/base/base-action-types';
+import { UPDATE_SETTINGS } from 'src/redux/modules/settings/settings-action-types';
 import { SDK_CONNECTION_UPDATE, SDK_ACCOUNT_STATUS, CHAT_CONNECTED } from 'src/redux/modules/chat/chat-action-types';
 import {
   TALK_EMBEDDABLE_CONFIG_SOCKET_EVENT,
@@ -35,7 +36,7 @@ import {
   getIpmHelpCenterAllowed,
   getSubmitTicketAvailable,
   getAnswerBotAvailable,
-  getWebWidgetVisible
+  getWebWidgetVisible,
 } from 'src/redux/modules/selectors';
 import { getArticleViewActive } from 'src/redux/modules/helpCenter/helpCenter-selectors';
 import { getZopimChatOnline, getZopimIsChatting } from 'src/redux/modules/zopimChat/zopimChat-selectors';
@@ -52,7 +53,8 @@ const shouldResetForChat = (type, state) => {
   const eligibleActiveEmbeds = [
     'chat',
     'channelChoice',
-    'ticketSubmissionForm'
+    'ticketSubmissionForm',
+    'nilEmbed'
   ];
   const isChatActionEligible = _.includes(eligibleChatActions, type);
   const isActiveEmbedEligible = _.includes(eligibleActiveEmbeds, activeEmbed);
@@ -91,7 +93,7 @@ const getChatActiveEmbed = (state) => {
 
 const setNewActiveEmbed = (state, dispatch) => {
   let backButton = false;
-  let activeEmbed = '';
+  let activeEmbed = 'nilEmbed';
   const articleViewActive = getArticleViewActive(state);
 
   if (isPopout()) {
@@ -148,9 +150,15 @@ export default function resetActiveEmbed(prevState, nextState, action, dispatch 
     GET_ACCOUNT_SETTINGS_REQUEST_SUCCESS
   ];
 
+  const hasSuppressSettings = (type === UPDATE_SETTINGS)
+    && _.some(action.payload.webWidget, entry => (
+      _.has(entry, 'suppress') || _.has(entry, 'hideWhenOffline')
+    ));
   const isZopimChatting = getZopimIsChatting(state) && getActiveEmbed(state) === 'zopimChat';
   const isNewChatChatting = getIsChatting(state) && getActiveEmbed(state) === 'chat';
-  const shouldReset = _.includes(updateActions, type) && !isZopimChatting && !isNewChatChatting;
+  const shouldReset = (_.includes(updateActions, type) || hasSuppressSettings)
+    && !isZopimChatting
+    && !isNewChatChatting;
   const chatReset = shouldResetForChat(type, nextState);
   const zopimChatReset = shouldResetForZopimChat(type, nextState);
 
