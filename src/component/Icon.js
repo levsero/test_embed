ccfd0cf0fb5c26f430c6908e4ecd289c301c65ff
Wayne See <wsee@zendesk.com>
2019-03-13 14:44:54 +1100

    Add article icon

--- src/component/Icon.js
+++ src/component/Icon.js
@@ -21,6 +21,7 @@ const icons = {
   [ICONS.SEND_CHAT]: require('icons/widget-icon_sendChat.svg'),
   'Icon--agent-avatar': require('icons/widget-icon_avatar.svg'),
   'Icon--answerBot': require('icons/widget-icon_answerBot.svg'),
+  'Icon--article': require('icons/widget-icon_article.svg'),
   'Icon--avatar': require('icons/widget-icon_concierge.svg'),
   'Icon--channelChoice-contactForm': require('icons/widget-icon_channelChoice-contactForm.svg'),
   'Icon--channelChoice-talk': require('icons/widget-icon_channelChoice-talk.svg'),
