2455d2fe54cf93eae91aa5ec7a434b3cbb19ac77
Adrian Evans <aevans@zendesk.com>
2019-04-11 17:02:50 +1000

    Remove persistence init from boot

--- test/unit/boot_test.js
+++ test/unit/boot_test.js
@@ -18,8 +18,7 @@ describe('boot', () => {
     apiSpy = jasmine.createSpyObj('webWidgetApi', ['apisExecutePostRenderQueue', ]),
     zopimApiSpy = jasmine.createSpyObj('zopimApi', ['setupZopimQueue', 'setUpZopimApiMethods']),
     trackerSpy = jasmine.createSpyObj('tracker', ['send', 'enable']),
-    initSpy = jasmine.createSpy('init'),
-    persistenceInitSpy = jasmine.createSpy('persistence.init');
+    initSpy = jasmine.createSpy('init');
 
   let updateEmbeddableConfigSpy = jasmine.createSpy('updateEmbeddableConfig');
 
@@ -81,8 +80,7 @@ describe('boot', () => {
       },
       'service/persistence': {
         store: {
-          get: noop,
-          init: persistenceInitSpy
+          get: noop
         },
       }
     });
@@ -105,8 +103,6 @@ describe('boot', () => {
       expect(transportSpy.http.init)
         .toHaveBeenCalled();
       expect(gaSpy.GA.init)
-        .toHaveBeenCalled();
-      expect(persistenceInitSpy)
         .toHaveBeenCalled();
     });
 
