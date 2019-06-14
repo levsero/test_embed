637c5d456855775e4089bf92cc021c00a185a660
Wayne See <wsee@zendesk.com>
2019-06-14 16:19:53 +1000

    Update answer bot test description

--- src/redux/modules/answerBot/__tests__/reducer.test.js
+++ src/redux/modules/answerBot/__tests__/reducer.test.js
@@ -1,7 +1,7 @@
 import reducer from 'src/redux/modules/answerBot/reducer';
 
 describe('answerBot root reducer', () => {
-  it('has the messages sub state', () => {
+  it('has the expected sub states', () => {
     const state = reducer({}, { type: '' });
 
     expect(state)
