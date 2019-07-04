df2a1d523d117919d8bac366903ef6f4dc5ffc91
Adrian Yong <ayong@zendesk.com>
2019-07-04 11:51:09 +1000

    Migrated Help Center mobile test to Jest

--- src/component/helpCenter/__tests__/HelpCenterMobile.test.js
+++ src/component/helpCenter/__tests__/HelpCenterMobile.test.js
@@ -1,4 +1,4 @@
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import React from 'react';
 import createStore from 'src/redux/createStore';
 import { Provider } from 'react-redux';
@@ -32,6 +32,15 @@ const renderComponent = (props) => {
   );
 };
 
+jest.useFakeTimers();
+
+test('renders the expected components', () => {
+  const { container } = renderComponent({ searchPlaceholder: 'How can we help?', title: 'hello' });
+
+  expect(container)
+    .toMatchSnapshot();
+});
+
 describe('render', () => {
   it('renders button with loading animation', () => {
     const { container } = renderComponent({
@@ -57,22 +66,16 @@ describe('render', () => {
 
   describe('show channelChoice options', () => {
     it('show channel choice when channelChoice is true', () => {
-      const { container } = renderComponent( { channelChoice: true } );
+      const { queryByText } = renderComponent( { channelChoice: true } );
 
-      expect(container.querySelector('.channelChoiceContainer'))
+      expect(queryByText('Leave a message'))
         .toBeInTheDocument();
-
-      expect(container.querySelector('.ChannelChoicePopupMobile'))
-        .not.toBeInTheDocument();
     });
 
     it('hide channel choice when channelChoice is false', () => {
-      const { container } = renderComponent({ channelChoice: false });
+      const { queryByText } = renderComponent({ channelChoice: false });
 
-      expect(container.querySelector('.channelChoiceContainer'))
-        .not.toBeInTheDocument();
-
-      expect(container.querySelector('.ChannelChoicePopupMobile'))
+      expect(queryByText('Leave a message'))
         .not.toBeInTheDocument();
     });
   });
@@ -111,22 +114,126 @@ describe('render', () => {
 
   describe('footer content', () => {
     it('renders footer buttons when showNextButton and articleViewActive is true', () => {
-      const { container } = renderComponent({
+      const { queryByText } = renderComponent({
         showNextButton: true,
+        buttonLabel: 'Leave us a message',
         articleViewActive: true
       });
 
-      expect(container.querySelector('button.footerButton'))
+      expect(queryByText('Leave us a message'))
         .toBeInTheDocument();
     });
 
     it('does not render footer buttons when showNextButton is false', () => {
-      const { container } = renderComponent({
+      const { queryByText } = renderComponent({
         showNextButton: false
       });
 
-      expect(container.querySelector('button.footerButton'))
+      expect(queryByText('Leave us a message'))
         .not.toBeInTheDocument();
     });
+  });
+
+  describe('child content', () => {
+    test('child content is not rendered initially', () => {
+      const { queryByText } = renderComponent({
+        children: <div>Hello World</div>
+      });
+
+      expect(queryByText('Hello World')).not.toBeInTheDocument();
+    });
+
+    test('child content is rendered when articleViewActive and isContextualSearchPending are true', () => {
+      const { queryByText } = renderComponent({
+        children: <div>Hello World</div>,
+        articleViewActive: true,
+        isContextualSearchPending: true
+      });
+
+      expect(queryByText('Hello World')).toBeInTheDocument();
+    });
+
+    test('loading bar is rendered when articleViewActive is false and search input is clicked', () => {
+      const { getByPlaceholderText, container } = renderComponent({
+        children: <div>Hello World</div>,
+        articleViewActive: false,
+        isContextualSearchPending: true,
+        searchPlaceholder: 'How can we help?'
+      });
+
+      fireEvent.click(getByPlaceholderText('How can we help?'));
+      expect(container.querySelector('.loadingBarContent')).toBeInTheDocument();
+    });
+
+    test('child content is rendered when isContextualSearchPending is false and articleViewActive is true', () => {
+      const { queryByText } = renderComponent({
+        children: <div>Hello World</div>,
+        articleViewActive: true,
+        isContextualSearchPending: false
+      });
+
+      expect(queryByText('Hello World')).toBeInTheDocument();
+    });
+  });
+});
+
+describe('search box clicked', () => {
+  it('changes screen when search box is clicked', () => {
+    const { queryByText, getByPlaceholderText } = renderComponent({ searchPlaceholder: 'How can we help?' });
+
+    expect(queryByText('Do you have a specific question?'))
+      .toBeInTheDocument();
+
+    fireEvent.click(getByPlaceholderText('How can we help?'));
+
+    expect(queryByText('Do you have a specific question?'))
+      .not.toBeInTheDocument();
+  });
+
+  it('changes screen back to inital view when user clicks outside the search box ', () => {
+    const { queryByText, getByPlaceholderText } = renderComponent({ searchPlaceholder: 'How can we help?' });
+
+    fireEvent.click(getByPlaceholderText('How can we help?'));
+    fireEvent.blur(getByPlaceholderText('How can we help?'));
+
+    expect(queryByText('Do you have a specific question?'))
+      .not.toBeInTheDocument();
+    jest.runAllTimers();
+    expect(queryByText('Do you have a specific question?'))
+      .toBeInTheDocument();
+  });
+
+  it('hides the footer', () => {
+    const { queryByText, getByPlaceholderText } = renderComponent({
+      showNextButton: true,
+      hasSearched: true,
+      contextualHelpRequestNeeded: true,
+      buttonLabel: 'Leave us a message',
+      searchPlaceholder: 'How can we help?'
+    });
+
+    expect(queryByText('Leave us a message'))
+      .toBeInTheDocument();
+
+    fireEvent.focus(getByPlaceholderText('How can we help?'));
+
+    expect(queryByText('Leave us a message'))
+      .not.toBeInTheDocument();
+  });
+
+  it('shows the footer after focus is lost from search input', () => {
+    const { queryByText, getByPlaceholderText } = renderComponent({
+      showNextButton: true,
+      hasSearched: true,
+      contextualHelpRequestNeeded: true,
+      buttonLabel: 'Leave us a message',
+      searchPlaceholder: 'How can we help?'
+    });
+
+    fireEvent.focus(getByPlaceholderText('How can we help?'));
+    fireEvent.blur(getByPlaceholderText('How can we help?'));
+    jest.runAllTimers();
+    expect(queryByText('Leave us a message'))
+      .toBeInTheDocument();
   });
 });
