db5e4a5d21d37e40dfb4b12b91586855b94691a1
Wayne See <wsee@zendesk.com>
2019-07-16 14:57:14 +1000

    Silence the warnings about act

--- jest.setup.js
+++ jest.setup.js
@@ -35,6 +35,24 @@ document.createRange = () => ({
   }
 })
 
+// this is just a little hack to silence a warning that we'll get until react
+// fixes this: https://github.com/facebook/react/pull/14853
+/* eslint-disable no-console */
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
+/* eslint-enable no-console */
+
 process.on('unhandledRejection', function(err, promise) {
   console.error('Unhandled rejection (promise: ', promise, ', reason: ', err, ').') // eslint-disable-line no-console
 })
