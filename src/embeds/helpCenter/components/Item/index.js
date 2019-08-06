da92911ba4344b6086dd443026bd63800c5ea477
Wayne See <wsee@zendesk.com>
2019-08-06 14:43:59 +1000

    Move Item to functional components

--- src/embeds/helpCenter/components/Item/index.js
+++ src/embeds/helpCenter/components/Item/index.js
@@ -1,34 +1,29 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { locals as styles } from './styles.scss'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-export default class Item extends PureComponent {
-  static propTypes = {
-    isMobile: PropTypes.bool,
-    article: PropTypes.object,
-    onClick: PropTypes.func
-  }
+const Item = React.forwardRef(({ isMobile, article, onClick }, ref) => {
+  const classes = classNames(styles.item, { [styles.itemMobile]: isMobile })
+  return (
+    <li className={classes}>
+      <a
+        ref={ref}
+        href={article.html_url}
+        target="_blank"
+        onClick={onClick}
+        className={styles.link}
+      >
+        {article.title || article.name}
+      </a>
+    </li>
+  )
+})
 
-  focus() {
-    this.link.focus()
-  }
-
-  render() {
-    const { isMobile, article, onClick } = this.props
-    const classes = classNames(styles.item, { [styles.itemMobile]: isMobile })
-    return (
-      <li className={classes}>
-        <a
-          ref={el => (this.link = el)}
-          href={article.html_url}
-          target="_blank"
-          onClick={onClick}
-          className={styles.link}
-        >
-          {article.title || article.name}
-        </a>
-      </li>
-    )
-  }
+Item.propTypes = {
+  isMobile: PropTypes.bool,
+  article: PropTypes.object,
+  onClick: PropTypes.func
 }
+
+export default Item
