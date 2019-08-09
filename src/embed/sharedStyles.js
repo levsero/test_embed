eb004d14b9c0d0ea880ea0b5e237e35d92e02c67
Briana Coppard <bcoppard@zendesk.com>
2019-08-09 11:42:27 +1000

    Remove garden button css package

--- src/embed/sharedStyles.js
+++ src/embed/sharedStyles.js
@@ -2,7 +2,6 @@ import LoadingSpinnerStyles from 'component/loading/LoadingSpinner.scss'
 import ScrollContainerStyles from 'component/container/ScrollContainer.scss'
 import AvatarStyles from 'component/Avatar.scss'
 import IconStyles from 'component/Icon.scss'
-import ButtonStyles from 'component/button/Button.scss'
 import ButtonNavStyles from 'component/button/ButtonNav.scss'
 import ZendeskLogoStyles from 'component/ZendeskLogo.scss'
 import NavigationStyles from 'component/frame/Navigation.scss'
@@ -11,7 +10,6 @@ export const sharedStyles = `
   ${LoadingSpinnerStyles}
   ${ScrollContainerStyles}
   ${AvatarStyles}
-  ${ButtonStyles}
   ${ButtonNavStyles}
   ${ZendeskLogoStyles}
   ${IconStyles}
