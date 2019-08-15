27a727b47f7e9d168691b925c13c0a2346b95bda
Apoorv Kansal <akansal@zendesk.com>
2019-08-15 10:28:52 +1000

    Remove internal Zendesk Logo check

--- src/components/ZendeskLogo/__tests__/index.test.js
+++ src/components/ZendeskLogo/__tests__/index.test.js
@@ -2,16 +2,8 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { Component as ZendeskLogo } from '../index'
 
-describe('ZendeskLogo', () => {
-  it('renders logo if enabled', () => {
-    const { container } = render(<ZendeskLogo href="yolo.com" />)
+it('renders logo', () => {
+  const { container } = render(<ZendeskLogo href="yolo.com" />)
 
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
-  it('does not render logo if disabled', () => {
-    const { container } = render(<ZendeskLogo href="yolo.com" hideZendeskLogo={true} />)
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
+  expect(container.firstChild).toMatchSnapshot()
 })
