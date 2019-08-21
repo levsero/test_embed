c99df8303e52ca6609daf742365b44a48b599975
Alex Robinson <arobinson@zendesk.com>
2019-08-21 22:57:32 +1000

    Update Talk embed's SuccessNotificationPage component to use styled-components

--- src/embeds/talk/styles.js
+++ src/embeds/talk/styles.js
@@ -1,8 +1,6 @@
-import SuccessNotificationPageStyles from 'embeds/talk/pages/SuccessNotificationPage/styles.scss'
 import TalkCallbackFormStyles from 'embeds/talk/components/CallbackForm/styles.scss'
 
 const styles = `
-  ${SuccessNotificationPageStyles}
   ${TalkCallbackFormStyles}
 `
 
