d56a7cb9feeeadec74dadb42a35126322458a92c
Alex Robinson <arobinson@zendesk.com>
2019-08-21 18:48:27 +1000

    Update Talk embed's CountryDropdown component to be in line with styled-components guidelines

---
+++ src/embeds/talk/components/CountryDropdown/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Item } from '@zendeskgarden/react-select'
+
+const FlexItem = styled(Item)`
+  display: flex !important;
+`
+
+export { FlexItem as Item }
