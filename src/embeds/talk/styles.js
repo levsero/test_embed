532375901f4fe2d7773350b16839f99007449206
Alex Robinson <arobinson@zendesk.com>
2019-08-21 19:02:20 +1000

    Update Talk embed's ErrorNotification component to use styled-components

--- src/embeds/talk/styles.js
+++ src/embeds/talk/styles.js
@@ -2,7 +2,6 @@ import FlagStyles from 'src/embeds/talk/components/Flag/styles.scss'
 import SuccessNotificationPageStyles from 'embeds/talk/pages/SuccessNotificationPage/styles.scss'
 import SuccessNotificationStyles from 'src/embeds/talk/components/SuccessNotification/styles.scss'
 import TalkCallbackFormStyles from 'embeds/talk/components/CallbackForm/styles.scss'
-import TalkNotificationStyles from 'embeds/talk/components/ErrorNotification/styles.scss'
 import TalkOfflinePageStyles from 'embeds/talk/pages/OfflinePage/styles.scss'
 import TalkPhoneNumberStyles from 'embeds/talk/components/PhoneNumber/styles.scss'
 import TalkPhoneOnlyPageStyles from 'embeds/talk/pages/PhoneOnlyPage/styles.scss'
@@ -12,7 +11,6 @@ const styles = `
   ${SuccessNotificationPageStyles}
   ${SuccessNotificationStyles}
   ${TalkCallbackFormStyles}
-  ${TalkNotificationStyles}
   ${TalkOfflinePageStyles}
   ${TalkPhoneNumberStyles}
   ${TalkPhoneOnlyPageStyles}
