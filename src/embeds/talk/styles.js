e2885f3d483db5c9d3a3d4b9e0d90d2548628811
Alex Robinson <arobinson@zendesk.com>
2019-08-21 22:36:51 +1000

    Update Talk embed's OfflinePage component to use styled-components

--- src/embeds/talk/styles.js
+++ src/embeds/talk/styles.js
@@ -1,12 +1,10 @@
 import SuccessNotificationPageStyles from 'embeds/talk/pages/SuccessNotificationPage/styles.scss'
 import TalkCallbackFormStyles from 'embeds/talk/components/CallbackForm/styles.scss'
-import TalkOfflinePageStyles from 'embeds/talk/pages/OfflinePage/styles.scss'
 import TalkPhoneOnlyPageStyles from 'embeds/talk/pages/PhoneOnlyPage/styles.scss'
 
 const styles = `
   ${SuccessNotificationPageStyles}
   ${TalkCallbackFormStyles}
-  ${TalkOfflinePageStyles}
   ${TalkPhoneOnlyPageStyles}
 `
 
