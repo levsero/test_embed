d9790473431f7d4c49fb9d038ff74131be42a346
Apoorv Kansal <akansal@zendesk.com>
2019-09-09 15:45:19 +1000

    Add new loading bar content component

---
+++ src/components/LoadingBarContent/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyledSkeleton } from './styles'
+
+const LoadingBarContent = () => {
+  return (
+    <>
+      <StyledSkeleton width="50%" />
+      <StyledSkeleton />
+      <StyledSkeleton />
+      <StyledSkeleton />
+      <StyledSkeleton />
+    </>
+  )
+}
+
+export default LoadingBarContent
