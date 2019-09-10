b8166fcc6a1b420f57787b1488028af9d2785909
Alex Robinson <arobinson@zendesk.com>
2019-09-10 10:46:10 +1000

    Fix scroll hacks so it doesn't prevent from displaying on mobile safari

--- src/util/scrollHacks.js
+++ src/util/scrollHacks.js
@@ -3,6 +3,7 @@ import { win, document as doc } from 'utility/globals'
 let oldHostBodyPosition
 let oldWindowScrollY = null
 let scrollKillerActive = false
+let oldPositionTop, oldPositionBottom, oldPositionLeft, oldPositionRight
 
 function setWindowScroll(y) {
   if (oldWindowScrollY === null) {
@@ -21,14 +22,33 @@ function revertWindowScroll() {
 function setScrollKiller(active) {
   if (active) {
     if (!scrollKillerActive) {
+      // store the current values for the properties we are about to update so we can revert them when they are not
+      // needed anymore. This is so the customer's website is not affected.
       oldHostBodyPosition = doc.body.style.position
+      oldPositionTop = doc.body.style.top
+      oldPositionBottom = doc.body.style.bottom
+      oldPositionLeft = doc.body.style.left
+      oldPositionRight = doc.body.style.right
 
+      // position is set to fixed to prevent host page from scolling when user scrolls within the widget
       doc.body.style.position = 'fixed'
+
+      // on safari mobile, fixed position elements are affected by their fixed position parents. If the doc body has
+      // "overflow: hidden;" we need to make sure the body takes up the entire browser, so the full widget
+      // is visible for the user
+      doc.body.style.top = 0
+      doc.body.style.bottom = 0
+      doc.body.style.left = 0
+      doc.body.style.right = 0
       scrollKillerActive = true
     }
   } else {
     if (scrollKillerActive) {
       doc.body.style.position = oldHostBodyPosition
+      doc.body.style.top = oldPositionTop
+      doc.body.style.bottom = oldPositionBottom
+      doc.body.style.left = oldPositionLeft
+      doc.body.style.right = oldPositionRight
       scrollKillerActive = false
     }
   }
