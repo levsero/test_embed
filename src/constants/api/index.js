5906acabd36f73b792f2750e19407201a3d24a78
Wayne See <wsee@zendesk.com>
2019-09-13 16:11:46 +1000

    Fire callback when popout is opened

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -7,6 +7,7 @@ export const API_ON_CHAT_STATUS_NAME = 'status'
 export const API_ON_CHAT_UNREAD_MESSAGES_NAME = 'unreadMessages'
 export const API_ON_OPEN_NAME = 'open'
 export const API_ON_CHAT_DEPARTMENT_STATUS = 'departmentStatus'
+export const API_ON_CHAT_POPOUT = 'popout'
 
 // get api params
 export const API_GET_IS_CHATTING_NAME = 'isChatting'
