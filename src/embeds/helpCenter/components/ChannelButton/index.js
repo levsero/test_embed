a5d134f18ca4cd371c01e5000eafd651bb381af0
levsero <levsero@gmail.com>
2019-09-19 11:30:09 +1000

    Revert "Merge pull request #3266 from zendesk/EWW-676-update-channel-button"
    This reverts commit ddbae791a89490b7b8b563cc32ea3d2c2c9cf5d6, reversing
    changes made to 017cb7e5b31a8305074faf1d46a049ebc58862a1.

--- src/embeds/helpCenter/components/ChannelButton/index.js
+++ src/embeds/helpCenter/components/ChannelButton/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Button } from '@zendeskgarden/react-buttons'
 import { ButtonGroup } from 'component/button/ButtonGroup'
-import { onHelpCenterNextClick } from 'src/redux/modules/base'
+import { LoadingEllipses } from 'component/loading/LoadingEllipses'
+import { locals as styles } from './index.scss'
+import classNames from 'classnames'
+import { isMobileBrowser } from 'utility/devices'
+import { onHelpCenterNextClick as onClick } from 'src/redux/modules/base'
 import { getHelpCenterButtonLabel, getChatConnectionConnecting } from 'src/redux/modules/selectors'
 import { i18n } from 'service/i18n'
-import { ButtonContainer, StyledButton } from './styles'
-import { Inline } from '@zendeskgarden/react-loaders'
 
-const ChannelButton = ({ buttonLabel, isRTL, onClick, loading }) => {
+const ChannelButton = ({ buttonLabel, isRTL, onClick, loading, isMobile }) => {
+  const container = classNames(
+    styles.container,
+    isMobile ? styles.containerMobile : styles.containerDesktop
+  )
+
+  const buttonStyles = classNames(styles.button, {
+    [styles.disabledButton]: loading
+  })
+
   return (
-    <ButtonContainer>
-      <ButtonGroup rtl={isRTL}>
-        <StyledButton primary={true} onClick={!loading ? onClick : null}>
-          {loading ? <Inline color="white" size={24} /> : buttonLabel}
-        </StyledButton>
+    <div className={container}>
+      <ButtonGroup rtl={isRTL} containerClasses={styles.buttonGroup}>
+        <Button primary={true} className={buttonStyles} onClick={!loading ? onClick : null}>
+          {loading ? (
+            <LoadingEllipses useUserColor={false} itemClassName={styles.loadingAnimation} />
+          ) : (
+            buttonLabel
+          )}
+        </Button>
       </ButtonGroup>
-    </ButtonContainer>
+    </div>
   )
 }
 
@@ -24,17 +40,21 @@ ChannelButton.propTypes = {
   buttonLabel: PropTypes.string,
   isRTL: PropTypes.bool,
   loading: PropTypes.bool,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  isMobile: PropTypes.bool
 }
 
 ChannelButton.defaultProps = {
   buttonLabel: '',
   isRTL: false,
-  loading: false
+  loading: false,
+  onHelpCenterNextClick: () => {},
+  isMobile: false
 }
 
 const mapStateToProps = state => {
   return {
+    isMobile: isMobileBrowser(),
     buttonLabel: getHelpCenterButtonLabel(state),
     isRTL: i18n.isRTL(),
     loading: getChatConnectionConnecting(state)
@@ -42,7 +62,7 @@ const mapStateToProps = state => {
 }
 
 const actionCreators = {
-  onClick: onHelpCenterNextClick
+  onClick
 }
 
 const connectedComponent = connect(
--- src/embeds/helpCenter/styles.js
+++ src/embeds/helpCenter/styles.js
@@ -2,6 +2,7 @@ import ListStyles from 'embeds/helpCenter/components/List/styles.scss'
 import LegendStyles from 'embeds/helpCenter/components/Legend/styles.scss'
 import NoResultsMessageStyles from 'embeds/helpCenter/components/NoResultsMessage/styles.scss'
 import InitialSearchPageStyles from 'embeds/helpCenter/pages/SearchPromptPage/styles.scss'
+import ChannelButtonStyles from 'embeds/helpCenter/components/ChannelButton/index.scss'
 import SearchFormStyles from 'embeds/helpCenter/components/SearchForm/styles.scss'
 import SearchHeaderStyles from 'embeds/helpCenter/components/SearchHeader/styles.scss'
 import SearchFieldStyles from 'embeds/helpCenter/components/SearchField/styles.scss'
@@ -12,6 +13,7 @@ const styles = `
   ${ListStyles}
   ${LegendStyles}
   ${NoResultsMessageStyles}
+  ${ChannelButtonStyles}
   ${SearchFormStyles}
   ${SearchHeaderStyles}
   ${SearchIconButtonStyles}
