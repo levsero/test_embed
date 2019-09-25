f0c58bba76f95695d084186cddd22a95ef4d26e2
Alex Robinson <arobinson@zendesk.com>
2019-09-25 16:03:18 +1000

    Add e2e tests for the public api to set the locale

---
+++ e2e/spec/legacy-apis/set-locale.test.js
@@ -0,0 +1,40 @@
+import { queries } from 'pptr-testing-library'
+import widgetPage from '../../helpers/widget-page'
+import launcher from '../../helpers/launcher'
+
+describe('zE.setLocale(locale)', () => {
+  beforeEach(async () => {
+    await widgetPage.loadWithConfig('helpCenter')
+  })
+
+  it('updates the locale when called with a supported locale', async () => {
+    await page.evaluate(() => {
+      zE.setLocale('fr')
+    })
+    expect(await queries.getNodeText(await launcher.getLabel())).toEqual('Aide')
+  })
+
+  it('falls back to en-US when called with an unsupported locale', async () => {
+    await page.evaluate(() => {
+      zE.setLocale('fr')
+    })
+    expect(await queries.getNodeText(await launcher.getLabel())).toEqual('Aide')
+
+    await page.evaluate(() => {
+      zE.setLocale('unsupported locale')
+    })
+    expect(await queries.getNodeText(await launcher.getLabel())).toEqual('Help')
+  })
+
+  it('does nothing when no locale is provided', async () => {
+    await page.evaluate(() => {
+      zE.setLocale('fr')
+    })
+    expect(await queries.getNodeText(await launcher.getLabel())).toEqual('Aide')
+
+    await page.evaluate(() => {
+      zE.setLocale()
+    })
+    expect(await queries.getNodeText(await launcher.getLabel())).toEqual('Aide')
+  })
+})
