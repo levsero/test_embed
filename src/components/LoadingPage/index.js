ff39e86faa6601ea12c9d050584aa321b115fd4b
Lucas Hills <lucas@lucashills.com>
2019-11-14 11:30:14 +1100

    Adding zendesk logo + footer to the waiting screen, also include the minimise button for the title bar

--- src/components/LoadingPage/index.js
+++ src/components/LoadingPage/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-import { Widget, Header } from 'src/components/Widget'
+import { Widget, Header, Main, Footer } from 'src/components/Widget'
 import { CenteredDiv, LoadingSpinner } from './styles'
 
 const LoadingPage = () => {
   return (
     <Widget>
-      <Header />
-      <CenteredDiv>
-        <LoadingSpinner size="4rem" />
-      </CenteredDiv>
+      <Header showBackButton={false} />
+      <Main>
+        <CenteredDiv>
+          <LoadingSpinner size="4rem" />
+        </CenteredDiv>
+      </Main>
+      <Footer />
     </Widget>
   )
 }
