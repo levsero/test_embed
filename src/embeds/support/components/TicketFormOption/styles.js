5192c4a7660423b5140a3ec7def982ecbb79310b
Alex Robinson <arobinson@zendesk.com>
2019-11-14 16:19:04 +1100

    Fix styles for ticket form list

--- src/embeds/support/components/TicketFormOption/styles.js
+++ src/embeds/support/components/TicketFormOption/styles.js
@@ -5,36 +5,37 @@ import { zdColorGrey200, zdColorGrey100 } from '@zendeskgarden/css-variables'
 import { isMobileBrowser } from 'utility/devices'
 
 const mobileStyles = `
-  padding: ${24 / FONT_SIZE}rem ${20 / FONT_SIZE}rem !important;
   font-size: ${15 / FONT_SIZE}rem;
-
-  &:hover,
-  &:focus,
-  &:active {
-    background: ${zdColorGrey100};
-  }
+  padding: ${25 / FONT_SIZE}rem ${12 / FONT_SIZE}rem !important;
 `
 
-export const ListItem = styled.li`
+export const Button = styled.button`
+  border: 0;
   color: ${({ theme }) => `${theme.listColorStr} !important`};
-  fill: ${({ theme }) => `${theme.listColorStr} !important`};
   text-decoration-color: ${({ theme }) => `${theme.baseColor} !important`};
   border-bottom: ${1.1 / FONT_SIZE}rem solid ${zdColorGrey200};
-  padding: ${18 / FONT_SIZE}rem 0 !important;
+  padding: ${18 / FONT_SIZE}rem ${12 / FONT_SIZE}rem !important;
   line-height: ${18 / FONT_SIZE};
   list-style: none;
-  cursor: pointer !important;
+  display: block;
+  width: 100%;
+  text-align: start;
+  background-color: transparent;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   &:hover,
   &:focus,
   &:active {
     text-decoration: underline;
     color: ${({ theme }) => `${theme.listHighlightColorStr} !important`};
-    fill: ${({ theme }) => `${theme.listHighlightColorStr} !important`};
+    background-color: ${zdColorGrey100};
   }
 
-  &:last-child {
-    border: none;
+  &:not(:last-child) {
+    border-bottom: ${1.1 / FONT_SIZE}rem solid ${zdColorGrey200};
   }
 
   ${isMobileBrowser() && mobileStyles}
--- src/embeds/support/pages/TicketFormsPage/styles.js
+++ src/embeds/support/pages/TicketFormsPage/styles.js
@@ -1,30 +1,23 @@
 import styled from 'styled-components'
-import { zdColorGrey800, zdColorGrey200, zdColorGrey100 } from '@zendeskgarden/css-variables'
+import { zdColorGrey800, zdColorGrey200 } from '@zendeskgarden/css-variables'
 
 import { isMobileBrowser } from 'utility/devices'
 import { FONT_SIZE } from 'constants/shared'
-
-const mobileStyles = `
-  padding-left: ${20 / FONT_SIZE}rem !important;
-  padding-right: ${20 / FONT_SIZE}rem !important;
-  padding-bottom: ${24 / FONT_SIZE}rem;
-  font-size: ${15 / FONT_SIZE}rem;
-
-  &:hover,
-  &:focus,
-  &:active {
-    background: ${zdColorGrey100};
-  }
-`
+import { Main } from 'components/Widget'
 
 export const HeaderTitle = styled.h2`
-  padding-left: 0 !important;
-  padding-right: 0 !important;
+  padding-left: ${12 / FONT_SIZE}rem !important;
+  padding-right: ${12 / FONT_SIZE}rem !important;
   border-bottom: ${1.1 / FONT_SIZE}rem solid ${zdColorGrey200};
   padding-bottom: ${16 / FONT_SIZE}rem;
   color: ${zdColorGrey800};
-  font-size: ${14 / FONT_SIZE}rem;
+  font-size: ${(isMobileBrowser() ? 15 : 14) / FONT_SIZE}rem;
   font-weight: 700;
+`
 
-  ${isMobileBrowser() && mobileStyles}
+export const TicketFormsMain = styled(Main)`
+  &&& {
+    margin-left: 0;
+    margin-right: 0;
+  }
 `
