79d71d17fd392bb237f10abf97ae22b8153bc947
Lucas Hills <lhills@zendesk.com>
2019-11-19 16:08:45 +1100

    Fixed header height rendering issue when header includes search field

--- src/components/Widget/Header/HeaderView/index.js
+++ src/components/Widget/Header/HeaderView/index.js
@@ -4,7 +4,6 @@ import { FONT_SIZE } from 'constants/shared'
 import { isMobileBrowser } from 'utility/devices'
 
 const HeaderView = styled.div`
-  min-height: 3rem;
   border-radius: ${isMobileBrowser() ? 'none' : `${8 / FONT_SIZE}rem ${8 / FONT_SIZE}rem 0 0`};
   padding: ${8 / FONT_SIZE}rem ${14 / FONT_SIZE}rem;
   background: ${props => props.theme.headerColorStr} !important;
