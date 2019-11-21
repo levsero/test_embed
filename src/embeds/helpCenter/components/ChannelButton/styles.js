2326b147473bd97f364edd909d0812d9acad39ee
levsero <levsero@gmail.com>
2019-11-22 10:25:15 +1100

    use dots loader not inline

--- src/embeds/helpCenter/components/ChannelButton/styles.js
+++ src/embeds/helpCenter/components/ChannelButton/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { Button } from '@zendeskgarden/react-buttons'
 import { FONT_SIZE } from 'constants/shared'
-import { Inline } from '@zendeskgarden/react-loaders'
+import { Dots } from '@zendeskgarden/react-loaders'
 
 export const ButtonContainer = styled.div`
   position: relative !important;
@@ -13,6 +13,6 @@ export const StyledButton = styled(Button)`
   max-width: ${200 / FONT_SIZE}rem;
 `
 
-export const Loading = styled(Inline)`
+export const Loading = styled(Dots)`
   color: ${props => props.theme.buttonTextColorStr} !important;
 `
