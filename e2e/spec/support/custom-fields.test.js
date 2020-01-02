d6f38a31a4eb4f3149eba05bdba5a9e712680575
Wayne See <wsee@zendesk.com>
2020-01-02 22:08:05 +1100

    Add tests for tagger field

--- e2e/spec/support/custom-fields.test.js
+++ e2e/spec/support/custom-fields.test.js
@@ -271,4 +271,154 @@ describe('support custom fields', () => {
       })
     })
   })
+
+  describe('tagger', () => {
+    const openDropdown = async () => {
+      const doc = await widget.getDocument()
+      const dropdown = await queries.getByTestId(doc, 'dropdown-field')
+      await dropdown.click()
+    }
+
+    const select = async option => {
+      const doc = await widget.getDocument()
+      const item = await queries.getByText(doc, option)
+      await item.click()
+    }
+
+    describe('simple dropdown', () => {
+      const customFieldOptions = [
+        {
+          id: 64426144,
+          name: 'Sales',
+          raw_name: 'Sales',
+          value: 'one',
+          default: false
+        },
+        {
+          id: 64426154,
+          name: 'support',
+          raw_name: 'support',
+          value: 'two',
+          default: true
+        },
+        {
+          id: 63810110,
+          name: 'Test',
+          raw_name: 'Test',
+          value: 'test',
+          default: false
+        }
+      ]
+
+      test('option selected', async () => {
+        const field = createField({
+          type: 'tagger',
+          custom_field_options: customFieldOptions
+        })
+        const { expectSuccess } = await testCustomForm({ field })
+        await openDropdown()
+        await select('Sales')
+        await expectSuccess({
+          [field.id]: 'one'
+        })
+      })
+
+      test('default', async () => {
+        const field = createField({
+          type: 'tagger',
+          custom_field_options: customFieldOptions
+        })
+        const { expectSuccess } = await testCustomForm({ field })
+        await expectSuccess({
+          [field.id]: 'two'
+        })
+      })
+
+      test('allows empty selection when not required', async () => {
+        const field = createField({
+          type: 'tagger',
+          required_in_portal: false,
+          custom_field_options: customFieldOptions
+        })
+        const { expectSuccess } = await testCustomForm({ field })
+        await openDropdown()
+        await select('-')
+        await expectSuccess({
+          [field.id]: ''
+        })
+      })
+
+      // TODO: Fix the bug this test is testing
+      // This test should be enabled once this bug is fixed
+      test.skip('required but not selected', async () => {
+        const field = createField({
+          type: 'tagger',
+          required_in_portal: true,
+          custom_field_options: [customFieldOptions[0], customFieldOptions[2]]
+        })
+        const { expectSuccess, submit } = await testCustomForm({ field })
+        await submit()
+
+        const doc = await widget.getDocument()
+        await expect(await doc.$('body')).toMatch('Please enter a value.')
+
+        await openDropdown()
+        await select('Sales')
+        await expectSuccess({
+          [field.id]: 'one'
+        })
+      })
+    })
+
+    describe('nested dropdown', () => {
+      const customFieldOptions = [
+        {
+          id: 66706606,
+          name: 'what::happens::when::I::do::something::stupid::like::this',
+          raw_name: 'what::happens::when::I::do::something::stupid::like::this',
+          value: 'what__happens__when__i__do__something__stupid__like__this',
+          default: false
+        }
+      ]
+
+      test('selects nested option', async () => {
+        const field = createField({
+          type: 'tagger',
+          custom_field_options: customFieldOptions
+        })
+        const { expectSuccess } = await testCustomForm({ field })
+        await openDropdown()
+        await select('what')
+        await select('happens')
+        await select('when')
+        await select('I')
+        await select('do')
+        await select('something')
+        await select('stupid')
+        await select('like')
+        await select('this')
+        await expectSuccess({
+          [field.id]: 'what__happens__when__i__do__something__stupid__like__this'
+        })
+      })
+
+      test('navigates to previous', async () => {
+        const field = createField({
+          type: 'tagger',
+          custom_field_options: customFieldOptions
+        })
+        const { expectSuccess } = await testCustomForm({ field })
+        await openDropdown()
+        await select('what')
+        await select('happens')
+        await select('when')
+        await select('when')
+        await select('happens')
+        await select('what')
+        await expectSuccess({
+          [field.id]: ''
+        })
+      })
+    })
+  })
 })
