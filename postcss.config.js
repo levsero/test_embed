63c794933b1e33f324e8245ebfcf150051e47a5d
Briana Coppard <bcoppard@zendesk.com>
2020-01-02 14:07:07 +1100

    Bump css-loader and postcss-loader

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')({
-      browsers: ['last 2 versions', 'Firefox ESR', 'ie >= 10', 'Safari >= 8']
-    })
-  ]
+  plugins: [require('autoprefixer')]
 }
