c14a01b0a6b6c169647f3f11be695d31eb7fa071
Kari Matthews <kmatthews@zendesk.com>
2020-02-05 11:54:46 +1100

    Replace Hang up with End call

--- src/embeds/talk/pages/ClickToCallPage/__tests__/index.test.js
+++ src/embeds/talk/pages/ClickToCallPage/__tests__/index.test.js
@@ -1,44 +1,89 @@
-jest.mock('src/redux/modules/talk/talk-selectors')
 import { snapcallAPI } from 'snapcall'
-
 import React from 'react'
 import { fireEvent } from '@testing-library/react'
 
+import { TEST_IDS } from 'src/embeds/talk/constants'
 import { render } from 'utility/testHelpers'
 import { Component as ClickToCallPage } from '..'
 
-const renderComponent = props => render(<ClickToCallPage {...props} />)
+jest.mock('src/redux/modules/talk/talk-selectors')
+jest.mock('src/embeds/talk/hooks/useSnapcallUpdateTime', () => () => '00:00')
 
-describe('when the user starts a call', () => {
-  it('calls the snapcall start call function', () => {
-    jest.spyOn(snapcallAPI, 'startCall')
+let props = {}
 
-    const { getByText } = renderComponent({
-      buttonId: '1234'
-    })
+const renderComponent = () => render(<ClickToCallPage {...props} />)
 
-    fireEvent.click(getByText('Start Call'))
+describe('render', () => {
+  it('renders the expected title', () => {
+    const { getByText } = renderComponent()
 
-    expect(snapcallAPI.startCall).toHaveBeenCalledWith('1234')
+    expect(getByText('Call Us')).toBeInTheDocument()
   })
-})
 
-describe('when there is an average wait time', () => {
-  it('displays the average wait time', () => {
-    const { getByText } = renderComponent({
-      averageWaitTime: 'Average wait time: 10 minutes'
+  describe('when there is no call in progress', () => {
+    beforeEach(() => {
+      props.callStatus = null
+    })
+    describe('when the user starts a call', () => {
+      it('calls the snapcall start call function', () => {
+        jest.spyOn(snapcallAPI, 'startCall')
+
+        const { getByText } = renderComponent()
+
+        fireEvent.click(getByText('Start Call'))
+
+        expect(snapcallAPI.startCall).toHaveBeenCalled()
+      })
     })
 
-    expect(getByText('Average wait time: 10 minutes')).toBeInTheDocument()
+    describe('when there is an average wait time', () => {
+      it('displays the average wait time', () => {
+        props.averageWaitTime = 'Average wait time: 10 minutes'
+        const { getByText } = renderComponent()
+
+        expect(getByText('Average wait time: 10 minutes')).toBeInTheDocument()
+      })
+    })
+
+    describe('when there is no average wait time', () => {
+      it('does not display the average wait time', () => {
+        props.averageWaitTime = null
+        const { queryByText } = renderComponent()
+
+        expect(queryByText('Average wait time: 10 minutes')).toBeNull()
+      })
+    })
   })
-})
 
-describe('when there is no average wait time', () => {
-  it('does not display the average wait time', () => {
-    const { queryByText } = renderComponent({
-      averageWaitTime: null
+  describe('when there is a call in progress', () => {
+    beforeEach(() => {
+      props.callStatus = 'active'
     })
 
-    expect(queryByText('Average wait time: 10 minutes')).toBeNull()
+    it('renders the avatar', () => {
+      const { getByTestId } = renderComponent()
+
+      expect(getByTestId(TEST_IDS.ICON_AVATAR)).toBeInTheDocument()
+    })
+
+    it('renders the current call time', () => {
+      const { getByText } = renderComponent()
+
+      expect(getByText('00:00')).toBeInTheDocument()
+    })
+
+    it('renders the End call button', () => {
+      const { getByTestId } = renderComponent()
+
+      expect(getByTestId(TEST_IDS.BUTTON_HANG_UP)).toBeInTheDocument()
+    })
+
+    it('calls snapcallCallEnded on End call button click', () => {
+      const { getByText } = renderComponent()
+
+      fireEvent.click(getByText('End call'))
+
+      expect(snapcallAPI.endCall).toHaveBeenCalled()
+    })
   })
 })
