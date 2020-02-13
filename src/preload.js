4a2519ed010d68f1e35ffb6ed0e90ae3392e582c
Apoorv Kansal <akansal@zendesk.com>
2020-02-13 15:25:08 +1100

    Correctly access domain for EF config request

--- src/preload.js
+++ src/preload.js
@@ -2,7 +2,9 @@ import 'core-js/modules/es6.promise'
 import 'core-js/modules/es6.array.iterator'
 
 if (window.ACFetch) {
-  global.configRequest = window.ACFetch(`https://${window.document.zendesk.id}/embeddable/config`)
+  global.configRequest = window.ACFetch(
+    `https://${window.document.zendesk.web_widget.id}/embeddable/config`
+  )
 }
 
 import(/* webpackChunkName: "web_widget" */ './main')
