86c57b1720a7a22bce544b36135207ab6ae36ede
levsero <levsero@gmail.com>
2020-02-13 14:01:30 +1100

    fix feedback popup positiong

--- src/embeds/answerBot/pages/ArticlePage/styles.js
+++ src/embeds/answerBot/pages/ArticlePage/styles.js
@@ -4,9 +4,10 @@ import { SlideAppear } from 'component/transition/SlideAppear'
 
 const FeedbackContainer = styled(SlideAppear)`
   width: 100%;
-  position: absolute;
-  overflow: hidden;
-  border-radius: 0 0 ${8 / FONT_SIZE}rem ${8 / FONT_SIZE}rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 -${1 / FONT_SIZE}rem ${12 / FONT_SIZE}rem rgba(0, 0, 0, 0.08);
 `
 
 export { FeedbackContainer }
