f937664fc03f4166981e39f1b278d9f9d3179241
Alex Robinson <arobinson@zendesk.com>
2020-02-27 14:41:11 +1100

    Fix article rate popup rendering on IE11

--- src/embeds/answerBot/pages/ArticlePage/styles.js
+++ src/embeds/answerBot/pages/ArticlePage/styles.js
@@ -8,6 +8,7 @@ const FeedbackContainer = styled(SlideAppear)`
   justify-content: center;
   align-items: center;
   box-shadow: 0 -${1 / FONT_SIZE}rem ${12 / FONT_SIZE}rem rgba(0, 0, 0, 0.08);
+  flex-shrink: 0;
 `
 
 export { FeedbackContainer }
