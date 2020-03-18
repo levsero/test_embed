1dc5ebe86d8ed228204bd5494e7faf7afc5a6912
Wayne See <wsee@zendesk.com>
2020-03-19 09:00:34 +1100

    Parallelize e2e tests in CI

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,3 +1,22 @@
+const parallelism = require('ci-parallel-vars')
+const chunkd = require('chunkd')
+
+const limitTests = () => {
+  // JEST_TESTS env var is the console output of `npm run e2e --listTests --json`
+  let tests = process.env.JEST_TESTS
+  // the console output contains some extra lines that we want to ignore before
+  // we can parse the JSON input.
+  tests = tests.split('\n').filter(line => line.startsWith('['))
+  // sort the files so that the list is deterministic
+  tests = JSON.parse(tests[0]).sort((a, b) => {
+    return b.localeCompare(a)
+  })
+  tests = chunkd(tests, parallelism.index, parallelism.total)
+  return { testMatch: tests }
+}
+
+const options = parallelism && !process.env.SKIP_PARALLEL ? limitTests() : {}
+
 module.exports = {
   rootDir: './e2e',
   moduleDirectories: ['node_modules', './'],
@@ -7,5 +26,6 @@ module.exports = {
   setupFilesAfterEnv: ['expect-puppeteer', './setup.js'],
   launch: {
     headless: process.env.HEADLESS !== 'false'
-  }
+  },
+  ...options
 }
