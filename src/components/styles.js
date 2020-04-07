3fd72ed0bb7e689df443eb281e748dcee8fa9708
Wayne See <wsee@zendesk.com>
2020-04-07 12:31:20 +1000

    Update ZendeskLogo to use styled components

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,10 +1,8 @@
 import HelpCenterArticleStyles from './HelpCenterArticle/styles.scss'
-import ZendeskLogoStyles from './ZendeskLogo/styles.scss'
 import PopupNotificationStyles from './NotificationPopup/styles.scss'
 
 const styles = `
   ${HelpCenterArticleStyles}
-  ${ZendeskLogoStyles}
   ${PopupNotificationStyles}
 `
 
