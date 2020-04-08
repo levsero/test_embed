9bbeae744431a436ea951efb8986df9ba55afd8f
Briana Coppard <bcoppard@zendesk.com>
2020-04-08 14:31:18 +1000

    Add test for proactive chat on mobile

--- e2e/spec/chat/proactive.test.js
+++ e2e/spec/chat/proactive.test.js
@@ -43,3 +43,50 @@ test('first proactive chat message opens the widget', async () => {
   expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
   expect(await queries.queryByText(await launcher.getDocument(), '1 new')).toBeTruthy()
 })
+
+test('proactive chats show a notification on mobile', async () => {
+  await loadWidget()
+    .withPresets('chat')
+    .hiddenInitially()
+    .useMobile()
+    .load()
+  await zChat.online()
+  await launcher.waitForLauncherPill()
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
+
+  await agentJoinsChat('An agent')
+  await sendMessageFromAgent(true)
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
+  expect(await queries.queryByText(await widget.getDocument(), 'message from agent')).toBeTruthy()
+
+  const nextButton = await queries.queryByText(await widget.getDocument(), 'Reply')
+  await nextButton.click()
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeTruthy()
+})
+
+test('proactive chat notifications can be closed on mobile', async () => {
+  await loadWidget()
+    .withPresets('chat')
+    .hiddenInitially()
+    .useMobile()
+    .load()
+  await zChat.online()
+  await launcher.waitForLauncherPill()
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
+
+  await agentJoinsChat('An agent')
+  await sendMessageFromAgent(true)
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
+  expect(await queries.queryByText(await widget.getDocument(), 'message from agent')).toBeTruthy()
+
+  const closeButton = await queries.queryByText(await widget.getDocument(), 'Dismiss')
+  await closeButton.click()
+
+  expect(await queries.queryByText(await widget.getDocument(), 'Chat with us')).toBeFalsy()
+  expect(await queries.queryByText(await widget.getDocument(), 'message from agent')).toBeFalsy()
+})
