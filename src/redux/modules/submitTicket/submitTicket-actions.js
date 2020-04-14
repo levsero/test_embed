8630cd2a55e5e028e03eda2302cfbf230b04bfb0
Alex Robinson <arobinson@zendesk.com>
2020-04-14 13:44:01 +1000

    Update webWidget boot to use Support embed for fetching ticket fields

--- src/redux/modules/submitTicket/submitTicket-actions.js
+++ src/redux/modules/submitTicket/submitTicket-actions.js
@@ -1,13 +1,9 @@
-import _ from 'lodash'
 import {
   FORM_ON_CHANGE,
   TICKET_FORM_UPDATE,
   TICKET_SUBMISSION_REQUEST_SENT,
   TICKET_SUBMISSION_REQUEST_SUCCESS,
-  TICKET_SUBMISSION_REQUEST_FAILURE,
-  TICKET_FIELDS_REQUEST_SENT,
-  TICKET_FIELDS_REQUEST_SUCCESS,
-  TICKET_FIELDS_REQUEST_FAILURE
+  TICKET_SUBMISSION_REQUEST_FAILURE
 } from './submitTicket-action-types'
 import {
   getFormState,
@@ -24,37 +20,6 @@ export function handleFormChange(state) {
   return {
     type: FORM_ON_CHANGE,
     payload: state
-  }
-}
-
-export function getTicketFields(customFields, locale) {
-  return dispatch => {
-    const pathIds = customFields.all ? '' : `field_ids=${_.toString(customFields.ids)}&`
-    const path = `/embeddable/ticket_fields?${pathIds}locale=${locale}`
-    const httpData = {
-      method: 'get',
-      path,
-      timeout: 20000,
-      locale,
-      callbacks: {
-        done(res) {
-          dispatch({
-            type: TICKET_FIELDS_REQUEST_SUCCESS,
-            payload: JSON.parse(res.text)
-          })
-        },
-        fail() {
-          dispatch({
-            type: TICKET_FIELDS_REQUEST_FAILURE
-          })
-        }
-      }
-    }
-
-    http.get(httpData, false)
-    dispatch({
-      type: TICKET_FIELDS_REQUEST_SENT
-    })
   }
 }
 
