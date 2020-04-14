3568d43ec685a6b27c7aa447f6fc0f2e97a40543
Wayne See <wsee@zendesk.com>
2020-04-14 14:04:31 +1000

    Add chat authentication tests

--- src/redux/modules/chat/helpers/mockZChat.js
+++ src/redux/modules/chat/helpers/mockZChat.js
@@ -5,14 +5,46 @@ function mockZChat(zChat) {
     get(target, prop) {
       if (prop === 'getFirehose') {
         return mockFirehose(subscribers)
+      } else if (prop === 'init') {
+        return mockInit()
+      } else if (prop === 'setOnFirstReady') {
+        return mockSetOnFirstReady(this)
       } else if (prop === '__mock__') {
         return setMock(mocks)
       } else if (prop === '__fire__') {
-        return fireEvent(subscribers)
+        return fireEvent(subscribers, this)
       }
       return mocks[prop] || noop
     }
   })
+}
+
+function mockAuthentication(config) {
+  const jwtFn = config.authentication.jwt_fn
+  jwtFn(token => {
+    fetch('https://id.zopim.com/authenticated/web/jwt', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        account_key: config.account_key,
+        token: token
+      })
+    })
+  })
+}
+
+function mockInit() {
+  return config => {
+    if (config.authentication) {
+      mockAuthentication(config)
+    }
+  }
+}
+
+function mockSetOnFirstReady(target) {
+  return obj => (target.setOnFirstReady = obj)
 }
 
 function noop() {}
@@ -33,8 +65,35 @@ function mockFirehose(subscribers) {
   })
 }
 
-function fireEvent(subscribers) {
+function initialSettings(obj) {
+  const payload = {}
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const func = obj[key]
+      if (typeof func === 'function') {
+        payload[key] = func.call()
+      }
+    }
+  }
+  return payload
+}
+
+function fireEvent(subscribers, target) {
   return (event, payload) => {
+    if (
+      event === 'data' &&
+      payload.type === 'connection_update' &&
+      payload.detail === 'connected'
+    ) {
+      const detail = initialSettings(target.setOnFirstReady)
+      subscribers['data'].forEach(cb => {
+        cb({
+          type: 'initialSettings',
+          detail
+        })
+      })
+    }
     if (subscribers[event]) {
       subscribers[event].forEach(cb => {
         cb(payload)
