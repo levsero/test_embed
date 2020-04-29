f85eb1dd27407d34fa03bf6da3ca5a851a7321c1
Alex Robinson <arobinson@zendesk.com>
2020-04-29 12:00:20 +1000

    Split AuthenticatedProfile component out of UserProfile

---
+++ src/embeds/chat/components/AuthenticatedProfile/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ICONS, TEST_IDS } from 'constants/shared'
+import UserProfileDetails from 'embeds/chat/components/UserProfileDetails'
+import useTranslate from 'src/hooks/useTranslate'
+import { CHAT_SOCIAL_LOGIN_SCREENS } from 'constants/chat'
+import {
+  AuthenticatedProfileContainer,
+  LoadingSpinnerIcon,
+  LogoutIcon,
+  SocialAvatar
+} from './styles'
+
+const AuthenticatedProfile = ({ socialLogin, visitor, initiateSocialLogout }) => {
+  const translate = useTranslate()
+
+  const logoutButton =
+    socialLogin.screen === CHAT_SOCIAL_LOGIN_SCREENS.LOGOUT_PENDING ? (
+      <LoadingSpinnerIcon />
+    ) : (
+      <LogoutIcon onClick={initiateSocialLogout} data-testid={TEST_IDS.ICON_LOGOUT} />
+    )
+
+  return (
+    <>
+      <p>{translate('embeddable_framework.chat.form.common.field.social_login.title')}</p>
+      <AuthenticatedProfileContainer>
+        {socialLogin.authenticated && (
+          <SocialAvatar src={socialLogin.avatarPath} fallbackIcon={ICONS.AGENT_AVATAR} />
+        )}
+        <UserProfileDetails
+          isSociallyAuthenticated={socialLogin.authenticated}
+          displayName={visitor.display_name}
+          email={visitor.email}
+        />
+        {socialLogin.authenticated && logoutButton}
+      </AuthenticatedProfileContainer>
+    </>
+  )
+}
+
+AuthenticatedProfile.propTypes = {
+  socialLogin: PropTypes.shape({
+    avatarPath: PropTypes.string,
+    authenticated: PropTypes.bool,
+    screen: PropTypes.oneOf([...Object.values(CHAT_SOCIAL_LOGIN_SCREENS), ''])
+  }),
+  visitor: PropTypes.shape({
+    display_name: PropTypes.string,
+    email: PropTypes.string
+  }),
+  initiateSocialLogout: PropTypes.func
+}
+
+export default AuthenticatedProfile
--- src/embeds/chat/components/UserProfile/index.js
+++ src/embeds/chat/components/UserProfile/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import useTranslate from 'src/hooks/useTranslate'
 import SocialLogin from 'src/embeds/chat/components/SocialLogin'
-import UserProfileDetails from 'src/embeds/chat/components/UserProfileDetails'
-import { ICONS, TEST_IDS } from 'src/constants/shared'
-import { CHAT_SOCIAL_LOGIN_SCREENS } from 'constants/chat'
-import {
-  LoadingSpinnerIcon,
-  AuthenticatedProfileContainer,
-  DefaultProfileContainer,
-  SocialAvatar,
-  LogoutIcon
-} from './styles'
+import { DefaultProfileContainer } from './styles'
+import AuthenticatedProfile from 'embeds/chat/components/AuthenticatedProfile'
 
 const UserProfile = ({
   isAuthenticated,
@@ -23,35 +14,17 @@ const UserProfile = ({
   initiateSocialLogout,
   shouldSpaceSocialLogin
 }) => {
-  const translate = useTranslate()
-  const { authenticated: isSociallyAuthenticated, screen, avatarPath } = socialLogin
+  const { authenticated: isSociallyAuthenticated } = socialLogin
   const authenticatedProfile = isSociallyAuthenticated || isAuthenticated
-  const { display_name: displayName, email } = visitor
-
-  const logoutButton =
-    screen !== CHAT_SOCIAL_LOGIN_SCREENS.LOGOUT_PENDING ? (
-      <LogoutIcon onClick={initiateSocialLogout} data-testid={TEST_IDS.ICON_LOGOUT} />
-    ) : (
-      <LoadingSpinnerIcon />
-    )
 
   return (
     <div>
       {authenticatedProfile && (
-        <>
-          <p>{translate('embeddable_framework.chat.form.common.field.social_login.title')}</p>
-          <AuthenticatedProfileContainer>
-            {isSociallyAuthenticated && (
-              <SocialAvatar src={avatarPath} fallbackIcon={ICONS.AGENT_AVATAR} />
-            )}
-            <UserProfileDetails
-              isSociallyAuthenticated={isSociallyAuthenticated}
-              displayName={displayName}
-              email={email}
-            />
-            {isSociallyAuthenticated && logoutButton}
-          </AuthenticatedProfileContainer>
-        </>
+        <AuthenticatedProfile
+          visitor={visitor}
+          socialLogin={socialLogin}
+          initiateSocialLogout={initiateSocialLogout}
+        />
       )}
 
       {!authenticatedProfile && (
