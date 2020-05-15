b8c04fd0decf154539d4b74e4fc929b8145e4f14
Alex Robinson <arobinson@zendesk.com>
2020-05-15 16:46:33 +1000

    Upgrade @zendeskgarden/react-notifications to 8.9.0

--- src/embed/gardenStyles.js
+++ src/embed/gardenStyles.js
@@ -1,12 +1,10 @@
 import buttonStyles from '@zendeskgarden/react-buttons/dist/styles.css'
-import notificationStyles from '@zendeskgarden/react-notifications/dist/styles.css'
 import tooltipStyles from '@zendeskgarden/react-tooltips/dist/styles.css'
 import formStyles from '@zendeskgarden/react-forms/dist/styles.css'
 import dropdownStyles from '@zendeskgarden/react-dropdowns/dist/styles.css'
 
 export default `
   ${buttonStyles.toString()}
-  ${notificationStyles.toString()}
   ${tooltipStyles.toString()}
   ${formStyles.toString()}
   ${dropdownStyles.toString()}
