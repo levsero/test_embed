d8ad11a02e091504788ecd7887bf8074e39fac36
Adrian Yong <ayong@zendesk.com>
2020-05-19 11:11:18 +1000

    Fix back button and alert message icon for RTL
    Rerun travis
    
    remove comment
    
    Fix snapshot

--- src/embeds/support/components/AttachmentError/styles.js
+++ src/embeds/support/components/AttachmentError/styles.js
@@ -10,3 +10,7 @@ export const FileSize = styled.div`
   font-size: ${14 / FONT_SIZE}rem;
   color: ${zdColorGrey600};
 `
+
+export const ErrorBody = styled.div`
+  font-size: ${props => 14 / props.theme.fontSize}rem;
+`
