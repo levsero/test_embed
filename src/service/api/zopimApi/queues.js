182f3f8d9754028eacbc7334b2357039b1563c23
Wayne See <wsee@zendesk.com>
2020-05-28 15:28:50 +1000

    Override the zopim global object

--- src/service/api/zopimApi/queues.js
+++ src/service/api/zopimApi/queues.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import { zopimExistsOnPage } from './helpers'
 import ZopimApiError from 'errors/nonFatal/ZopimApiError'
 import { logAndTrackApiError } from 'src/service/api/errorHandlers'
 
@@ -7,24 +6,22 @@ export function setupZopimQueue(win) {
   // To enable $zopim api calls to work we need to define the queue callback.
   // When we inject the snippet we remove the queue method and just inject
   // the script tag.
-  if (!zopimExistsOnPage(win)) {
-    let $zopim
+  let $zopim
 
-    $zopim = win.$zopim = callback => {
-      if ($zopim.flushed) {
-        callback()
-      } else {
-        $zopim._.push(callback)
-      }
+  $zopim = win.$zopim = callback => {
+    if ($zopim.flushed) {
+      callback()
+    } else {
+      $zopim._.push(callback)
     }
-
-    $zopim.set = callback => {
-      $zopim.set._.push(callback)
-    }
-    $zopim._ = []
-    $zopim.set._ = []
-    $zopim._setByWW = true
   }
+
+  $zopim.set = callback => {
+    $zopim.set._.push(callback)
+  }
+  $zopim._ = []
+  $zopim.set._ = []
+  $zopim._setByWW = true
 }
 
 export function handleZopimQueue(win) {
