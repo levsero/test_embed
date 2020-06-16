a1462edd5342c93fee4ce6e51c9d67bb881c1a24
Wayne See <wsee@zendesk.com>
2020-06-16 16:42:01 +1000

    When preloading translation, use api value

--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,31 @@
 import 'core-js/modules/es6.promise'
 import 'core-js/modules/es6.array.iterator'
 
+global.fetchLocale = locale =>
+  import(
+    /* webpackChunkName: "locales/[request]" */ `./translation/locales/${locale.toLowerCase()}.json`
+  ).catch(() => {})
+global.__ZENDESK_CLIENT_I18N_GLOBAL = 'WW_I18N'
+
+let localeFetched = false
+
+if (document.zEQueue) {
+  for (let i = 0; i < document.zEQueue.length; i++) {
+    const args = document.zEQueue[i]
+    if (args[1] === 'setLocale' && args[2]) {
+      global.fetchLocale(args[2])
+      localeFetched = true
+    }
+  }
+}
+
 if (window.ACFetch) {
   global.configRequest = window
     .ACFetch(`https://${window.document.zendesk.web_widget.id}/embeddable/config`)
     .then(config => {
-      global.fetchLocale(config.locale ?? 'en-US')
+      if (!localeFetched) {
+        global.fetchLocale(config.locale ?? 'en-US')
+      }
 
       return {
         success: true,
@@ -16,11 +36,5 @@ if (window.ACFetch) {
       success: false
     }))
 }
-
-global.fetchLocale = locale =>
-  import(
-    /* webpackChunkName: "locales/[request]" */ `./translation/locales/${locale.toLowerCase()}.json`
-  ).catch(() => {})
-global.__ZENDESK_CLIENT_I18N_GLOBAL = 'WW_I18N'
 
 import(/* webpackChunkName: "web_widget" */ './main')
