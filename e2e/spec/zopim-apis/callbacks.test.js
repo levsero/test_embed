3fc37105a3f72992f3e4d1137a560f9ffd9c3b61
Wayne See <wsee@zendesk.com>
2020-06-18 11:10:29 +1000

    Add handling for nested zopim callbacks

---
+++ e2e/spec/zopim-apis/callbacks.test.js
@@ -0,0 +1,23 @@
+import { wait } from 'pptr-testing-library'
+import loadWidget from 'e2e/helpers/widget-page'
+
+test('callbacks in $zopim are called', async () => {
+  await loadWidget()
+    .withPresets('chat', 'helpCenter')
+    .evaluateAfterSnippetLoads(() => {
+      zE(() => {
+        $zopim(() => {
+          window.insideZopim = 'one'
+          $zopim(() => {
+            window.insideAnotherZopim = 'two'
+          })
+        })
+      })
+    })
+    .load()
+
+  await wait(async () => {
+    expect(await page.evaluate(() => window.insideZopim)).toEqual('one')
+    expect(await page.evaluate(() => window.insideAnotherZopim)).toEqual('two')
+  })
+})
