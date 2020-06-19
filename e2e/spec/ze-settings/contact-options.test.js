b384fe7b28cbdfcd2abbbded6e49fc46a277d319
Wayne See <wsee@zendesk.com>
2020-06-19 11:32:52 +1000

    Fix flaky contact options e2e test

--- e2e/spec/ze-settings/contact-options.test.js
+++ e2e/spec/ze-settings/contact-options.test.js
@@ -2,8 +2,9 @@ import loadWidget from 'e2e/helpers/widget-page'
 import zChat from 'e2e/helpers/zChat'
 import widget from 'e2e/helpers/widget'
 import launcher from 'e2e/helpers/launcher'
-import { search, waitForHelpCenter } from 'e2e/helpers/help-center-embed'
-import { mockSearchEndpoint } from 'e2e/helpers/help-center-embed'
+import { search, waitForHelpCenter, mockSearchEndpoint } from 'e2e/helpers/help-center-embed'
+import { waitForContactForm } from 'e2e/helpers/support-embed'
+import { waitForPrechatForm } from 'e2e/helpers/chat-embed'
 
 const setup = async (extraProducts = []) => {
   await loadWidget()
@@ -37,6 +38,8 @@ const testChatAndSubmitTicketInChannelChoicePage = () => {
   test('access chat product and go back', async () => {
     await widget.clickButton('Live chat')
 
+    await waitForPrechatForm()
+
     await widget.expectToSeeText('Chat with us')
     await widget.expectToSeeText('Start chat')
 
@@ -46,6 +49,8 @@ const testChatAndSubmitTicketInChannelChoicePage = () => {
 
   test('access contact form product and go back', async () => {
     await widget.clickButton('Leave a message')
+
+    await waitForContactForm()
 
     await widget.expectToSeeText('Leave us a message')
     await widget.expectToSeeText('How can we help you?')
