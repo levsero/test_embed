cb3fcf7e6847b9e29d3c1a8407d92d3f8b59fc11
Alex Robinson <arobinson@zendesk.com>
2020-06-25 16:34:42 +1000

    Fix chat popout styles

--- src/component/webWidget/styles.js
+++ src/component/webWidget/styles.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { isMobileBrowser } from 'utility/devices'
+import { isPopout } from 'utility/globals'
 
 const WidgetContainer = styled.div`
   height: ${props =>
     props.isFullHeight ? '100%' : `calc(100% - ${15 / props.theme.fontSize}rem)`};
-  padding: ${isMobileBrowser() ? 0 : '0 10px'};
+
+  padding: ${props => (props.theme.isMobile || isPopout() ? 0 : '0 10px')};
 `
 
 WidgetContainer.propTypes = {
