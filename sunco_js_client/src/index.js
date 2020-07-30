00adcdccf505a67b5dc1fe0d5b4ff6142b409c66
Lucas Hills <lucas@lucashills.com>
2020-07-30 12:14:51 +1000

    No longer needing to export the socket client

--- sunco_js_client/src/index.js
+++ sunco_js_client/src/index.js
@@ -1,7 +1,3 @@
 import Sunco from './Sunco'
-import SocketClient from './socket/SocketClient'
 
-export default {
-  Sunco,
-  SocketClient //temp - will move this into an appUser API call soon
-}
+export default Sunco
