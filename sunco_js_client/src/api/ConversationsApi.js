45226440b2d2472abdb42036f84bb4e942e9040f
Lucas Hills <lucas@lucashills.com>
2020-08-31 09:34:19 +1000

    Including Conversations APIs - we may not need them for the messenger, but it makes sense for the client

--- sunco_js_client/src/api/ConversationsApi.js
+++ sunco_js_client/src/api/ConversationsApi.js
@@ -1,17 +1,28 @@
 import BaseApi from './BaseApi'
+import storage from '../utils/storage'
 
 class ConversationsApi extends BaseApi {
   list(appUserId) {
     return this.request({
       method: 'GET',
-      path: `/sdk/v2/apps/${this.appId}/appusers/${appUserId}/conversations`
+      path: `/sdk/v2/apps/${this.appId}/appusers/${appUserId}/conversations`,
+      headers: {
+        Authorization: `Basic ${btoa(
+          `${appUserId}:${storage.getItem(`${this.integrationId}.sessionToken`)}`
+        )}`
+      }
     })
   }
 
   create(appUserId) {
     return this.request({
       method: 'POST',
-      path: `/sdk/v2/apps/${this.appId}/appusers/${appUserId}/conversations`
+      path: `/sdk/v2/apps/${this.appId}/appusers/${appUserId}/conversations`,
+      headers: {
+        Authorization: `Basic ${btoa(
+          `${appUserId}:${storage.getItem(`${this.integrationId}.sessionToken`)}`
+        )}`
+      }
     })
   }
 }
