3bc29c1a7c19064d0bed2ff966563135b7d0d653
Lucas Hills <2potatocakes@gmail.com>
2020-09-22 13:04:01 +1000

    Updated to use new color variables after a recent rebase

--- src/apps/messenger/features/sunco-components/MessageBubble/styles.js
+++ src/apps/messenger/features/sunco-components/MessageBubble/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const radius = props => props.theme.messenger.borderRadii.textMessage
 
 const Bubble = styled.div`
-  border-color: ${props => props.theme.messenger.colors.messageBorder};
   width: fit-content;
   margin-top: ${props => props.theme.messenger.space.xxxs};
   max-width: ${props => props.theme.messenger.space.messageMaxWidth};
@@ -12,7 +11,7 @@ const Bubble = styled.div`
 const PrimaryParticipantBubble = styled(Bubble)`
   background-color: ${props => props.theme.messenger.colors.message};
   color: ${props => props.theme.messenger.colors.messageText};
-  align-self: flex-end;
+
   ${props => {
     switch (props.shape) {
       case 'standalone':
@@ -28,8 +27,10 @@ const PrimaryParticipantBubble = styled(Bubble)`
 `
 
 const OtherParticipantBubble = styled(Bubble)`
-  background-color: ${props => props.theme.messenger.colors.primaryMessage};
-  color: ${props => props.theme.messenger.colors.primaryMessageText};
+  border-color: ${props => props.theme.messenger.colors.otherParticipantMessageBorder};
+  background-color: ${props => props.theme.messenger.colors.otherParticipantMessage};
+  color: ${props => props.theme.messenger.colors.otherParticipantMessageText};
+
   ${props => {
     switch (props.shape) {
       case 'standalone':
