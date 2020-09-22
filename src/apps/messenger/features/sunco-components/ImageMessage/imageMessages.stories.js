4462b7fa9db1e2cbef7b92bba660063318ab3b8e
Lucas Hills <2potatocakes@gmail.com>
2020-09-22 12:24:21 +1000

    Including Storybook story for testing/developing with

---
+++ src/apps/messenger/features/sunco-components/ImageMessage/imageMessages.stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ImageMessage from './'
+export default {
+  title: 'Sunco/Image Message',
+  component: ImageMessage
+}
+
+const Template = args => <ImageMessage {...args} />
+
+export const StandaloneImageWithText = Template.bind()
+StandaloneImageWithText.args = {
+  isPrimaryParticipant: false,
+  shape: 'standalone',
+  mediaUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/b/be/Emu_in_the_wild-1%2B_%282153629669%29.jpg',
+  text: 'Emus are lovely'
+}
