80c5f17559c0cec16b41c77c6d7c3904b8debd9e
Lucas Hills <2potatocakes@gmail.com>
2020-09-22 16:24:21 +1000

    Added another storybook story for testing with no text

--- src/apps/messenger/features/sunco-components/ImageMessage/imageMessages.stories.js
+++ src/apps/messenger/features/sunco-components/ImageMessage/imageMessages.stories.js
@@ -15,3 +15,12 @@ StandaloneImageWithText.args = {
     'https://upload.wikimedia.org/wikipedia/commons/b/be/Emu_in_the_wild-1%2B_%282153629669%29.jpg',
   text: 'Emus are lovely'
 }
+
+export const StandaloneImageWithNoText = Template.bind()
+StandaloneImageWithNoText.args = {
+  isPrimaryParticipant: false,
+  shape: 'standalone',
+  mediaUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/b/be/Emu_in_the_wild-1%2B_%282153629669%29.jpg',
+  text: ''
+}
