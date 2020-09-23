aecf1361d05c576b5cd06faffb3c60b6485307a2
Lucas Hills <2potatocakes@gmail.com>
2020-09-23 17:27:20 +1000

    Added storybook story for message bubble to help highlight their behaviour with and without content

---
+++ src/apps/messenger/features/sunco-components/MessageBubble/MessageBubble.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { MESSAGE_BUBBLE_SHAPES } from 'src/apps/messenger/features/sunco-components/constants'
+import MessageBubble from './'
+export default {
+  title: 'Sunco/Message Bubble',
+  component: MessageBubble,
+  argTypes: {
+    shape: {
+      defaultValue: MESSAGE_BUBBLE_SHAPES.standalone,
+      control: {
+        type: 'inline-radio',
+        options: Object.values(MESSAGE_BUBBLE_SHAPES)
+      }
+    }
+  }
+}
+
+export const Empty = args => <MessageBubble {...args} />
+
+export const WithText = args => (
+  <MessageBubble {...args}>
+    <p>Some dummy text</p>
+  </MessageBubble>
+)
+
+export const WithPaddedText = args => (
+  <MessageBubble {...args}>
+    <p style={{ padding: '8px 12px' }}>Some padded text</p>
+  </MessageBubble>
+)
