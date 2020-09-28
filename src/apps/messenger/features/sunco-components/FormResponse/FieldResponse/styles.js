6c4bc2f71bfa7cba0cd1c1f61726d28dd91bf108
Alex Robinson <arobinson@zendesk.com>
2020-09-28 16:24:13 +1000

    Update form response text to wrap

--- src/apps/messenger/features/sunco-components/FormResponse/FieldResponse/styles.js
+++ src/apps/messenger/features/sunco-components/FormResponse/FieldResponse/styles.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 const Label = styled.div`
   font-weight: ${props => props.theme.fontWeights.semibold};
   margin-bottom: ${props => props.theme.messenger.space.small};
+  word-break: break-word;
 `
 
-const Value = styled.div``
+const Value = styled.div`
+  word-break: break-word;
+`
 
 export { Label, Value }
