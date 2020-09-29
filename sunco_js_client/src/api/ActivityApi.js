b36149fea7e509a1e6902d480b34eea764c1e3a0
Alex Robinson <arobinson@zendesk.com>
2020-09-29 17:24:10 +1000

    Add support for sending activity events in the Sunco client

---
+++ sunco_js_client/src/api/ActivityApi.js
@@ -0,0 +1,43 @@
+import BaseApi from './BaseApi'
+import { getClientInfo, getSessionId } from '../utils/device'
+import storage from '../utils/storage'
+
+class MessagesApi extends BaseApi {
+  constructor(options, appUserId, conversationId) {
+    super(options)
+    this.appUserId = appUserId
+    this.conversationId = conversationId
+  }
+
+  sendActivity(activity) {
+    return this.request({
+      method: 'POST',
+      path: `/sdk/v2/apps/${this.appId}/conversations/${this.conversationId}/activity`,
+
+      headers: {
+        Authorization: `Basic ${btoa(
+          `${this.appUserId}:${storage.getItem(`${this.integrationId}.sessionToken`)}`
+        )}`
+      },
+      data: {
+        author: {
+          role: 'appUser',
+          appUserId: this.appUserId,
+          client: getClientInfo(this.integrationId),
+          sessionId: getSessionId()
+        },
+        activity
+      }
+    })
+  }
+
+  startTyping() {
+    return this.sendActivity({ type: 'typing:start' })
+  }
+
+  stopTyping() {
+    return this.sendActivity({ type: 'typing:stop' })
+  }
+}
+
+export default MessagesApi
