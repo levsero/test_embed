3a3db6efc34da8b598c53c9f38ad351aea043c97
Timothy Patullock <tpatullock@zendesk.com>
2020-10-02 16:02:07 +1000

    fix avatar styling

--- src/apps/messenger/features/sunco-components/Avatar/styles.js
+++ src/apps/messenger/features/sunco-components/Avatar/styles.js
@@ -4,7 +4,9 @@ import { Avatar } from '@zendeskgarden/react-avatars'
 const Container = styled(Avatar)`
   width: ${props => props.theme.messenger.iconSizes.xl};
   height: ${props => props.theme.messenger.iconSizes.xl};
-  margin-right: ${props => props.theme.messenger.space.xxs};
+  max-width: ${props => props.theme.messenger.iconSizes.xl};
+  max-height: ${props => props.theme.messenger.iconSizes.xl};
+  margin-right: ${props => props.theme.messenger.space.xxs} !important;
   align-self: flex-end;
 `
 
