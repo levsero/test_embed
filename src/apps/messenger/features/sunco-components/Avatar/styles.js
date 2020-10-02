bdac687b4db3c3097c49f1b658be172e830ff4c1
Timothy Patullock <tpatullock@zendesk.com>
2020-10-02 16:06:06 +1000

    override Figure styling

--- src/apps/messenger/features/sunco-components/Avatar/styles.js
+++ src/apps/messenger/features/sunco-components/Avatar/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import { Avatar } from '@zendeskgarden/react-avatars'
 
 const Container = styled(Avatar)`
-  width: ${props => props.theme.messenger.iconSizes.xl};
-  height: ${props => props.theme.messenger.iconSizes.xl};
+  width: ${props => props.theme.messenger.iconSizes.xl} !important;
+  height: ${props => props.theme.messenger.iconSizes.xl} !important;
   max-width: ${props => props.theme.messenger.iconSizes.xl};
   max-height: ${props => props.theme.messenger.iconSizes.xl};
   margin-right: ${props => props.theme.messenger.space.xxs} !important;
