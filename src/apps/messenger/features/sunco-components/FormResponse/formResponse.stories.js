19d574be2dd418d17fd14246a6123d9a84f709dd
levsero <levsero@gmail.com>
2020-10-09 15:16:00 +1100

    fix form response story

--- src/apps/messenger/features/sunco-components/FormResponse/formResponse.stories.js
+++ src/apps/messenger/features/sunco-components/FormResponse/formResponse.stories.js
@@ -14,12 +14,29 @@ TextFieldOnlyFormResponse.args = {
     {
       _id: 'asdf87asdf76as8d7f6ds7af876',
       label: 'Your first name',
-      text: 'Cordelia'
+      text: 'Cordelia',
+      type: 'text'
     },
     {
       _id: 'asd87sd897s897ds8d79',
       label: 'Your last name',
-      text: 'the greyhound'
+      text: 'the greyhound',
+      type: 'text'
+    },
+    {
+      _id: 'asd87sd897s897ds8d89',
+      label: 'email address',
+      email: 'greyhounds@zendesk.com',
+      type: 'email'
+    },
+    {
+      _id: 'asd87sd897s897ds8d79',
+      label: 'favourite teams',
+      select: [
+        { _id: 'asd87sd897s897ds8d70', label: 'taipan' },
+        { _id: 'asd87sd897s897ds8d70', label: 'emu' }
+      ],
+      type: 'select'
     }
   ],
   isFirstInGroup: true,
