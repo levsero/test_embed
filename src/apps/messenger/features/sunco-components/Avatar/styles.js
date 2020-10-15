281935fc82d154373caeecb6e2cc7aaae74c0434
Kari Matthews <kmatthews@zendesk.com>
2020-10-15 13:27:04 +1100

    Unsquish avatar on form messages

--- src/apps/messenger/features/sunco-components/Avatar/styles.js
+++ src/apps/messenger/features/sunco-components/Avatar/styles.js
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 import { Avatar } from '@zendeskgarden/react-avatars'
 
 const Container = styled(Avatar)`
-  width: ${props => props.theme.messenger.iconSizes.xl} !important;
-  height: ${props => props.theme.messenger.iconSizes.xl} !important;
-  max-width: ${props => props.theme.messenger.iconSizes.xl};
-  max-height: ${props => props.theme.messenger.iconSizes.xl};
-  margin-right: ${props => props.theme.messenger.space.xxs} !important;
   align-self: flex-end;
+  flex-shrink: 0;
+
+  && {
+    width: ${props => props.theme.messenger.iconSizes.xl};
+    height: ${props => props.theme.messenger.iconSizes.xl};
+    margin-right: ${props => props.theme.messenger.space.xxs};
+  }
 `
 
 const Image = styled.img``
