fb33ec73c70825c10ab347b853403e8b298a74d5
Timothy Patullock <tpatullock@zendesk.com>
2020-10-20 16:06:21 +1100

    sitting with sixteen

--- src/apps/messenger/features/sunco-components/Receipts/PrimaryParticipantReceipt/styles.js
+++ src/apps/messenger/features/sunco-components/Receipts/PrimaryParticipantReceipt/styles.js
@@ -6,7 +6,7 @@ const Layout = styled.div`
   display: flex;
   flex-direction: row;
   margin-top: ${props => props.theme.messenger.space.xxxs};
-  margin-right: ${props => props.theme.messenger.space.md};
+  margin-right: ${props => props.theme.messenger.space.sixteen};
   justify-content: flex-end;
 `
 
