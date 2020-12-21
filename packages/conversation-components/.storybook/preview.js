2ec12700df4696fe64f304ab16a4c163a7a8d889
Lucas Hills <lhills@zendesk.com>
2020-12-21 21:56:30 +1100

    Moved MessengerContainer into shared decorators file so can be imported only when needed

--- packages/conversation-components/.storybook/preview.js
+++ packages/conversation-components/.storybook/preview.js
@@ -1,25 +1,10 @@
-import { rem } from 'polished'
 import ThemeProvider from '../src/ThemeProvider'
-import { baseFontSize } from '../src/constants'
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
-}
 
 export const decorators = [
   Story => {
     return (
       <ThemeProvider>
-        <div
-          style={{
-            width: rem('380px', 16),
-            height: rem('700px', 16),
-            border: '1px solid black',
-            position: 'relative'
-          }}
-        >
-          <Story />
-        </div>
+        <Story />
       </ThemeProvider>
     )
   }
