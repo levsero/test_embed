a0b80bfe7e6a576d79418644f671fd8bae55a9fd
Lucas Hills <lhills@zendesk.com>
2020-12-22 21:58:36 +1100

    Export new components via the conversation-components package

--- packages/conversation-components/src/index.js
+++ packages/conversation-components/src/index.js
@@ -1,7 +1,9 @@
 import Avatar from './Avatar'
 import Banner from './Banner'
 import Carousel from './Carousel'
+import Header from './messenger/Header'
 import Label from './Label'
+import MessengerContainer from './messenger/MessengerContainer'
 import ThemeProvider from './ThemeProvider'
 import {
   FORM_MESSAGE_STATUS,
@@ -10,6 +12,6 @@ import {
   MESSAGE_STATUS
 } from './constants'
 
-export { Avatar, Banner, Carousel, Label, ThemeProvider }
+export { Avatar, Banner, Carousel, Header, Label, MessengerContainer, ThemeProvider }
 
 export { FORM_MESSAGE_STATUS, BANNER_STATUS, MESSAGE_BUBBLE_SHAPES, MESSAGE_STATUS }
