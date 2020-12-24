61c53587391842427a71b25a05ddf8d212e5fce0
Lucas Hills <2potatocakes@gmail.com>
2020-12-24 17:15:12 +1100

    Converted the Composer to a forwardRef component, removed redux references and added test

---
+++ packages/conversation-components/src/Composer/index.js
@@ -0,0 +1,87 @@
+import React, { useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+import { KEY_CODES } from '@zendeskgarden/react-selection'
+
+import { Container, Textarea, SendIcon, Field, SendButton } from './styles'
+
+const triggerOnEnter = callback => e => {
+  if (e.keyCode === KEY_CODES.ENTER && !e.shiftKey) {
+    e.preventDefault()
+    callback(e)
+  }
+}
+
+const Composer = React.forwardRef(
+  (
+    {
+      disabled = false,
+      placeholder = 'Type a message',
+      inputAriaLabel = 'Type a message',
+      value = '',
+      sendButtonTooltip = 'Send message',
+      sendButtonAriaLabel = 'Send message',
+      minRows = 1,
+      maxRows = 5,
+      onSendMessage = _value => {},
+      onChange = _event => {}
+    },
+    ref
+  ) => {
+    const localRef = useRef(null)
+    const inputRef = ref || localRef
+    const [composerValue, setComposerValue] = useState(value)
+
+    const handleChange = event => {
+      setComposerValue(event.target.value)
+      onChange(event)
+    }
+
+    const handleSubmit = () => {
+      onSendMessage(composerValue)
+      setComposerValue('')
+      // inputRef.current.focus()
+    }
+
+    return (
+      <Container>
+        <Field>
+          <Textarea
+            maxRows={maxRows}
+            minRows={minRows}
+            onKeyDown={triggerOnEnter(handleSubmit)}
+            onChange={handleChange}
+            value={composerValue}
+            placeholder={placeholder}
+            aria-label={inputAriaLabel}
+            ref={inputRef}
+            disabled={disabled}
+          />
+          {composerValue && !disabled && (
+            <SendButton
+              onClick={handleSubmit}
+              aria-label={sendButtonAriaLabel}
+              title={sendButtonTooltip}
+            >
+              <SendIcon />
+            </SendButton>
+          )}
+        </Field>
+      </Container>
+    )
+  }
+)
+
+Composer.propTypes = {
+  disabled: PropTypes.bool,
+  placeholder: PropTypes.string,
+  inputAriaLabel: PropTypes.string,
+  sendButtonAriaLabel: PropTypes.string,
+  sendButtonTooltip: PropTypes.string,
+  minRows: PropTypes.number,
+  maxRows: PropTypes.number,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onSendMessage: PropTypes.func
+}
+
+export default Composer
---
+++ packages/conversation-components/src/Composer/styles.js
@@ -0,0 +1,109 @@
+import styled, { css } from 'styled-components'
+import { rem, rgba } from 'polished'
+import { Textarea, Field } from '@zendeskgarden/react-forms'
+import { IconButton } from '@zendeskgarden/react-buttons'
+
+import SendIcon from './send-icon.svg'
+
+const StyledSendIcon = styled(SendIcon)``
+
+// Send button size needs to match the height of the text area when it is a single row
+// To do this, add the line height, padding and border together
+const sendButtonSize = css`
+  ${props => `
+  calc(${props.theme.messenger.lineHeights.md} + ${props.theme.messenger.space.sm} + ${props.theme.messenger.space.sm} + ${props.theme.borderWidths.sm})
+`}
+`
+
+const SendButton = styled(IconButton)`
+  &&& {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    height: ${sendButtonSize};
+    width: ${sendButtonSize};
+    border-radius: ${props => rem(22, props.theme.messenger.baseFontSize)};
+    color: ${props => props.theme.messenger.primary};
+
+    ${StyledSendIcon} {
+      height: ${props => props.theme.messenger.iconSizes.md};
+      width: ${props => props.theme.messenger.iconSizes.md};
+      margin-left: ${props => props.theme.messenger.space.xxs};
+
+      g {
+        fill: ${props => props.theme.messenger.colors.primary};
+      }
+    }
+
+    &:hover,
+    &:focus,
+    &:active {
+      background-color: transparent;
+    }
+
+    &:focus,
+    &:active {
+      box-shadow: inset
+        ${props => props.theme.shadows.md(rgba(props.theme.messenger.colors.action, 0.35))};
+    }
+  }
+`
+
+const StyledTextarea = styled(Textarea)`
+  &&& {
+    border-radius: ${props => rem(22, props.theme.messenger.baseFontSize)};
+    border: ${props => props.theme.borders.sm} rgb(216, 220, 222);
+    background: transparent;
+    min-height: auto;
+    padding: ${props => props.theme.messenger.space.sm}
+      ${props => props.theme.messenger.space.sixteen};
+    padding-right: ${sendButtonSize};
+
+    box-shadow: none;
+    line-height: ${props => props.theme.messenger.lineHeights.md};
+    font-size: ${props => props.theme.messenger.fontSizes.md};
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+
+    &:not(:disabled) {
+      :hover {
+        border: ${props => props.theme.borders.sm} ${props => props.theme.palette.grey[500]};
+      }
+
+    :focus,
+    :active,
+    &[data-garden-focus-visible] {
+      border: ${props => props.theme.borders.sm} ${props => props.theme.messenger.colors.action};
+      box-shadow: ${props =>
+        props.theme.shadows.md(rgba(props.theme.messenger.colors.action, 0.35))};
+    }
+    }
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: ${props => props.theme.messenger.fontFamily};
+  padding: ${props => props.theme.messenger.space.sm}
+    ${props => props.theme.messenger.space.sixteen};
+  flex-shrink: 0;
+  position: relative;
+`
+
+const StyledField = styled(Field)`
+  display: flex;
+  flex-grow: 1;
+  position: relative;
+`
+
+export {
+  Container,
+  StyledTextarea as Textarea,
+  StyledSendIcon as SendIcon,
+  StyledField as Field,
+  SendButton
+}
