c0bda05ed826c8438e7bc1e1bd9a0e4386e2cec8
Alex Robinson <arobinson@zendesk.com>
2021-01-13 10:54:27 +1100

    Add comment about why useSelector is needed in the useTranslate hook

--- packages/framework/src/apps/messenger/features/i18n/useTranslate.js
+++ packages/framework/src/apps/messenger/features/i18n/useTranslate.js
@@ -3,6 +3,7 @@ import i18n from 'src/framework/services/i18n'
 import { getLocale } from 'src/apps/messenger/features/i18n/store'
 
 const useTranslate = () => {
+  // This selector is needed to trigger the component to re-render when the locale changes
   useSelector(getLocale)
 
   return i18n.translate
