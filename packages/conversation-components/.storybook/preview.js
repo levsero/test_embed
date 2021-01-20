68d7ff48c49e855d7244cc9c06a06e0eb802d94c
Alex Robinson <arobinson@zendesk.com>
2021-01-20 13:37:58 +1100

    Add basic support for switching locale direction in storybook

--- packages/conversation-components/.storybook/preview.js
+++ packages/conversation-components/.storybook/preview.js
@@ -1,11 +1,30 @@
 import ThemeProvider from 'src/ThemeProvider'
+import { useEffect } from 'react'
 
 export const decorators = [
-  Story => {
+  (Story, storyContext) => {
+    useEffect(() => {
+      document
+        .querySelector('#root')
+        ?.firstChild?.firstChild?.setAttribute?.('dir', storyContext.globals.localeDir)
+    })
+
     return (
-      <ThemeProvider>
+      <ThemeProvider rtl={storyContext.globals.localeDir === 'rtl'}>
         <Story />
       </ThemeProvider>
     )
   }
 ]
+
+export const globalTypes = {
+  localeDir: {
+    name: 'Locale direction',
+    description: 'Locale direction',
+    defaultValue: 'ltr',
+    toolbar: {
+      title: 'Locale direction',
+      items: ['ltr', 'rtl']
+    }
+  }
+}
