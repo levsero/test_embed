3d2ad489e6a505ef24dce1607b8557ea7eb79075
Alex Robinson <arobinson@zendesk.com>
2021-01-20 13:18:44 +1100

    Add dir and lang attributes to each iframe for the messenger

--- packages/framework/src/apps/messenger/features/themeProvider/index.js
+++ packages/framework/src/apps/messenger/features/themeProvider/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import bedrockCSS from '@zendeskgarden/css-bedrock'
 import { ThemeProvider as SuncoThemeProvider } from '@zendesk/conversation-components'
-import { CurrentFrameConsumer } from 'src/framework/components/Frame'
+import { useCurrentFrame } from 'src/framework/components/Frame'
 import { getMessengerColors } from 'src/apps/messenger/features/themeProvider/store'
 import { getIsFullScreen } from 'src/apps/messenger/features/responsiveDesign/store'
 import { baseFontSize, baseFontSizeFullScreen } from 'src/apps/messenger/constants'
@@ -25,26 +25,28 @@ const ThemeProvider = ({ children }) => {
   const messengerColors = useSelector(getMessengerColors)
   const isFullScreen = useSelector(getIsFullScreen)
   const currentBaseFontSize = isFullScreen ? baseFontSizeFullScreen : baseFontSize
+  const frame = useCurrentFrame()
+
+  useEffect(() => {
+    frame.document.documentElement.setAttribute('dir', i18n.isRTL() ? 'rtl' : 'ltr')
+    frame.document.documentElement.setAttribute('lang', i18n.getLocale())
+  }, [i18n.getLocale()])
 
   return (
-    <CurrentFrameConsumer>
-      {frame => (
-        <SuncoThemeProvider
-          currentFrame={frame}
-          primaryColor={messengerColors.primary}
-          messageColor={messengerColors.message}
-          actionColor={messengerColors.action}
-          baseFontSize={currentBaseFontSize}
-          rtl={i18n.isRTL()}
-        >
-          <>
-            <GlobalStyles />
+    <SuncoThemeProvider
+      currentFrame={frame}
+      primaryColor={messengerColors.primary}
+      messageColor={messengerColors.message}
+      actionColor={messengerColors.action}
+      baseFontSize={currentBaseFontSize}
+      rtl={i18n.isRTL()}
+    >
+      <>
+        <GlobalStyles />
 
-            {children}
-          </>
-        </SuncoThemeProvider>
-      )}
-    </CurrentFrameConsumer>
+        {children}
+      </>
+    </SuncoThemeProvider>
   )
 }
 
