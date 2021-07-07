e120639afadf482e2aa336602fc5a39e175aab0d
Matt Doyle <matt.doyle@zendesk.com>
2021-07-07 16:28:54 +1000

    Add test for linkItegration dispatch and back button
    Co-authored-by: Linda Lai <llai@zendesk.com>

--- packages/framework/src/apps/messenger/features/widget/components/ChannelPage/__tests__/index.test.js
+++ packages/framework/src/apps/messenger/features/widget/components/ChannelPage/__tests__/index.test.js
@@ -1,6 +1,27 @@
+import * as integrationStore from 'src/apps/messenger/store/integrations'
+import { Route, MemoryRouter } from 'react-router-dom'
+import createStore from 'src/apps/messenger/store'
 import { render } from 'src/apps/messenger/utils/testHelpers'
-import ChannelPage from '../'
 import { createMemoryHistory } from 'history'
+
+import ChannelPage from '../'
+
+const renderWithRouter = ({ channelId }) => {
+  const store = createStore()
+  jest.spyOn(integrationStore, 'linkIntegration')
+  jest
+    .spyOn(integrationStore, 'selectIntegrationById')
+    .mockImplementation(() => ({ linkRequest: { channelId, url: 'http://some.url/' } }))
+
+  return render(
+    <MemoryRouter initialEntries={[`/channelPage/${channelId}`]}>
+      <Route path={'/channelPage/:channelId'}>
+        <ChannelPage />
+      </Route>
+    </MemoryRouter>,
+    { store }
+  )
+}
 
 describe('Header', () => {
   const renderComponent = (options = {}) => {
@@ -16,5 +37,21 @@ describe('Header', () => {
     expect(getByLabelText('Back to conversation')).toBeInTheDocument()
     getByLabelText('Back to conversation').click()
     expect(history.location.pathname).toEqual('/')
+  })
+})
+
+describe('ChannelPage', () => {
+  it('dispatches a channel link request on page load', () => {
+    const channelId = 'messenger'
+
+    renderWithRouter({ channelId })
+
+    expect(integrationStore.linkIntegration).toHaveBeenCalledWith(channelId)
+  })
+
+  it('renders a back button ', () => {
+    const { getByText } = renderWithRouter({ channelId: 'messenger' })
+
+    expect(getByText('Back')).toBeInTheDocument()
   })
 })
