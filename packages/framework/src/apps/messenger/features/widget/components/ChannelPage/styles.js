40135537e3a84bcb7fff13ba130b29003e85e442
Matt Doyle <matt.doyle@zendesk.com>
2021-07-21 11:01:11 +1000

    refactor: Remove unused test and styles after move to conversation-components

--- packages/framework/src/apps/messenger/features/widget/components/ChannelPage/styles.js
+++ packages/framework/src/apps/messenger/features/widget/components/ChannelPage/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import { rem } from 'polished'
 
 const Container = styled.div`
   display: flex;
@@ -7,47 +6,4 @@ const Container = styled.div`
   justify-content: center;
 `
 
-const Header = styled.div`
-  position: fixed;
-  left: ${(props) => props.theme.messenger.space.sm};
-  top: ${(props) => props.theme.messenger.space.xs};
-`
-
-// Semantically, should this be <main>?
-const Body = styled.div`
-  display: flex;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-  flex-direction: column;
-`
-
-const Title = styled.h1`
-  font-size: ${(props) => props.theme.messenger.fontSizes.lg};
-  line-height: ${(props) => props.theme.messenger.lineHeights.lg};
-  margin-bottom: ${(props) => props.theme.messenger.space.xs};
-`
-
-const Subtitle = styled.p`
-  font-size: ${(props) => props.theme.messenger.fontSizes.md};
-  line-height: ${(props) => props.theme.messenger.lineHeights.md};
-  margin-bottom: ${(props) => props.theme.messenger.space.md};
-  max-width: ${(props) => rem(300, props.theme.baseFontSize)};
-`
-
-const ChannelIcon = styled.div`
-  height: ${(props) => props.theme.messenger.iconSizes.xl};
-  width: ${(props) => props.theme.messenger.iconSizes.xl};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: ${(props) => props.theme.messenger.space.md};
-
-  svg,
-  img {
-    height: ${(props) => props.theme.messenger.iconSizes.xl};
-    width: ${(props) => props.theme.messenger.iconSizes.xl};
-  }
-`
-
-export { Container, Body, Header, Title, Subtitle, ChannelIcon }
+export { Container }
