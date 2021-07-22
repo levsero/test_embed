4bb6db1fdddcada0f65ecaba38297b67ac0308fb
Alex Robinson <arobinson@zendesk.com>
2021-07-22 16:43:49 +1000

    Remove webpack alias 'mixin'

--- packages/framework/webpack/webpack.common.js
+++ packages/framework/webpack/webpack.common.js
@@ -114,7 +114,6 @@ module.exports = {
   resolve: {
     alias: {
       // Framework
-      mixin: path.join(projectRoot + '/src/component/mixin'),
       service: path.join(projectRoot + '/src/service'),
       src: path.join(projectRoot + '/src'),
       utility: path.join(projectRoot + '/src/util'),
