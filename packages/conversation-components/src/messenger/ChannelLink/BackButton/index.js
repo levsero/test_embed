7b24d563dbf4a9d19ebb596bf6807881599d8be4
Matt Doyle <matt.doyle@zendesk.com>
2021-07-26 16:44:02 +1000

    refactor: remove default function for BackButton onClick handler

--- packages/conversation-components/src/messenger/ChannelLink/BackButton/index.js
+++ packages/conversation-components/src/messenger/ChannelLink/BackButton/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import { IconButton } from '@zendeskgarden/react-buttons'
 import ChevronIcon from '@zendeskgarden/svg-icons/src/16/chevron-left-stroke.svg'
 
-const BackButton = ({ onClick = () => {}, ariaLabel }) => {
+const BackButton = ({ onClick, ariaLabel }) => {
   return (
     <IconButton onClick={onClick} aria-label={ariaLabel}>
       <ChevronIcon />
