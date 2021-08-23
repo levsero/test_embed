ecca6875bcbbc7b0d47f43aa16592b3352d23648
Matt Doyle <matt.doyle@zendesk.com>
2021-08-23 11:36:41 +1000

    feat: Animate the menu trigger when it first appears

--- packages/conversation-components/src/messenger/MessengerHeader/Menu/index.js
+++ packages/conversation-components/src/messenger/MessengerHeader/Menu/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { useContext } from 'react'
+import Transition from 'react-transition-group/Transition'
 import { ThemeContext } from 'styled-components'
 import { Trigger } from '@zendeskgarden/react-dropdowns'
 import { Dropdown } from 'src/Dropdown'
@@ -8,7 +9,7 @@ import { HeaderControl, IconButton } from 'src/messenger/MessengerHeader/styles'
 import FBMessengerIcon from './FBMessengerIcon'
 import InstagramIcon from './InstagramIcon'
 import WhatsAppIcon from './WhatsAppIcon'
-import { ChannelIcon, HeaderMenu, HeaderMenuItem, MenuIcon } from './styles'
+import { ChannelIcon, HeaderMenu, HeaderMenuItem, MenuIcon, MenuTrigger } from './styles'
 
 const channelOptions = [
   {
@@ -38,43 +39,53 @@ const Menu = ({ channels = {}, onChannelSelect, isOpen, onStateChange }) => {
     (channel) => channels?.[channel.key] !== undefined
   )
 
-  if (channelsToDisplay.length === 0) {
-    return null
-  }
-
   return (
-    <HeaderControl>
-      <Dropdown
-        isOpen={isOpen}
-        onStateChange={onStateChange}
-        onSelect={(selected) => {
-          onChannelSelect(selected)
-        }}
-        downshiftProps={{
-          environment: currentFrame?.window,
-        }}
-      >
-        <Trigger>
-          <IconButton isPill={true} aria-label={labels.channelLinkingMenuAriaLabel}>
-            <MenuIcon />
-          </IconButton>
-        </Trigger>
-        <HeaderMenu placement="bottom-end">
-          {channelsToDisplay.map((channel) => {
-            const ChannelLogo = channel.icon
-            return (
-              <HeaderMenuItem key={channel.key} value={channel.key}>
-                <ChannelIcon channel={channel.key}>
-                  <ChannelLogo />
-                </ChannelIcon>
-                {` `}
-                {labels.continueOnChannel(channel.name)}
-              </HeaderMenuItem>
-            )
-          })}
-        </HeaderMenu>
-      </Dropdown>
-    </HeaderControl>
+    <Transition
+      appear={false}
+      in={channelsToDisplay.length !== 0}
+      timeout={0}
+      mountOnEnter={true}
+      unmountOnExit={true}
+    >
+      {(state) => {
+        return (
+          <HeaderControl>
+            <Dropdown
+              isOpen={isOpen}
+              onStateChange={onStateChange}
+              onSelect={(selected) => {
+                onChannelSelect(selected)
+              }}
+              downshiftProps={{
+                environment: currentFrame?.window,
+              }}
+            >
+              <MenuTrigger state={state}>
+                <Trigger>
+                  <IconButton isPill={true} aria-label={labels.channelLinkingMenuAriaLabel}>
+                    <MenuIcon />
+                  </IconButton>
+                </Trigger>
+              </MenuTrigger>
+              <HeaderMenu placement="bottom-end">
+                {channelsToDisplay.map((channel) => {
+                  const ChannelLogo = channel.icon
+                  return (
+                    <HeaderMenuItem key={channel.key} value={channel.key}>
+                      <ChannelIcon channel={channel.key}>
+                        <ChannelLogo />
+                      </ChannelIcon>
+                      {` `}
+                      {labels.continueOnChannel(channel.name)}
+                    </HeaderMenuItem>
+                  )
+                })}
+              </HeaderMenu>
+            </Dropdown>
+          </HeaderControl>
+        )
+      }}
+    </Transition>
   )
 }
 
