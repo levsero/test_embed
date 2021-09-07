080e04709a96b1e136c560698be45b49e4208198
Matt Doyle <matt.doyle@zendesk.com>
2021-09-07 16:24:21 +1000

    style: ChannelPageContainer 120px from top on portrait and 60px from the top when landscape and fullscreen

--- packages/conversation-components/src/messenger/ChannelLinkContainer/index.js
+++ packages/conversation-components/src/messenger/ChannelLinkContainer/index.js
@@ -1,9 +1,16 @@
-import styled from 'styled-components'
+import { rem } from 'polished'
+import styled, { css } from 'styled-components'
 
 const ChannelLinkContainer = styled.div`
-  display: flex;
-  height: 100%;
-  justify-content: center;
+  margin-top: ${(props) => rem(120, props.theme.messenger.baseFontSize)};
+
+  ${(props) =>
+    props.theme.messenger.isFullScreen &&
+    css`
+      @media (orientation: landscape) {
+        margin-top: ${(props) => rem(60, props.theme.messenger.baseFontSize)};
+      }
+    `}
 `
 
 export default ChannelLinkContainer
