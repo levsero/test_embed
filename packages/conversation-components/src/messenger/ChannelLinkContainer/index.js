540d2dfc3b22d4ca02ac025d7f8db851e6973412
Matt Doyle <matt.doyle@zendesk.com>
2021-09-28 19:32:00 +1000

    style: Use position: fixed to allow scrolling on channel link container

--- packages/conversation-components/src/messenger/ChannelLinkContainer/index.js
+++ packages/conversation-components/src/messenger/ChannelLinkContainer/index.js
@@ -2,16 +2,21 @@ import { rem } from 'polished'
 import styled, { css } from 'styled-components'
 
 const ChannelLinkContainer = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
   display: flex;
-  justify-content: center;
-  margin-top: ${(props) => rem(120, props.theme.messenger.baseFontSize)};
-  margin-bottom: ${(props) => props.theme.messenger.space.lg};
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
+  padding-top: ${(props) => rem(120, props.theme.messenger.baseFontSize)};
+  padding-bottom: ${(props) => props.theme.messenger.space.lg};
 
   ${(props) =>
     props.theme.messenger.isFullScreen &&
     css`
       @media (orientation: landscape) {
-        margin-top: ${rem(60, props.theme.messenger.baseFontSize)};
+        padding-top: ${rem(60, props.theme.messenger.baseFontSize)};
       }
     `}
 `
