1fa5a8739d8f0290318a58342f6be5eb46a9ff38
keitheous <kchong@zendesk.com>
2021-11-15 16:42:29 +1100

    Create LauncherShape for Circle and Square

--- packages/conversation-components/src/messenger/Launcher/__tests__/index.test.js
+++ packages/conversation-components/src/messenger/Launcher/__tests__/index.test.js
@@ -3,13 +3,13 @@ import userEvent from '@testing-library/user-event'
 import { find } from 'styled-components/test-utils'
 import render from 'src/utils/test/render'
 import { MessengerIcon, CloseIcon } from '../LauncherIcon/styles'
-import SquareLauncher from '../index'
+import Launcher from '../index'
 
 const renderComponent = (props = {}) => {
-  return render(<SquareLauncher {...props} />)
+  return render(<Launcher {...props} />)
 }
 
-describe('SquareLauncher', () => {
+describe('Launcher', () => {
   it('initially renders the Open icon', () => {
     const { container } = renderComponent()
 
@@ -33,5 +33,22 @@ describe('SquareLauncher', () => {
     background-color: #17494D !important;
     color: #17494D !important;
     box-shadow: -4px 0px 20px 0px rgba(36,36,36,0.2);`)
+  })
+
+  describe('button border radius', () => {
+    it('is 0 by default', () => {
+      renderComponent()
+      expect(screen.getByRole('button')).toHaveStyle('border-radius: 0')
+    })
+
+    it('is 0 when shape is square', () => {
+      renderComponent({ shape: 'square' })
+      expect(screen.getByRole('button')).toHaveStyle('border-radius: 0')
+    })
+
+    it('is 50% when shape is circle', () => {
+      renderComponent({ shape: 'circle' })
+      expect(screen.getByRole('button')).toHaveStyle('border-radius: 50%')
+    })
   })
 })
--- packages/conversation-components/src/messenger/Launcher/index.js
+++ packages/conversation-components/src/messenger/Launcher/index.js
@@ -1,36 +1,39 @@
 import PropTypes from 'prop-types'
 import { forwardRef } from 'react'
+import { LAUNCHER_SHAPES, LAUNCHER_POSITION } from 'src/constants'
 import useLabels from 'src/hooks/useLabels'
 import LauncherIcon from './LauncherIcon'
 import { Container, Button } from './styles'
 
-const SquareLauncher = forwardRef(({ isOpen, onClick, position, size }, ref) => {
+const Launcher = forwardRef(({ shape, isOpen, onClick, position, size }, ref) => {
   const { launcher } = useLabels()
 
   return (
     <Container
       ref={ref}
       size={size}
+      shape={shape}
       onKeyDown={() => {
         // The focus jail does not pick up onKeyDown if not used at least once.
       }}
     >
-      <Button onClick={onClick} aria-label={launcher.ariaLabel} isPill={false}>
+      <Button shape={shape} onClick={onClick} aria-label={launcher.ariaLabel} isPill={false}>
         <LauncherIcon isOpen={isOpen} position={position} />
       </Button>
     </Container>
   )
 })
 
-SquareLauncher.propTypes = {
+Launcher.propTypes = {
+  shape: PropTypes.oneOf(Object.keys(LAUNCHER_SHAPES)),
   isOpen: PropTypes.bool,
   onClick: PropTypes.func,
-  position: PropTypes.oneOf(['left', 'right']),
+  position: PropTypes.oneOf(LAUNCHER_POSITION),
   size: PropTypes.string,
 }
 
-SquareLauncher.defaultProps = {
+Launcher.defaultProps = {
   size: '64px',
 }
 
-export default SquareLauncher
+export default Launcher
