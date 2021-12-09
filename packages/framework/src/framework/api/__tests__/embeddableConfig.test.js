49b1eeac3bc2adc4c862b4ba3b8a57baece02b95
Timothy Patullock <tpatullock@zendesk.com>
2021-12-09 12:39:16 +1100

    add tests:

--- packages/framework/src/framework/api/__tests__/embeddableConfig.test.js
+++ packages/framework/src/framework/api/__tests__/embeddableConfig.test.js
@@ -1,4 +1,4 @@
-import { isRequestFromLivePreview } from 'src/framework/api/embeddableConfig'
+import { fetchEmbeddableConfig, isRequestFromLivePreview } from 'src/framework/api/embeddableConfig'
 
 describe('isRequestFromLivePreview', () => {
   const mockHost = (host) => {
@@ -65,5 +65,55 @@ describe('isRequestFromLivePreview', () => {
         expect(isRequestFromLivePreview()).toEqual(false)
       })
     })
+  })
+})
+
+describe('fetchEmbeddableConfig', () => {
+  beforeEach(() => {
+    document.zendeskHost = 'testing.zendesk.com'
+    window.fetch = jest.fn().mockImplementation(async () => ({
+      status: 200,
+      json: async () => ({ embeddable: 'config' }),
+    }))
+  })
+
+  it('installs the fetch polyfill if fetch is not available in the browser', async () => {
+    delete window.fetch
+    document.zendeskHost = 'testing.zendesk.com'
+    try {
+      await fetchEmbeddableConfig()
+    } catch {}
+    expect(window.fetch).toBeTruthy()
+  })
+
+  it('throws an error if zendeskHost is not defined', async () => {
+    document.zendeskHost = undefined
+    await expect(fetchEmbeddableConfig()).rejects.toThrowError('Missing zendeskHost config param.')
+  })
+
+  it('throws an error if the status is not 200', async () => {
+    window.fetch = jest.fn().mockImplementation(async () => ({ status: 400 }))
+    await expect(fetchEmbeddableConfig()).rejects.toThrowError('Failed to fetch config')
+  })
+
+  it('returns the embeddable config', async () => {
+    await expect(fetchEmbeddableConfig()).resolves.toEqual({ embeddable: 'config' })
+  })
+
+  it('fetches the embeddable config if not on the live preview page', async () => {
+    await fetchEmbeddableConfig()
+    expect(window.fetch).toHaveBeenCalledWith('https://testing.zendesk.com/embeddable/config')
+  })
+
+  it('fetches a fake embeddable config if on the live preview page', async () => {
+    jest
+      .spyOn(window, 'location', 'get')
+      .mockImplementation(() => ({ host: 'static-staging.zdassets.com' }))
+    window.zESettings = { preview: true }
+
+    await fetchEmbeddableConfig()
+    expect(window.fetch).toHaveBeenCalledWith(
+      'https://testing.zendesk.com/embeddable/preview/config'
+    )
   })
 })
