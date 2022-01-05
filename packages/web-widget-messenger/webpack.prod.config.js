06d8ed623bba9ce7f1695599711d7cbc5860f0f8
Alex Robinson <arobinson@zendesk.com>
2022-01-06 10:42:59 +1100

    Only have one build script for the messaging wdiget

--- packages/web-widget-messenger/webpack.prod.config.js
+++ packages/web-widget-messenger/webpack.prod.config.js
@@ -1,6 +1,7 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.config.js')
+
 let config = merge(common, {
   mode: 'production',
   plugins: [
