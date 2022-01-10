87e224d01b2b49fdcf3945b35a765d7519ddc5fc
Linda Lai <llai@zendesk.com>
2022-01-10 16:50:19 +1100

    test: Add tests for useOnMultilineSubmit hook

---
+++ packages/web-widget-classic/classicSrc/hooks/__tests__/useOnMultilineSubmit.test.js
@@ -0,0 +1,36 @@
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import useOnMultilineSubmit from '../useOnMultilineSubmit'
+
+describe('useOnMultilineSubmit', () => {
+  // eslint-disable-next-line react/prop-types
+  const RandomComponent = ({ onSubmit }) => {
+    const { handleCompositionStart, handleCompositionEnd, handleKeyDown } = useOnMultilineSubmit(
+      onSubmit
+    )
+    return (
+      <textarea
+        aria-label="randomText"
+        onCompositionStart={handleCompositionStart}
+        onCompositionEnd={handleCompositionEnd}
+        onKeyDown={handleKeyDown}
+      />
+    )
+  }
+
+  it('submits text when Enter key is pressed', () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<RandomComponent onSubmit={onSubmit} />)
+    userEvent.type(getByLabelText('randomText'), 'The quick brown fox')
+    userEvent.type(getByLabelText('randomText'), '{enter}')
+    expect(onSubmit).toBeCalled()
+  })
+
+  it('does not submit text when Shift and Enter keys are pressed', () => {
+    const onSubmit = jest.fn()
+    const { getByLabelText } = render(<RandomComponent onSubmit={onSubmit} />)
+    userEvent.type(getByLabelText('randomText'), 'The quick{shift}{enter}brown fox')
+    expect(onSubmit).not.toBeCalled()
+    expect(getByLabelText('randomText')).toHaveValue('The quick\nbrown fox')
+  })
+})
