b00dc6815ad84b1156f7fe8bea75a4336c4fe4e1
Linda Lai <llai@zendesk.com>
2022-01-20 13:35:51 +1100

    fix: Add browser conditions to useOnMultilineSubmit for composition events

--- packages/web-widget-classic/classicSrc/hooks/useOnMultilineSubmit.js
+++ packages/web-widget-classic/classicSrc/hooks/useOnMultilineSubmit.js
@@ -1,34 +1,30 @@
 import { useRef } from 'react'
+import { isSafari } from '@zendesk/widget-shared-services'
 import { keyCodes } from 'classicSrc/util/keyboard'
 
 const useOnMultilineSubmit = (onSubmit) => {
-  const isComposing = useRef(false)
-  const compositionHasJustEnded = useRef(false)
+  const formingNewWordInJapanese = useRef(false)
 
-  const handleCompositionStart = () => (isComposing.current = true)
-  const handleCompositionEnd = () => {
-    isComposing.current = false
-    compositionHasJustEnded.current = true
-  }
+  const handleCompositionStart = () => (formingNewWordInJapanese.current = true)
+  const handleCompositionEnd = () => (formingNewWordInJapanese.current = true)
 
   // If Enter key is part of an IME or multi-line composition, don't submit the text.
-  // The e.nativeEvent.isComposing, isComposing and compositionHasJustEnded flags
+  // The e.nativeEvent.isComposing and formingNewWordInJapanese flags
   // support composing text in foreign languages and order of keypress events across browsers.
   // See https://github.com/threema-ch/threema-web/issues/777
   const handleKeyDown = (e) => {
-    if (e.key === keyCodes.ENTER && !e.shiftKey) {
-      if (e.nativeEvent.isComposing || isComposing.current || compositionHasJustEnded.current) {
-        return
-      }
+    if (e.key === keyCodes.ENTER && e.shiftKey) {
+      return
+    } else if (e.key === keyCodes.ENTER) {
+      if (e.nativeEvent.isComposing) return
+      if (isSafari() && formingNewWordInJapanese.current) return
 
       e.preventDefault()
       onSubmit()
     }
   }
 
-  const handleKeyUp = () => {
-    compositionHasJustEnded.current = false
-  }
+  const handleKeyUp = () => (formingNewWordInJapanese.current = false)
 
   return { handleCompositionStart, handleCompositionEnd, handleKeyDown, handleKeyUp }
 }
