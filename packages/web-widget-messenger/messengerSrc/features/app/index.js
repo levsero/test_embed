61a64c1370868195a56784279e769c76c2ed56b6
Kan Wang <kan.wang@zendesk.coo>
2022-01-24 09:16:20 +1100

    Remove lazy load for widget label

--- packages/web-widget-messenger/messengerSrc/features/app/index.js
+++ packages/web-widget-messenger/messengerSrc/features/app/index.js
@@ -2,12 +2,12 @@ import React, { Suspense } from 'react'
 import { useSelector } from 'react-redux'
 import { LAUNCHER_SHAPES } from '@zendesk/conversation-components'
 import { getLauncherShape } from 'messengerSrc/features/launcher/store'
+import LauncherLabel from 'messengerSrc/features/launcherLabel'
 import Widget from 'messengerSrc/features/widget'
 import { getAreCookiesEnabled } from 'messengerSrc/store/cookies'
 import { useFocusJail } from './hooks/focusJail'
 
 const Launcher = React.lazy(() => import('messengerSrc/features/launcher'))
-const LauncherLabel = React.lazy(() => import('messengerSrc/features/launcherLabel'))
 const LauncherUnreadIndicator = React.lazy(() =>
   import('messengerSrc/features/launcherUnreadIndicator')
 )
@@ -24,13 +24,13 @@ const App = () => {
     <div style={{ fontSize: 'initial' }} onKeyDown={onKeyDownForContainer} role="presentation">
       <Widget ref={refWidget} />
       {launcherShape !== LAUNCHER_SHAPES.none && (
-        <Suspense fallback={null}>
+        <>
           <LauncherLabel ref={refLauncherLabel} />
-
-          <Launcher ref={refLauncher} />
-
-          <LauncherUnreadIndicator />
-        </Suspense>
+          <Suspense fallback={null}>
+            <Launcher ref={refLauncher} />
+            <LauncherUnreadIndicator />
+          </Suspense>
+        </>
       )}
     </div>
   )
